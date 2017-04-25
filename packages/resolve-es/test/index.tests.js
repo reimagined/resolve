9eab8dc2599229ac35fc145ff044db39c89e490b
Pavel Glushenkov <pglushenkov88@gmail.com>
2017-04-25 13:38:56 +0300

    eventstore interface (#11)
    * eventstore interface

---
+++ packages/resolve-es/test/index.tests.js
@@ -0,0 +1,43 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import createStore from '../src/index';
+
+const driver = {
+    saveEvent: () => Promise.resolve(),
+    loadEventsByTypes: sinon.spy(),
+    loadEventsByAggregateId: sinon.spy()
+};
+
+const eventstore = createStore({ driver });
+
+const event = {
+    name: 'test'
+};
+
+describe('resolve-es', () => {
+    it('onEventSaved called', () => {
+        const cb = sinon.spy();
+        eventstore.onEventSaved(cb);
+
+        return eventstore
+            .saveEvent(event)
+            .then(() => {
+                expect(cb.callCount).to.be.equal(1);
+                expect(cb.firstCall.args[0]).to.be.deep.equal(event);
+            });
+    });
+
+    it('loadEventsByTypes', () => {
+        const cb = () => {};
+        eventstore.loadEventsByTypes(['type'], cb);
+        expect(driver.loadEventsByTypes.callCount).to.be.equal(1);
+        expect(driver.loadEventsByTypes.firstCall.args).to.be.deep.equal([['type'], cb]);
+    });
+
+    it('loadEventsByAggregateId', () => {
+        const cb = () => {};
+        eventstore.loadEventsByAggregateId('id', cb);
+        expect(driver.loadEventsByAggregateId.callCount).to.be.equal(1);
+        expect(driver.loadEventsByAggregateId.firstCall.args).to.be.deep.equal(['id', cb]);
+    });
+});
