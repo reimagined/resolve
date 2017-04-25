ab14de78100fda9d678be160a941fa659d2814fe
timbset <timophey.trainenkov@devexpress.com>
2017-04-25 15:25:42 +0300

    Extract base logic to bus

--- packages/resolve-bus/src/index.js
+++ packages/resolve-bus/src/index.js
@@ -1,6 +1,34 @@
-export default function ({ driver }) {
+export default function (createDriver, options) {
+    const eventHandlersMap = new Map();
+    const driver = createDriver(options);
+
+    function trigger(event) {
+        const handlers = eventHandlersMap.get(event.__type) || [];
+        handlers.forEach(handler => handler(event));
+    }
+
+    driver.subscribe(trigger);
+
     return {
-        emitEvent: event => driver.emitEvent(event),
-        onEvent: (eventTypes, handler) => driver.onEvent(eventTypes, handler)
+        emitEvent: event => driver.publish(event),
+        onEvent: (eventTypes, callback) => {
+            eventTypes.forEach((eventType) => {
+                const handlers = eventHandlersMap.get(eventType) || [];
+                handlers.push(callback);
+                eventHandlersMap.set(eventType, handlers);
+            });
+
+            return () => {
+                eventTypes.forEach((eventType) => {
+                    const callbackArray = eventHandlersMap.get(eventType);
+                    const callbackIndex = callbackArray.indexOf(callback);
+
+                    if (callbackIndex >= 0) {
+                        callbackArray.splice(callbackIndex, 1);
+                    }
+                });
+            };
+        }
     };
 }
+
--- packages/resolve-bus/test/index.tests.js
+++ packages/resolve-bus/test/index.tests.js
@@ -4,25 +4,53 @@ import sinon from 'sinon';
 import createBus from '../src';
 
 describe('resolve-bus', () => {
-    it('calls driver\'s emitEvent', () => {
-        const fakeDriver = { emitEvent: sinon.spy() };
-        const fakeEvent = { __type: 'fakeType' };
-        const bus = createBus({ driver: fakeDriver });
-        bus.emitEvent(fakeEvent);
+    let driver;
+    let driverInstance;
 
-        expect(fakeDriver.emitEvent.calledOnce).to.be.true;
-        expect(fakeDriver.emitEvent.args[0][0]).to.be.deep.equal(fakeEvent);
+    beforeEach(() => {
+        driver = sinon.spy(() => {
+            driverInstance = {
+                publish: sinon.spy(),
+                subscribe: sinon.spy()
+            };
+
+            return driverInstance;
+        });
     });
 
-    it('calls driver\'s onEvent', () => {
-        const fakeDriver = { onEvent: sinon.spy() };
-        const fakeEventTypes = ['firstType', 'secondType'];
-        const handler = () => {};
-        const bus = createBus({ driver: fakeDriver });
-        bus.onEvent(fakeEventTypes, handler);
+    it('creates correct adapter instance', () => {
+        const options = { a: 1, b: '2' };
+        createBus(driver, options);
+        expect(driver.calledOnce).to.be.true;
+        expect(driver.args[0][0]).to.be.deep.equal(options);
+    });
 
-        expect(fakeDriver.onEvent.calledOnce).to.be.true;
-        expect(fakeDriver.onEvent.args[0][0]).to.be.deep.equal(fakeEventTypes);
-        expect(fakeDriver.onEvent.args[0][1]).to.be.deep.equal(handler);
+    it('passes handler to adapter', () => {
+        createBus(driver);
+        expect(driverInstance.subscribe.calledOnce).to.be.true;
+        expect(driverInstance.subscribe.args[0][0]).to.be.a('function');
+    });
+
+    it('emitEvent calls driver\'s publish', () => {
+        const fakeEvent = { __type: 'fakeType' };
+        const bus = createBus(driver);
+        bus.emitEvent(fakeEvent);
+
+        expect(driverInstance.publish.calledOnce).to.be.true;
+        expect(driverInstance.publish.args[0][0]).to.be.deep.equal(fakeEvent);
+    });
+
+    it('calls onEvent handler if subscribe on type', (done) => {
+        const bus = createBus(driver);
+
+        const fakeEventTypes = ['firstType', 'secondType'];
+        const fakeEvent = { __type: 'secondType' };
+
+        bus.onEvent(fakeEventTypes, (event) => {
+            expect(event).to.be.deep.equal(fakeEvent);
+            done();
+        });
+
+        driverInstance.subscribe.args[0][0](fakeEvent);
     });
 });
