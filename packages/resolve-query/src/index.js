d14a72e9a8638fa4c9ff04f0f486ccde25e276e6
Pavel Glushenkov <pglushenkov88@gmail.com>
2017-04-26 16:12:20 +0300

    use eventbus (#19)
    * use eventbus

--- packages/resolve-query/src/index.js
+++ packages/resolve-query/src/index.js
@@ -1,7 +1,18 @@
-export default ({ eventStore, projection }) => {
+const updateState = (projection, event, state) =>
+    projection.handlers[event.__type](state, event);
+
+export default ({ eventStore, eventBus, projection }) => {
     const eventsNames = Object.keys(projection.handlers);
     let state = projection.initialState;
-    return () => eventStore.loadEventsByTypes(eventsNames, (event) => {
-        state = projection.handlers[event.__type](state, event);
-    }).then(() => state);
+
+    const handler = event => (state = updateState(projection, event, state));
+
+    let result = null;
+    return () => {
+        result = result || eventStore.loadEventsByTypes(eventsNames, handler).then(() => {
+            eventBus.onEvent(eventsNames, handler);
+        });
+
+        return result.then(() => state);
+    };
 };
