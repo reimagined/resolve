7c77bbe388cb2fea3e0099e66901d5721d846611
Pavel Glushenkov <pavel.glushenkov@devexpress.com>
2017-04-26 15:01:28 +0300

    use eventbus

--- packages/resolve-query/src/index.js
+++ packages/resolve-query/src/index.js
@@ -1,7 +1,23 @@
-export default ({ eventStore, projection }) => {
+const updateState = (projection, event, state) =>
+    projection.handlers[event.__type](state, event);
+
+export default ({ eventStore, eventBus, projection }) => {
     const eventsNames = Object.keys(projection.handlers);
     let state = projection.initialState;
-    return () => eventStore.loadEventsByTypes(eventsNames, (event) => {
-        state = projection.handlers[event.__type](state, event);
-    }).then(() => state);
+
+    let firstCall = true;
+    return () => {
+        if (firstCall) {
+            firstCall = false;
+            return eventStore.loadEventsByTypes(eventsNames, event =>
+                (state = updateState(projection, event, state)))
+                .then(() => {
+                    eventBus.onEvent(eventsNames, (event) => {
+                        state = updateState(projection, event, state);
+                    });
+                    return state;
+                });
+        }
+        return Promise.resolve(state);
+    };
 };
