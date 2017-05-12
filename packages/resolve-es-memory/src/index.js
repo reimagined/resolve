8f9f340ddecef3a3404f2aac36f398124005700a
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2017-05-12 11:54:49 +0300

    sorting by timestamp for memory driver

--- packages/resolve-es-memory/src/index.js
+++ packages/resolve-es-memory/src/index.js
@@ -1,3 +1,5 @@
+const compareEvents = (a, b) => a.timestamp - b.timestamp;
+
 export default (events) => {
     const store = events || [];
 
@@ -8,9 +10,19 @@ export default (events) => {
         },
 
         loadEventsByTypes: (types, callback) =>
-            Promise.resolve(store.filter(event => types.includes(event.type)).forEach(callback)),
+            Promise.resolve(
+                store
+                    .filter(event => types.includes(event.type))
+                    .sort(compareEvents)
+                    .forEach(callback)
+            ),
 
         loadEventsByAggregateId: (id, callback) =>
-            Promise.resolve(store.filter(event => event.aggregateId === id).forEach(callback))
+            Promise.resolve(
+                store
+                    .filter(event => event.aggregateId === id)
+                    .sort(compareEvents)
+                    .forEach(callback)
+            )
     };
 };
