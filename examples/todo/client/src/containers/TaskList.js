c8cca34d5a9b7e5686c31b6939e5f7eb96c2d9d6
VadimKhoroshiltsev <vadim.khoroshiltsev@devexpress.com>
2017-05-18 11:08:07 +0300

    Fix issue with empty state

--- examples/todo/client/src/containers/TaskList.js
+++ examples/todo/client/src/containers/TaskList.js
@@ -3,19 +3,21 @@ import { withRouter } from 'react-router-dom';
 
 import TaskList from '../components/TaskList';
 
-const getTaskList = (cards, currentCardId) => (
-    currentCardId
-        ? cards[currentCardId].todoList
+function getTaskList(cards, currentCardId) {
+    const currentCard = cards[currentCardId];
+
+    return currentCard
+        ? (currentCard.todoList || {})
         : Object.keys(cards).reduce((result, key) =>
             Object.assign(result, cards[key].todoList)
         , {})
-)
+}
 
 const mapStateToProps = ({ cards }, { match }) => {
     const cardId = match.params.cardId;
 
     return {
-        title: (cardId ? cards[cardId].name : 'All'),
+        title: (cardId && cards[cardId]) ? cards[cardId].name : 'All',
         tasks: getTaskList(cards, cardId)
     };
 };
