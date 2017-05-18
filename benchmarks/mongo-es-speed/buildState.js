9f2545657ed2f0edf0e834fa0b87cd3abbf46b29
IhostVlad <ihost@mail.ru>
2017-05-18 14:37:22 +0300

    Feature/benchmark es mongo (#49)
    * Implement benchmark for resolve-es-mongo driver

---
+++ benchmarks/mongo-es-speed/buildState.js
@@ -0,0 +1,38 @@
+import mongoDbDriver from 'resolve-es-mongo';
+import createEs from 'resolve-es';
+
+import { INFO_TOKEN, DONE_TOKEN, ERR_TOKEN } from './constants';
+import config from './config';
+
+const TYPES = config.GENERATED_EVENT_TYPES;
+
+const store = createEs({ driver: mongoDbDriver({
+    url: config.MONGODB_CONNECTION_URL,
+    collection: config.MONGODB_COLLECTION_NAME
+}) });
+
+let eventCounter = 0;
+let lastReportedEvents = 0;
+
+function reporterHandler() {
+    if (lastReportedEvents !== eventCounter) {
+        const tickSize = eventCounter - lastReportedEvents;
+        // eslint-disable-next-line no-console
+        console.log(INFO_TOKEN, tickSize);
+        lastReportedEvents = eventCounter;
+    }
+
+    setTimeout(reporterHandler, 500);
+}
+
+setTimeout(reporterHandler, 500);
+
+store.loadEventsByTypes(TYPES, () => (eventCounter++)).then(() =>
+    // eslint-disable-next-line no-console
+    console.log(DONE_TOKEN, JSON.stringify({
+        memory: process.memoryUsage()
+    }))
+).catch(err =>
+    // eslint-disable-next-line no-console
+    console.log(ERR_TOKEN, err)
+);
---
+++ benchmarks/mongo-es-speed/prepareEvents.js
@@ -0,0 +1,51 @@
+import mongoDbDriver from 'resolve-es-mongo';
+import createEs from 'resolve-es';
+
+import config from './config';
+
+const TYPES = config.GENERATED_EVENT_TYPES;
+const PAYLOAD_COUNT = config.PAYLOAD_ELEMENTS_COUNT;
+const PAYLOAD_SIZE = config.PAYLOAD_ELEMENT_SIZE;
+
+const store = createEs({ driver: mongoDbDriver({
+    url: config.MONGODB_CONNECTION_URL,
+    collection: config.MONGODB_COLLECTION_NAME
+}) });
+
+function numericRandom(maxlen) {
+    let value = '';
+    while (value.length < maxlen) {
+        value += String(Math.random()).substring(2, 10);
+    }
+    return value;
+}
+
+function buildPayload() {
+    let nameCounter = 0;
+
+    return Array.from(new Array(PAYLOAD_COUNT)).reduce((obj) => {
+        const content = `FieldValue${numericRandom(PAYLOAD_SIZE)}`;
+        obj[`FieldName${nameCounter++}`] = content;
+        return obj;
+    }, Object.create(null));
+}
+
+export default function (eventsCount, reportObj) {
+    let processedEvents = 0;
+
+    return Array.from(new Array(eventsCount)).reduce(
+        acc => acc.then(() =>
+            store.saveEvent({
+                type: TYPES[Math.floor(Math.random() * TYPES.length)],
+                aggregateId: `GUID${numericRandom(20)}`,
+                payload: buildPayload(),
+                timestamp: Date.now()
+            }).then(() => ((++processedEvents % 5000 === 0)
+                ? (reportObj.value += 5000)
+                : null
+            ))
+        ),
+        Promise.resolve()
+    );
+}
+
