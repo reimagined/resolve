036df9e5854c0a61d4b5548188954f47721b165f
Vladislav Vlasov <vladislav.vlasov@devexpress.com>
2017-05-19 17:39:09 +0300

    Working version of zmq bus based on xpub/xsub

--- packages/resolve-bus-zmq/src/index.js
+++ packages/resolve-bus-zmq/src/index.js
@@ -1,26 +1,59 @@
-import zeromq from 'zeromq';
+import zmq from 'zeromq';
 
 const defaultOptions = {
-    url: 'tcp://127.0.0.1:2110'
+    channel: 'DEFAULT',
+    address: '127.0.0.1',
+    pubPort: 2110,
+    subPort: 2111
 };
 
+function runBroker({ address, pubPort, subPort }) {
+    const pubListener = `tcp://${address}:${pubPort}`;
+    const subListener = `tcp://${address}:${subPort}`;
+    const hwm = 1000;
+    const verbose = 0;
+
+    const subSock = zmq.socket('xsub');
+    subSock.identity = `subscriber${process.pid}`;
+    subSock.bindSync(subListener);
+
+    const pubSock = zmq.socket('xpub');
+    pubSock.identity = `publisher${process.pid}`;
+    pubSock.setsockopt(zmq.ZMQ_SNDHWM, hwm);
+    pubSock.setsockopt(zmq.ZMQ_XPUB_VERBOSE, verbose);
+    pubSock.bindSync(pubListener);
+
+    subSock.on('message', data => pubSock.send(data));
+    pubSock.on('message', data => subSock.send(data));
+}
+
+function getPublisher({ address, subPort, channel }) {
+    const sock = zmq.socket('pub');
+    sock.connect(`tcp://${address}:${subPort}`);
+    return (event) => {
+        const message = `${channel} ${event}`;
+        sock.send(message);
+    };
+}
+
+function buildConsumer({ address, pubPort, channel }, trigger) {
+    const sock = zmq.socket('sub');
+    sock.subscribe(channel);
+    sock.connect(`tcp://${address}:${pubPort}`);
+    sock.on('message', (message) => {
+        const data = message.toString().substring(channel.length + 1);
+        trigger(JSON.parse(data));
+    });
+}
+
 function init(options, trigger) {
-    return Promise.reject()
-        .catch(() => {
-            const busSocket = zeromq.socket('push');
-            busSocket.bindSync(options.url);
-            return busSocket;
-        })
-        .catch(() => {
-            const busSocket = zeromq.socket('pull');
-            busSocket.connect(options.url);
-            return busSocket;
-        })
-        .catch(() => null)
-        .then((busSocket) => {
-            if (!busSocket) return null;
-            busSocket.on('message', trigger);
-            return busSocket;
+    return Promise.resolve()
+        .then(() => runBroker(options))
+        .catch(err => err) // Broken may not run if already started
+        .then(() => {
+            const publisher = getPublisher(options);
+            buildConsumer(options, trigger);
+            return { publisher };
         });
 }
 
@@ -31,8 +64,8 @@ export default function (options) {
 
     return {
         publish: event =>
-            initPromise.then(channel =>
-                channel.send(JSON.stringify(event))
+            initPromise.then(({ publisher }) =>
+                publisher(JSON.stringify(event))
             ),
         setTrigger: callback =>
             initPromise.then(() =>
