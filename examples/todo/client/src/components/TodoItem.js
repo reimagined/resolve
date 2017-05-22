c20517e74f83228ef2c8afa36d4f612826e1dabe
timbset <timophey.trainenkov@devexpress.com>
2017-05-22 14:47:12 +0300

    Fix TodoItem unused state

--- examples/todo/client/src/components/TodoItem.js
+++ examples/todo/client/src/components/TodoItem.js
@@ -1,35 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classnames from 'classnames';
 
-export default class TodoItem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { editing: false };
-    }
+export default function TodoItem(props) {
+    const { todo } = props;
 
-    render() {
-        const { todo } = this.props;
-
-        return (
-            <li
-                className={classnames({
-                    completed: todo.completed,
-                    editing: this.state.editing
-                })}
-            >
-                <div className="view">
-                    <input
-                        className="toggle"
-                        type="checkbox"
-                        checked={todo.checked}
-                        onChange={this.props.onCheck}
-                    />
-                    <label htmlFor="itemContext">
-                        {todo.name}
-                    </label>
-                    <button className="destroy" onClick={this.props.onRemove} />
-                </div>
-            </li>
-        );
-    }
+    return (
+        <li
+            className={classnames({
+                completed: todo.completed
+            })}
+        >
+            <div className="view">
+                <input
+                    className="toggle"
+                    type="checkbox"
+                    checked={todo.checked}
+                    onChange={props.onCheck}
+                />
+                <label htmlFor="itemContext">
+                    {todo.name}
+                </label>
+                <button className="destroy" onClick={props.onRemove} />
+            </div>
+        </li>
+    );
 }
