a19677ed992c14feee1f2f4ed4d7258f78b03e4e
Vladislav Vlasov <vladislav.vlasov@devexpress.com>
2017-05-26 13:29:13 +0300

    Eslint fixes

--- benchmarks/event-circuit-speed/index.js
+++ benchmarks/event-circuit-speed/index.js
@@ -7,5 +7,5 @@ benchmark({
     totalValue: config.BENCHMARK_SERIES.reduce((acc, val) => (acc + val), 0) * 2,
     preparerModule: path.join(__dirname, './preparer.js'),
     workerModule: path.join(__dirname, './worker.js'),
-    reporterModule: path.join(__dirname, './reporter.js'),
+    reporterModule: path.join(__dirname, './reporter.js')
 });
