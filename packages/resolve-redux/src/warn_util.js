5e5cbe56c56f0a0103c5d7bde7064a68105b738d
Timophey Trainenkov <timophey.trainenkov@devexpress.com>
2017-06-05 11:19:37 +0300

    Add warnings if send command and fetch more data is incorrect in resolve-redux (#75)

---
+++ packages/resolve-redux/src/warn_util.js
@@ -0,0 +1,16 @@
+export default function (obj, beforeWarnings, afterWarnings) {
+    const warningMessages = Object.keys(obj)
+        .map(fieldName => (obj[fieldName] ? null : `The '${fieldName}' is required`))
+        .filter(msg => msg);
+
+    const shouldWarningsBePrinted = warningMessages.length > 0;
+
+    if (shouldWarningsBePrinted) {
+        // eslint-disable-next-line no-console
+        console.warn(
+            [beforeWarnings, ...warningMessages, afterWarnings].filter(line => line).join('\n')
+        );
+    }
+
+    return shouldWarningsBePrinted;
+}
--- packages/resolve-redux/test/fetch_more_saga.tests.js
+++ packages/resolve-redux/test/fetch_more_saga.tests.js
@@ -6,6 +6,7 @@ import fetchMoreSaga from '../src/fetch_more_saga';
 
 describe('fetchMoreSaga', () => {
     let sandbox;
+    let originalWarn;
 
     const state = {
         id1: {
@@ -25,6 +26,18 @@ describe('fetchMoreSaga', () => {
         sandbox.restore();
     });
 
+    beforeEach(() => {
+        // eslint-disable-next-line no-console
+        originalWarn = console.warn;
+        // eslint-disable-next-line no-console
+        console.warn = sinon.spy();
+    });
+
+    afterEach(() => {
+        // eslint-disable-next-line no-console
+        console.warn = originalWarn;
+    });
+
     it('works correctly', () => {
         const fetchMore = sinon.spy();
         const projectionName = 'projectionName';
@@ -37,5 +50,22 @@ describe('fetchMoreSaga', () => {
         expect(generator.next(state).value).to.be.deep.equal(
             put(actions.merge(projectionName, state))
         );
+    });
+
+    it('should throw error on send command if some of required fields are not defined', () => {
+        const args = { sendCommand() {} };
+        const action = actions.sendCommand({});
+        const generator = fetchMoreSaga(args, action);
+        generator.next();
+
+        // eslint-disable-next-line no-console
+        expect(console.warn.callCount).to.be.equal(1);
+
+        // eslint-disable-next-line no-console
+        const warnArgs = console.warn.args[0];
+
+        expect(warnArgs[0]).contains('Fetch more error:');
+        expect(warnArgs[0]).contains('The \'projectionName\' is required');
+        expect(warnArgs[0]).contains('The \'query\' is required');
     });
 });
