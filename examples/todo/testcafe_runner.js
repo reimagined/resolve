bf298394d080b39d2edb7247f4f55dc7bde001e0
Alexey Semikozov <forworknoname@gmail.com>
2017-06-22 16:55:07 +0300

    Add travis end-to-end tests

--- examples/todo/testcafe_runner.js
+++ examples/todo/testcafe_runner.js
@@ -1,21 +1,31 @@
 #!/usr/bin/env node
+const yargs = require('yargs');
 const createTestCafe = require('testcafe');
 const { getInstallations } = require('testcafe-browser-tools');
+const DELAY = 10000;
 let testcafe = null;
-getInstallations()
-    .then((browsers) => {
-        createTestCafe('localhost', 1337, 1338)
-            .then((tc) => {
-                testcafe = tc;
-                const runner = testcafe.createRunner();
-                const browser = Object.keys(browsers).slice(0, 1);
-                return runner
-                    .startApp('npm start')
-                    .src(['./testcafe/index.tests.js'])
-                    .browsers(browser)
-                    .run();
-            })
-            .then(() => {
-                testcafe.close();
-            })
-    });
+
+const argv = yargs
+    .option('browser', {
+        alias: 'b',
+        default: false
+    })
+    .help().argv;
+
+getInstallations().then(browsers =>
+    createTestCafe('localhost', 1337, 1338)
+        .then((tc) => {
+            testcafe = tc;
+            const runner = testcafe.createRunner();
+            const browser = argv.browser || Object.keys(browsers).slice(0, 1);
+            return runner
+                .startApp('npm run start', DELAY)
+                .src(['./testcafe/index.tests.js'])
+                .browsers(browser)
+                .run();
+        })
+        .then((exitCode) => {
+            testcafe.close();
+            process.exit(exitCode);
+        })
+);
