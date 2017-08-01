77c70ebb8585878add181d2ec60eee4b64f1a64c
IhostVlad <ihost@mail.ru>
2017-08-01 11:54:49 +0400

    Mongo reconnect fix (#131)
    * Implement multi process todolist
    
    * Extract config to file and rename projection to read-model
    
    * Refactoring
    
    * Handle error in es
    
    * Implement generalized async error handling

--- packages/resolve-es/src/index.js
+++ packages/resolve-es/src/index.js
@@ -1,23 +1,41 @@
 import 'regenerator-runtime/runtime';
 
-export default ({ storage, bus, transforms = [] }) => ({
-    async subscribeByEventType(eventTypes, handler) {
-        await storage.loadEventsByTypes(eventTypes, handler);
-
-        return bus.onEvent(eventTypes, handler);
-    },
-
-    getEventsByAggregateId(aggregateId, handler) {
-        return storage.loadEventsByAggregateId(aggregateId, handler);
-    },
-
-    onEvent(eventTypes, callback) {
-        return bus.onEvent(eventTypes, callback);
-    },
-
-    async saveEvent(event) {
-        await storage.saveEvent(event);
-        await bus.emitEvent(event);
-        return event;
+export default (
+    config,
+    errorHandler = (err) => {
+        throw err;
     }
-});
+) => {
+    const result = {
+        async subscribeByEventType(eventTypes, handler) {
+            await config.storage.loadEventsByTypes(eventTypes, handler);
+            return config.bus.onEvent(eventTypes, handler);
+        },
+
+        async getEventsByAggregateId(aggregateId, handler) {
+            return await config.storage.loadEventsByAggregateId(aggregateId, handler);
+        },
+
+        async onEvent(eventTypes, callback) {
+            return await config.bus.onEvent(eventTypes, callback);
+        },
+
+        async saveEvent(event) {
+            await config.storage.saveEvent(event);
+            await config.bus.emitEvent(event);
+            return event;
+        }
+    };
+
+    return Object.keys(result).reduce((acc, methodName) => {
+        acc[methodName] = async (...args) => {
+            try {
+                return await result[methodName](...args);
+            } catch (err) {
+                errorHandler(err);
+            }
+        };
+
+        return acc;
+    }, {});
+};
