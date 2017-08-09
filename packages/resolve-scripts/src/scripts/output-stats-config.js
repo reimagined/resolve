c733fbb313812e985e1c5fb33f12cef5e2ca80d2
Pavel Glushenkov <pglushenkov88@gmail.com>
2017-08-09 12:48:38 +0300

    Issues fix (#145)
    * fix issues
    
    * Show only important information in webpack log
    
    * fix css url
    
    * fix merge with master
    
    * prettier fix
    
    * fix package
    
    * fix package versions
    
    * fix static files
    
    * fix prettier
    
    * server side source map
    
    * fix resolve es
    
    * fix subscribe

---
+++ packages/resolve-scripts/src/scripts/output-stats-config.js
@@ -0,0 +1,11 @@
+export default {
+    assets: true,
+    colors: true,
+    version: false,
+    hash: false,
+    timings: true,
+    chunks: false,
+    chunkModules: false,
+    children: false,
+    modules: false
+};
