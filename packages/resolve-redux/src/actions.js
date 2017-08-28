48583600574cfe7cd10864467368fc6d79bf10ff
Vladislav Vlasov <vladislav.vlasov@devexpress.com>
2017-08-28 14:23:52 +0300

    Subscription fixes

--- packages/resolve-redux/src/actions.js
+++ packages/resolve-redux/src/actions.js
@@ -1,6 +1,8 @@
 export const MERGE = '@@resolve/READ_MODEL_STATE_MERGE';
 export const FETCH_MORE = '@@resolve/READ_MODEL_FETCH_MORE';
 export const SEND_COMMAND = '@@resolve/SEND_COMMAND';
+export const SET_SUBSCRIPTION = '@@resolve/SET_SUBSCRIPTION';
+export const REPLACE_STATE = '@@resolve/REPLACE_STATE';
 
 function merge(readModelName, state) {
     return {
@@ -28,8 +30,26 @@ function fetchMore(readModelName, query) {
     };
 }
 
+function setSubscription(eventTypes, aggregateIds) {
+    return {
+        type: SET_SUBSCRIPTION,
+        types: eventTypes,
+        ids: aggregateIds
+    };
+}
+
+function replaceState(readModelName, state) {
+    return {
+        type: REPLACE_STATE,
+        readModelName,
+        state
+    };
+}
+
 export default {
     merge,
     sendCommand,
-    fetchMore
+    fetchMore,
+    setSubscription,
+    replaceState
 };
