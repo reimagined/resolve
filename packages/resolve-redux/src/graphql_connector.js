b02a251d59012c09d9c959e055ec326457261f5e
Vladislav Vlasov <vladislav.vlasov@devexpress.com>
2017-10-10 13:32:39 +0300

    Fix resolve redux

---
+++ packages/resolve-redux/src/graphql_connector.js
@@ -0,0 +1,18 @@
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import React from 'react';
+import { ApolloProvider, graphql, gql } from 'react-apollo';
+
+const networkInterface = createNetworkInterface({ uri: '/api/graphql' });
+const client = new ApolloClient({ networkInterface });
+
+export default (gqlQuery, matchVariables = () => {}) => Component => (props) => {
+    const GraphQLConnector = graphql(gql(gqlQuery), {
+        options: ownProps => ({ variables: matchVariables(ownProps) })
+    })(Component);
+
+    return (
+        <ApolloProvider client={client}>
+            <GraphQLConnector {...props} />
+        </ApolloProvider>
+    );
+};
