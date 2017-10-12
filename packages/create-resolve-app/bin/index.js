6c89e8211ca5fec8ec411fe5a26ec3bb8b0092ed
Pavel Glushenkov <pavel.glushenkov@devexpress.com>
2017-10-12 16:40:56 +0300

    create resolve app fix

--- packages/create-resolve-app/bin/index.js
+++ packages/create-resolve-app/bin/index.js
@@ -3,16 +3,9 @@
 const commandLineArgs = require('command-line-args');
 const moduleCreator = require('../dist/create_resolve_app');
 
-const optionDefinitions = [
-    { name: 'scripts', type: String },
-    { name: 'empty', type: Boolean }
-];
+const optionDefinitions = [{ name: 'scripts', type: String }, { name: 'empty', type: Boolean }];
 
-const options = commandLineArgs(optionDefinitions);
-
-let appName = process.argv[2];
-if(!appName || !appName.indexOf('--scripts') || !appName.indexOf('--empty')) {
-    appName = 'resolve-app';
-}
+const options = commandLineArgs(optionDefinitions, { partial: true });
+let appName = options._unknown[0] || 'resolve-app';
 
 moduleCreator(appName, options.scripts, options.empty);
