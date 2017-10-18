3b8de52f15a992aabe518d281adab72147ac09a3
Vladislav Vlasov <vladislav.vlasov@devexpress.com>
2017-10-18 14:30:35 +0300

    Fix tests

--- packages/resolve-redux/test/graphql_connector.tests.js
+++ packages/resolve-redux/test/graphql_connector.tests.js
@@ -5,7 +5,7 @@ import React from 'react';
 import gqlConnector from '../src/graphql_connector';
 
 describe('graphql connector', () => {
-    it.only('should wrap component into graphql connector', () => {
+    it('should wrap component into graphql connector', () => {
         const WrappedComponent = gqlConnector('query { Test }')('div');
         const { props } = shallow(<WrappedComponent testProp="testValue" />).instance();
         expect(props).to.be.deep.equal({ testProp: 'testValue' });
