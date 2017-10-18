d4941f8e64ff6e39e95a7be4faa26e1a0220a69b
IhostVlad <ihost@mail.ru>
2017-10-18 15:13:25 +0300

    feat(resolve-redux): add support for graphql provider out-of-box

---
+++ packages/resolve-redux/src/graphql_connector.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider, graphql, gql } from 'react-apollo';
+
+export default (gqlQuery, matchVariables = () => ({}), endpoint) => {
+    const client = new ApolloClient({
+        networkInterface: createNetworkInterface({ uri: endpoint || '/api/graphql' })
+    });
+
+    return Component =>
+        function ResolveGraphglConnector(props) {
+            const GraphQLConnector = graphql(gql(gqlQuery), {
+                options: ownProps => ({ variables: matchVariables(ownProps) })
+            })(Component);
+
+            return (
+                <ApolloProvider client={client}>
+                    <GraphQLConnector {...props} />
+                </ApolloProvider>
+            );
+        };
+};
--- packages/resolve-redux/test/index.tests.js
+++ packages/resolve-redux/test/index.tests.js
@@ -1,5 +1,12 @@
 import { expect } from 'chai';
-import { actions, createReducer, createActions, sendCommandMiddleware } from '../src';
+import {
+    actions,
+    createReducer,
+    createActions,
+    gqlConnector,
+    sendCommandMiddleware,
+    setSubscriptionMiddleware
+} from '../src';
 
 describe('resolve-redux', () => {
     it('works the same way for different import types', () => {
@@ -7,6 +14,8 @@ describe('resolve-redux', () => {
         expect(actions).to.be.equal(importedModule.actions);
         expect(createReducer).to.be.equal(importedModule.createReducer);
         expect(createActions).to.be.equal(importedModule.createActions);
+        expect(gqlConnector).to.be.equal(importedModule.gqlConnector);
         expect(sendCommandMiddleware).to.be.equal(importedModule.sendCommandMiddleware);
+        expect(setSubscriptionMiddleware).to.be.equal(importedModule.setSubscriptionMiddleware);
     });
 });
