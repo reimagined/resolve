79421f83fb224047325194b71b53be868f54b6f8
mrcheater <slayerx@narod.ru>
2017-11-24 18:18:12 +0300

    Refactor resolveMiddleware

--- packages/resolve-redux/src/resolve_middleware.js
+++ packages/resolve-redux/src/resolve_middleware.js
@@ -5,26 +5,182 @@ import { getRootableUrl, getKey, checkRequiredFields } from './util';
 import fetch from 'isomorphic-fetch';
 
 const CRITICAL_LEVEL = 100;
+const REFRESH_TIMEOUT = 1000;
 
-const sendCommand = async (command) => {
-    const response = await fetch(getRootableUrl('/api/commands'), {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        credentials: 'same-origin',
-        body: JSON.stringify(command)
-    });
+export async function sendCommand(store, action) {
+    const { command, aggregateId, aggregateName, payload } = action;
 
-    if (response.ok) {
-        return response.blob();
+    if (
+        !(
+            command &&
+            checkRequiredFields(
+                { aggregateId, aggregateName },
+                'Send command error:',
+                JSON.stringify(action)
+            ) &&
+            !command.error
+        )
+    ) {
+        return;
     }
 
-    const text = await response.text();
-    // eslint-disable-next-line no-console
-    console.error('Send command error:', text);
-    return Promise.reject(text);
-};
+    const normalizedCommand = {
+        type: command.type,
+        aggregateId,
+        aggregateName,
+        payload
+    };
 
-function createMiddleware(viewModels) {
+    try {
+        const response = await fetch(getRootableUrl('/api/commands'), {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'same-origin',
+            body: JSON.stringify(normalizedCommand)
+        });
+
+        if (response.ok) {
+            return response.blob();
+        }
+
+        const text = await response.text();
+        // eslint-disable-next-line no-console
+        console.error('Send command error:', text);
+        throw new Error(text);
+    } catch (error) {
+        store.dispatch({
+            ...action,
+            command: {
+                ...action.command,
+                error
+            }
+        });
+    }
+}
+
+export async function getViewModelRawState(viewModel, aggregateId) {
+    const response = await fetch(
+        getRootableUrl(
+            `/api/query/${viewModel}?aggregateIds${aggregateId === '*' ? '' : '[]'}=${aggregateId}`
+        ),
+        {
+            method: 'GET',
+            credentials: 'same-origin'
+        }
+    );
+
+    if (!response.ok) {
+        throw new Error(response.text());
+    }
+
+    return await response.json();
+}
+
+export function getEventTypes(viewModels, subscribers) {
+    const eventTypes = {};
+
+    Object.keys(subscribers.viewModels).forEach((viewModel) => {
+        if (!subscribers.viewModels[viewModel]) {
+            return;
+        }
+
+        const { Init, ...projection } = viewModels.find(
+            ({ name }) => name === viewModel
+        ).projection;
+
+        Object.keys(projection).forEach((eventType) => {
+            eventTypes[eventType] = true;
+        });
+    });
+
+    return Object.keys(eventTypes);
+}
+
+export function getAggregateIds(viewModels, subscribers) {
+    if (subscribers.aggregateIds['*'] > 0) {
+        return '*';
+    }
+
+    return Object.keys(subscribers.aggregateIds).filter(
+        aggregateId => subscribers.aggregateIds[aggregateId]
+    );
+}
+
+export function initSocketIO(store, socket = { failCount: 0, io: null }) {
+    socket.io = socketIOClient(window.location.origin, {
+        path: getRootableUrl('/socket/')
+    });
+
+    socket.io.on('event', event => store.dispatch(JSON.parse(event)));
+
+    socket.io.on('disconnect', () => {
+        socket.failCount++;
+        if (socket.failCount > CRITICAL_LEVEL) {
+            window.location.reload();
+        }
+        initSocketIO(store, socket);
+    });
+
+    return socket;
+}
+
+export async function subscribe(store, socket, viewModels, subscribers, requests, action) {
+    const { viewModel, aggregateId } = action;
+
+    const needChange = !subscribers.viewModels[viewModel] || !subscribers.aggregateIds[aggregateId];
+
+    subscribers.viewModels[viewModel] = (subscribers.viewModels[viewModel] || 0) + 1;
+    subscribers.aggregateIds[aggregateId] = (subscribers.aggregateIds[aggregateId] || 0) + 1;
+
+    if (needChange) {
+        const key = getKey(viewModel, aggregateId);
+        requests[key] = true;
+
+        const rawState = await getViewModelRawState(viewModel, aggregateId);
+
+        const state = viewModels.find(({ name }) => name === viewModel).deserializeState(rawState);
+
+        if (requests[key]) {
+            delete requests[key];
+
+            store.dispatch({
+                type: MERGE_STATE,
+                aggregateId,
+                viewModel,
+                state
+            });
+
+            socket.io.emit('setSubscription', {
+                types: getEventTypes(viewModels, subscribers),
+                ids: getAggregateIds(viewModels, subscribers)
+            });
+        }
+    }
+}
+
+export function unsubscribe(store, socket, viewModels, subscribers, requests, action) {
+    const { viewModel, aggregateId } = action;
+
+    subscribers.viewModels[viewModel] = Math.max((subscribers.viewModels[viewModel] || 0) - 1, 0);
+    subscribers.aggregateIds[aggregateId] = Math.max(
+        (subscribers.aggregateIds[aggregateId] || 0) - 1,
+        0
+    );
+
+    const needChange = !subscribers.viewModels[viewModel] || !subscribers.aggregateIds[aggregateId];
+
+    const key = getKey(viewModel, aggregateId);
+    delete requests[key];
+
+    if (needChange) {
+        socket.io.emit('setSubscription', {
+            types: getEventTypes(viewModels, subscribers),
+            ids: getAggregateIds(viewModels, subscribers)
+        });
+    }
+}
+
+export function createMiddleware(viewModels) {
     const subscribers = {
         viewModels: {},
         aggregateIds: {}
@@ -32,185 +188,25 @@ function createMiddleware(viewModels) {
 
     const requests = {};
 
-    function getEventTypes() {
-        const eventTypes = {};
-
-        Object.keys(subscribers.viewModels).forEach((viewModel) => {
-            if (!subscribers.viewModels[viewModel]) {
-                return;
-            }
-
-            const { Init, ...projection } = viewModels.find(
-                ({ name }) => name === viewModel
-            ).projection;
-
-            Object.keys(projection).forEach((eventType) => {
-                eventTypes[eventType] = true;
-            });
-        });
-
-        return Object.keys(eventTypes);
-    }
-
-    function getAggregateIds() {
-        if (subscribers.aggregateIds['*'] > 0) {
-            return '*';
-        }
-
-        const aggregateIds = [];
-
-        Object.keys(subscribers.aggregateIds).forEach((aggregateId) => {
-            if (!subscribers.aggregateIds[aggregateId]) {
-                return;
-            }
-            aggregateIds.push(aggregateId);
-        });
-
-        return aggregateIds;
-    }
-
     return (store) => {
         store.dispatch(actions.provideViewModels(viewModels));
 
-        let socketIOFailCount = 0;
-        let socketIO = null;
-
-        const initSocketIO = () => {
-            socketIO = socketIOClient(window.location.origin, {
-                path: getRootableUrl('/socket/')
-            });
-
-            socketIO.on('event', event => store.dispatch(JSON.parse(event)));
-
-            socketIO.on('disconnect', () => {
-                socketIOFailCount++;
-                if (socketIOFailCount > CRITICAL_LEVEL) {
-                    window.location.reload();
-                }
-                initSocketIO();
-            });
-        };
-
-        initSocketIO();
+        const socket = initSocketIO(store);
 
         return next => (action) => {
             switch (action.type) {
                 case SUBSCRIBE: {
-                    const { viewModel, aggregateId } = action;
-
-                    const needChange =
-                        !subscribers.viewModels[viewModel] ||
-                        !subscribers.aggregateIds[aggregateId];
-
-                    subscribers.viewModels[viewModel] =
-                        (subscribers.viewModels[viewModel] || 0) + 1;
-                    subscribers.aggregateIds[aggregateId] =
-                        (subscribers.aggregateIds[aggregateId] || 0) + 1;
-
-                    if (needChange) {
-                        const key = getKey(viewModel, aggregateId);
-                        requests[key] = true;
-
-                        fetch(
-                            getRootableUrl(
-                                `/api/query/${viewModel}?aggregateIds${aggregateId === '*'
-                                    ? ''
-                                    : '[]'}=${aggregateId}`
-                            ),
-                            {
-                                method: 'GET',
-                                credentials: 'same-origin'
-                            }
-                        )
-                            .then((response) => {
-                                if (response.ok) {
-                                    return response.json();
-                                }
-                                throw new Error(response.text());
-                            })
-                            .then((rawState) => {
-                                const state = viewModels
-                                    .find(({ name }) => name === viewModel)
-                                    .deserializeState(rawState);
-
-                                if (requests[key]) {
-                                    delete requests[key];
-
-                                    store.dispatch({
-                                        type: MERGE_STATE,
-                                        aggregateId,
-                                        viewModel,
-                                        state
-                                    });
-
-                                    socketIO.emit('setSubscription', {
-                                        types: getEventTypes(),
-                                        ids: getAggregateIds()
-                                    });
-                                }
-                            });
-                    }
-
+                    subscribe(store, socket, viewModels, subscribers, requests, action).catch(() =>
+                        setTimeout(() => store.dispatch(action), REFRESH_TIMEOUT)
+                    );
                     break;
                 }
                 case UNSUBSCRIBE: {
-                    const { viewModel, aggregateId } = action;
-
-                    subscribers.viewModels[viewModel] = Math.max(
-                        (subscribers.viewModels[viewModel] || 0) - 1,
-                        0
-                    );
-                    subscribers.aggregateIds[aggregateId] = Math.max(
-                        (subscribers.aggregateIds[aggregateId] || 0) - 1,
-                        0
-                    );
-
-                    const needChange =
-                        !subscribers.viewModels[viewModel] ||
-                        !subscribers.aggregateIds[aggregateId];
-
-                    const key = getKey(viewModel, aggregateId);
-                    delete requests[key];
-
-                    if (needChange) {
-                        socketIO.emit('setSubscription', {
-                            types: getEventTypes(),
-                            ids: getAggregateIds()
-                        });
-                    }
-
+                    unsubscribe(store, socket, viewModels, subscribers, requests, action);
                     break;
                 }
                 case SEND_COMMAND: {
-                    const { command, aggregateId, aggregateName, payload } = action;
-
-                    if (
-                        command &&
-                        checkRequiredFields(
-                            { aggregateId, aggregateName },
-                            'Send command error:',
-                            JSON.stringify(action)
-                        ) &&
-                        !command.error
-                    ) {
-                        const normalizedCommand = {
-                            type: command.type,
-                            aggregateId,
-                            aggregateName,
-                            payload
-                        };
-
-                        sendCommand(normalizedCommand, store.dispatch).catch((error) => {
-                            store.dispatch({
-                                ...action,
-                                command: {
-                                    ...action.command,
-                                    error
-                                }
-                            });
-                        });
-                    }
-
+                    sendCommand(store, action);
                     break;
                 }
                 default:
