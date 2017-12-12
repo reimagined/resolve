1f312fb8be463f7fb7b74d918c14f0e65daef491
Vladislav Vlasov <vladislav.vlasov@devexpress.com>
2017-12-12 14:13:48 +0300

    Refactor adapter

--- packages/readmodel-adapters/resolve-readmodel-mongo/test/index.tests.js
+++ packages/readmodel-adapters/resolve-readmodel-mongo/test/index.tests.js
@@ -1,91 +1,63 @@
 import 'regenerator-runtime/runtime';
 
 import { expect } from 'chai';
+import mongoUnit from 'mongo-unit';
+import { MongoClient } from 'mongodb';
 import sinon from 'sinon';
 
 import buildProjection from '../src/init';
 import init from '../src/init';
 import reset from '../src/init';
 
-process.on('unhandledRejection', (...args) => console.log(...args));
-
 describe('Read model MongoDB adapter', () => {
-    let fakeLastReadResult;
+    const DEFAULT_COLLECTION_NAME = 'TestDefaultCollection';
+    const META_COLLECTION_NAME = 'TestMetaCollection';
     let fakeRepository;
-    let fakeDatabase;
+    let fakeConnection;
 
-    beforeEach(() => {
-        fakeDatabase = {
-            _collections: {},
-            listCollections: sinon.stub().callsFake(({ name: collectionName }) => ({
-                toArray: sinon
-                    .stub()
-                    .callsFake(async () =>
-                        Object.keys(fakeDatabase._collections).filter(
-                            name => name.indexOf(collectionName) > -1
-                        )
-                    )
-            })),
-            collection: sinon.stub().callsFake(async (collectionName) => {
-                if (!fakeDatabase._collections[collectionName]) {
-                    fakeDatabase._collections[collectionName] = {
-                        find: sinon.stub().callsFake(() => {
-                            const findApi = {
-                                skip: sinon.stub().callsFake(() => {
-                                    return findApi;
-                                }),
-                                limit: sinon.stub().callsFake(() => {
-                                    return findApi;
-                                }),
-                                toArray: sinon.stub().callsFake(async () => {
-                                    return fakeLastReadResult;
-                                })
-                            };
-                            return findApi;
-                        }),
-                        findOne: sinon.stub().callsFake(async () => {
-                            return fakeLastReadResult;
-                        }),
-                        count: sinon.stub().callsFake(async () => {
-                            return fakeLastReadResult;
-                        }),
-                        createIndex: sinon.spy(),
-                        dropIndex: sinon.spy(),
-                        insert: sinon.spy(),
-                        update: sinon.spy(),
-                        remove: sinon.spy()
-                    };
-                }
-
-                return fakeDatabase._collections[collectionName];
-            })
-        };
-
-        fakeRepository = {
-            metaCollectionName: 'TestMetaCollection',
-            connectDatabase: async () => fakeDatabase
-        };
+    before(async function () {
+        this.timeout(0);
+        const connectionUrl = await mongoUnit.start();
+        fakeConnection = await MongoClient.connect(connectionUrl);
     });
 
-    afterEach(() => {
-        fakeLastReadResult = null;
+    after(async () => {
+        await mongoUnit.drop();
+        fakeConnection = null;
+    });
+
+    beforeEach(async () => {
+        fakeRepository = {
+            connectDatabase: async () => fakeConnection,
+            metaCollectionName: META_COLLECTION_NAME
+        };
+
+        await mongoUnit.load({
+            [DEFAULT_COLLECTION_NAME]: [
+                { id: 0, content: 'test-0', ts: 10 },
+                { id: 1, content: 'test-1', ts: 20 }
+            ],
+            [META_COLLECTION_NAME]: [
+                {
+                    collectionName: DEFAULT_COLLECTION_NAME,
+                    lastTimestamp: 20,
+                    indexes: ['id']
+                }
+            ]
+        });
+    });
+
+    afterEach(async () => {
+        //await mongoUnit.clean();
         fakeRepository = null;
-        fakeDatabase = null;
     });
 
     describe('Build Projection function', () => {});
 
     describe('Init function', () => {
-        let readInstance = null;
-        beforeEach(() => {
-            fakeLastReadResult = [];
-            readInstance = init(fakeRepository);
-        });
-        afterEach(() => {
-            readInstance = null;
-        });
-
         it('should fill repository with internal fields', () => {
+            init(fakeRepository);
+
             expect(fakeRepository.lastTimestamp).to.be.equal(0);
             expect(fakeRepository.initHandler).to.be.an.instanceof(Function);
             expect(fakeRepository.connectionPromise).to.be.an.instanceof(Promise);
@@ -97,14 +69,45 @@ describe('Read model MongoDB adapter', () => {
         });
 
         it('should provide proper read-side interface', async () => {
-            await fakeRepository.connectionPromise;
+            const readInstance = init(fakeRepository);
             const lastTimestamp = await readInstance.getLastAppliedTimestamp();
             const readable = await readInstance.getReadable();
             const lastError = await readInstance.getError();
 
-            expect(lastTimestamp).to.be.equal(1);
+            expect(lastTimestamp).to.be.equal(20);
             expect(readable).to.be.equal(fakeRepository.readInterface);
             expect(lastError).to.be.equal(null);
+        });
+
+        it('should provide collection API on read-side on existing collections', async () => {
+            const readInstance = init(fakeRepository);
+            const readable = await readInstance.getReadable();
+            const collection = await readable.collection(DEFAULT_COLLECTION_NAME);
+
+            expect(collection).to.be.an.instanceof(Object);
+            expect(collection.find).to.be.an.instanceof(Function);
+            expect(collection.findOne).to.be.an.instanceof(Function);
+            expect(collection.findCount).to.be.an.instanceof(Function);
+        });
+
+        it('should throw error on read-side on non-existing collections', async () => {
+            const readInstance = init(fakeRepository);
+            const readable = await readInstance.getReadable();
+
+            try {
+                await readable.collection('wrong');
+                return Promise.reject('Unexisting collection call should throw error on read side');
+            } catch (err) {
+                expect(err.message).to.be.equal('Collection wrong does not exist');
+            }
+        });
+
+        it('should provide actual collections list on read-side', async () => {
+            const readInstance = init(fakeRepository);
+            const readable = await readInstance.getReadable();
+            const collectionsList = await readable.listCollections();
+
+            expect(collectionsList).to.be.deep.equal('default');
         });
     });
 
