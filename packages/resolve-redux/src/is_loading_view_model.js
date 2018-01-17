24bbb53661c63998d0675ebe50042fe5a0eba39a
mrcheater <slayerx@narod.ru>
2018-01-17 14:16:54 +0300

    Fix isLoadingViewModel

--- packages/resolve-redux/src/is_loading_view_model.js
+++ packages/resolve-redux/src/is_loading_view_model.js
@@ -1,3 +1,3 @@
 export default function isLoadingViewModel(store, viewModelName, aggregateId) {
-    return store.getState.isLoadingViewModel(store, viewModelName, aggregateId);
+    return store.getState.isLoadingViewModel(viewModelName, aggregateId);
 }
