7b88aecb2737eebc81f4531fbc7d80f57f08741f
kilimondjaro <kirill.babich@mail.ru>
2018-01-18 14:27:03 +0300

    Fix graphql_connector

--- packages/resolve-redux/src/graphql_connector.js
+++ packages/resolve-redux/src/graphql_connector.js
@@ -3,6 +3,7 @@ import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider, graphql } from 'react-apollo';
+import fetch from 'isomorphic-fetch';
 import gql from 'graphql-tag';
 import { getRootableUrl } from './util';
 
@@ -10,7 +11,8 @@ export default (gqlQuery, options = {}, endpoint) => {
     const client = new ApolloClient({
         link: new HttpLink({
             uri: endpoint || getRootableUrl('/api/query/graphql'),
-            credentials: 'same-origin'
+            credentials: 'same-origin',
+            fetch
         }),
         cache: new InMemoryCache()
     });
