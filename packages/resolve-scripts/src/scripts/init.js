9ca3c396fca2bfd1bd1e61e3f924c9ba081aa381
mrcheater <slayerx@narod.ru>
2018-01-23 11:45:49 +0300

    Fix resolve-scripts init

--- packages/resolve-scripts/src/scripts/init.js
+++ packages/resolve-scripts/src/scripts/init.js
@@ -8,7 +8,7 @@ const log = console.log;
 // eslint-disable-next-line no-console
 const error = console.error;
 
-const dependencies = ['react', 'react-dom', 'react-redux', 'redux'];
+const dependencies = ['react@^16.0.0', 'react-dom@^16.0.0', 'react-redux', 'redux'];
 
 const resolveDependencies = ['resolve-bus-memory', 'resolve-redux', 'resolve-storage-lite'];
 
