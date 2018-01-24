caafd8b8cd8528d4c02e8a7a36af2ddbc503afe4
vasiliy <29576033+Vasiliy-Lavrov@users.noreply.github.com>
2018-01-24 12:32:26 +0300

    change prettier options

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,55 +1,38 @@
 module.exports = {
-    env: {
-        node: true,
-        jest: true,
-        es6: true
-    },
-    extends: 'react-app',
-    parserOptions: {
-        ecmaFeature: {
-            jsx: true
-        }
-    },
-    plugins: ['react', 'jsx-a11y', 'import'],
-    rules: {
-        indent: ['error', 4, { SwitchCase: 1 }],
-        'func-names': 'off',
-        'no-underscore-dangle': 'off',
-        'import/no-unresolved': 'off',
-        'comma-dangle': ['error', 'never'],
-        'no-plusplus': 'off',
-        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-        'no-param-reassign': ['error', { props: false }],
-        'new-cap': ['error', { capIsNew: false }],
-        'no-mixed-operators': [
-            'error',
-            {
-                allowSamePrecedence: true
-            }
-        ],
-        'object-curly-spacing': ['error', 'always'],
-        'arrow-parens': [
-            'error',
-            'as-needed',
-            {
-                requireForBlockBody: true
-            }
-        ],
-        'max-len': [
-            'error',
-            {
-                code: 100
-            }
-        ],
-        'space-before-function-paren': [
-            'error',
-            {
-                anonymous: 'always',
-                named: 'never',
-                asyncArrow: 'always'
-            }
-        ],
-        quotes: ['error', 'single'],
-        'no-console': ['error']
+  env: {
+    node: true,
+    jest: true,
+    es6: true
+  },
+  extends: 'react-app',
+  parserOptions: {
+    ecmaFeature: {
+      jsx: true
     }
-};
+  },
+  plugins: ['react', 'jsx-a11y', 'import'],
+  rules: {
+    'func-names': 'off',
+    'no-underscore-dangle': 'off',
+    'import/no-unresolved': 'off',
+    'comma-dangle': ['error', 'never'],
+    'no-plusplus': 'off',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'no-param-reassign': ['error', { props: false }],
+    'new-cap': ['error', { capIsNew: false }],
+    'no-mixed-operators': [
+      'error',
+      {
+        allowSamePrecedence: true
+      }
+    ],
+    'object-curly-spacing': ['error', 'always'],
+    'max-len': [
+      'error',
+      {
+        code: 100
+      }
+    ],
+    'no-console': ['error']
+  }
+}
