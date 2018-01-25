5d372d823e040331c745337868c416824f08b345
Pavel Glushenkov <pavel.glushenkov@devexpress.com>
2018-01-25 12:57:08 +0300

    remove router examples

--- examples/resolve-scripts-with-router-2/resolve.client.config.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import rootRoute from './index'
-import { Router, browserHistory } from 'react-router'
-
-export default {
-  rootComponent: () => <Router history={browserHistory}>{rootRoute}</Router>
-}
