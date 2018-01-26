4c899dd527627c453442789cbde2a2099d72f748
Pavel Glushenkov <pavel.glushenkov@devexpress.com>
2018-01-26 11:04:22 +0300

    removef entries from server config

--- examples/todo-two-levels/resolve.server.config.js
+++ examples/todo-two-levels/resolve.server.config.js
@@ -1,8 +1,6 @@
-import React from 'react'
 import path from 'path'
 import fileAdapter from 'resolve-storage-lite'
 import busAdapter from 'resolve-bus-memory'
-import { StaticRouter } from 'react-router'
 
 import aggregates from './common/aggregates'
 import readModels from './common/read-models'
