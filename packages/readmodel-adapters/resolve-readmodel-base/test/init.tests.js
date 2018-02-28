06bb70face73ccc408aa7e24477d149fbaa90fa6
Vladislav Vlasov <vladislav.vlasov@devexpress.com>
2018-02-28 15:37:36 +0300

    Add tests for resolve-readmodel-base for init phase

--- packages/readmodel-adapters/resolve-readmodel-base/test/init.tests.js
+++ packages/readmodel-adapters/resolve-readmodel-base/test/init.tests.js
@@ -5,7 +5,7 @@ import init from '../src/init'
 import messages from '../src/messages'
 
 describe('resolve-readmodel-base init', () => {
-  it('should work properly - with custom init handler', async () => {
+  it('should work properly - with custom normal init handler', async () => {
     let currentTimestamp = 0
     const metaApi = {
       getLastTimestamp: sinon.stub().callsFake(async () => currentTimestamp),
@@ -54,6 +54,75 @@ describe('resolve-readmodel-base init', () => {
     expect(await getLastAppliedTimestamp()).to.be.equal(1)
   })
 
+  it('should work properly - with custom normal init handler on non-zero timestamp', async () => {
+    let currentTimestamp = 100
+    const metaApi = {
+      getLastTimestamp: sinon.stub().callsFake(async () => currentTimestamp),
+      setLastTimestamp: sinon.stub().callsFake(async ts => (currentTimestamp = ts))
+    }
+    const storeApi = {
+      find: sinon.stub(),
+      mutate: sinon.stub()
+    }
+    const internalContext = {
+      initHandler: sinon.stub()
+    }
+
+    const { getLastAppliedTimestamp, getReadable, getError } = init({
+      metaApi,
+      storeApi,
+      internalContext
+    })
+
+    const lastTimestamp = currentTimestamp
+    expect(internalContext.initHandler.callCount).to.be.equal(0)
+    expect(await getLastAppliedTimestamp()).to.be.equal(lastTimestamp)
+
+    const readInterface = await getReadable()
+    expect(internalContext.initHandler.callCount).to.be.equal(0)
+    expect(readInterface.find).to.be.equal(storeApi.find)
+
+    try {
+      await readInterface.mutate()
+      return Promise.reject('Mutation operator should be rejection on read side')
+    } catch (err) {
+      expect(err).to.be.instanceOf(Error)
+      expect(err.message).to.be.equal(messages.readSideForbiddenOperation('mutate'))
+    }
+
+    const lastError = await getError()
+    expect(lastError).to.be.equal(null)
+
+    expect(await metaApi.getLastTimestamp.firstCall.returnValue).to.be.equal(100)
+
+    expect(await getLastAppliedTimestamp()).to.be.equal(100)
+  })
+
+  it('should work properly - with custom failed init handler', async () => {
+    let currentTimestamp = 0
+    const metaApi = {
+      getLastTimestamp: sinon.stub().callsFake(async () => currentTimestamp),
+      setLastTimestamp: sinon.stub().callsFake(async ts => (currentTimestamp = ts))
+    }
+    const storeApi = {
+      find: sinon.stub(),
+      mutate: sinon.stub()
+    }
+    const internalContext = {
+      initHandler: sinon.stub().throws('ERR')
+    }
+
+    const { getError } = init({
+      metaApi,
+      storeApi,
+      internalContext
+    })
+
+    const lastError = await getError()
+    expect(lastError).to.be.instanceOf(Error)
+    expect(lastError.name).to.be.equal('ERR')
+  })
+
   it('should work properly - with default init handler', async () => {
     let currentTimestamp = 0
     const metaApi = {
@@ -97,6 +166,37 @@ describe('resolve-readmodel-base init', () => {
     expect(await metaApi.setLastTimestamp.firstCall.args[0]).to.be.equal(1)
 
     expect(await getLastAppliedTimestamp()).to.be.equal(1)
+  })
+
+  it('should translate meta-api failure to read api functions', async () => {
+    const metaApi = { getLastTimestamp: sinon.stub().throws('ERR') }
+    const storeApi = {
+      find: sinon.stub(),
+      mutate: sinon.stub()
+    }
+    const internalContext = {}
+
+    const { getReadable, getError } = init({
+      metaApi,
+      storeApi,
+      internalContext
+    })
+
+    try {
+      await getReadable()
+      return Promise.reject('Init should translate meta-api failure to read api function')
+    } catch (err) {
+      expect(err).to.be.instanceOf(Error)
+      expect(err.name).to.be.equal('ERR')
+    }
+
+    try {
+      await getError()
+      return Promise.reject('Init should translate meta-api failure to read api function')
+    } catch (err) {
+      expect(err).to.be.instanceOf(Error)
+      expect(err.name).to.be.equal('ERR')
+    }
   })
 
   it('should not allow reinitialization', async () => {
