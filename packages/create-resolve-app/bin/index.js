2fcfdf2cb4708bdb6a3dbf13926058dabc9b3f0b
alexander.ralkov <alexander.ralkov@devexpress.com>
2018-03-13 19:13:22 +0300

    remove git clone beautify logs

--- packages/create-resolve-app/bin/index.js
+++ packages/create-resolve-app/bin/index.js
@@ -1,11 +1,19 @@
 #!/usr/bin/env node
 
+const AdmZip = require('adm-zip')
 const commandLineArgs = require('command-line-args')
 const chalk = require('chalk')
-const { exec } = require('child_process')
+const fs = require('fs')
+const https = require('https')
+const replace = require('replace')
+const spawn = require('cross-spawn')
+const validateProjectName = require('validate-npm-package-name')
 
 // eslint-disable-next-line no-console
 const log = console.log
+
+// eslint-disable-next-line no-console
+const error = console.error
 
 const EOL = require('os').EOL
 
@@ -57,6 +65,11 @@ const messages = {
     EOL +
     `Run ${chalk.cyan('create-resolve-app --help')} to see all options.`,
 
+  startCreatingApp: (appName, example, options) =>
+    `Creating ${appName} in ./${appName} based on ${example} example` +
+    (options.commit ? ` (commit SHA:${options.commit})` : '') +
+    (options.branch ? ` (from ${options.branch} branch)` : ''),
+
   unknownOptions: options =>
     `You have specified an unsupported option(s): ${chalk.red(options)}${EOL}` +
     EOL +
@@ -81,32 +94,120 @@ if (unknownOptions && unknownOptions.length) {
 } else {
   let appName = options._unknown[0],
     example = options.example || 'hello-world',
-    resolveRepoPath = 'https://github.com/reimagined/resolve.git',
-    examplePath = './resolve/examples/' + example
+    branch = options.branch ? options.branch : 'master',
+    examplePath = `./resolve-${branch}/examples/${example}`,
+    resolveRepoPath = `https://codeload.github.com/reimagined/resolve/zip/${branch}`,
+    tmpFilePath = `./resolve-${branch}.zip`
 
-  log(
-    `Creating ${appName} in ./${appName} based on ${example} example` +
-      (options.commit ? ` (commit SHA:${options.commit})` : '')
-  )
+  const startCreatingApp = () =>
+    Promise.resolve(() =>
+      log(messages.startCreatingApp(appName, example, options))
+    )
 
-  let branchChangeOption = options.branch ? `--branch ${options.branch}` : '',
-    command =
-      `mkdir ${appName} && cd ${appName} ` +
-      `&& git clone ${resolveRepoPath} ${branchChangeOption}` +
-      (options.commit
-        ? ` && cd resolve && git checkout ${options.commit} && cd ..`
-        : '') +
-      ` && cp -r ${examplePath}/* ./ && cp -r ${examplePath}/.[^.]* ./ ` +
-      ` && rm -rf ./resolve && npm i >> log.log`
-
-  exec(command, (err, stdout, stderr) => {
-    if (err) {
-      log(err)
-      return
+  const checkAppName = () => {
+    const result = validateProjectName(appName)
+    if (!result.validForNewPackages) {
+      error(
+        `It is impossible to create an application called ${chalk.red(
+          `"${appName}"`
+        )} because of npm naming restrictions:`
+      )
+      ;[]
+        .concat(result.errors || [])
+        .concat(result.warnings || [])
+        .forEach(error => {
+          error(chalk.red(`  *  ${error}`))
+        })
     }
 
-    // the *entire* stdout and stderr (buffered)
-    log(`stdout: ${stdout}`)
-    log(`stderr: ${stderr}`)
-  })
+    return result.validForNewPackages
+  }
+
+  const downloadRepo = () =>
+    new Promise(resolve => {
+      log(chalk.green('Load example'))
+      https.get(resolveRepoPath, function(response) {
+        response.on('data', function(data) {
+          fs.appendFileSync(tmpFilePath, data)
+        })
+
+        response.on('end', function() {
+          var zip = new AdmZip(tmpFilePath)
+          zip.extractAllTo(`./${appName}`)
+          fs.unlink(tmpFilePath)
+          resolve()
+        })
+      })
+    })
+
+  const copyExample = () => {
+    log()
+    log(chalk.green('Copy example && install dependencies'))
+    let command =
+      `cd ${appName} ` +
+      ` && cp -r ${examplePath}/* . && cp -r ${examplePath}/.[a-zA-Z0-9]* .` +
+      ` && rm -rf ./resolve-${branch} && npm i`
+
+    const proc = spawn.sync(command, [], { stdio: 'inherit', shell: true })
+    if (proc.status !== 0) {
+      throw Error(`\`${command}\` failed`)
+    }
+  }
+
+  const patchPackageJson = () =>
+    replace({
+      regex: /^(\s*"name":\s*)".*$/,
+      replacement: /\1"${appName}"/,
+      paths: [`./${appName}/package.json`],
+      recursive: false,
+      silent: false
+    })
+
+  const printFinishOutput = () => {
+    const displayCommand = (isDefaultCmd = false) =>
+      isDefaultCmd ? 'npm' : 'npm run'
+
+    log()
+    log(`Success! ${appName} is created `)
+    log('In that directory, you can run the following commands:')
+
+    log()
+    log(chalk.cyan(`  ${displayCommand()} dev`))
+    log('    Starts the development server.')
+
+    log()
+    log(chalk.cyan(`  ${displayCommand()} test`))
+    log('    Starts the test runner.')
+
+    log()
+    log(chalk.cyan(`  ${displayCommand()} test:e2e`))
+    log('    Starts the functionality test runner.')
+
+    log()
+    log(chalk.cyan(`  ${displayCommand()} build`))
+    log('    Bundles the app into static files for production.')
+
+    log()
+    log(chalk.cyan(`  ${displayCommand(1)} start`))
+    log(
+      '    Starts the production server. (run ' +
+        `  ${chalk.cyan(`${displayCommand(false)} build`)} before)`
+    )
+
+    log()
+    log('We suggest that you begin by typing:')
+    log()
+    log(chalk.cyan('  cd'), `./${appName}`)
+    log(`  ${chalk.cyan(`${displayCommand(false)} dev`)}`)
+    log()
+    log('Happy coding!')
+  }
+
+  startCreatingApp()
+    .then(checkAppName)
+    .then(downloadRepo)
+    .then(copyExample)
+    .then(patchPackageJson)
+    .then(printFinishOutput)
+    .catch(e => log(chalk.red(e)))
 }
