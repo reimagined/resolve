a1aa8e50313058b98c697dc3565bbd5034ab715d
TatyanaRyzh <tatyana.ryzhenkova@devexpress.com>
2018-03-27 18:10:29 +0300

    Make hello-world example awesome

--- examples/hello-world/client/components/App.js
+++ examples/hello-world/client/components/App.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
+import Header from './Header.js'
 
 const RootComponent = () => (
   <div>
-    <h1>Hello, world!</h1>
+    <Helmet>
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+        crossorigin="anonymous"
+      />
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+        integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
+        crossorigin="anonymous"
+      />
+    </Helmet>
+
+    <Header />
+
+    <h1 align="center">Hello, reSolve world!</h1>
   </div>
 )
 
