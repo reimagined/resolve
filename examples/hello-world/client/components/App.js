041cb2b1e77293a01e18b1614aa0d61313f3ccba
TatyanaRyzh <tatyana.ryzhenkova@devexpress.com>
2018-03-27 18:36:31 +0300

    Delete redundant link

--- examples/hello-world/client/components/App.js
+++ examples/hello-world/client/components/App.js
@@ -11,12 +11,6 @@ const RootComponent = () => (
         integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
         crossorigin="anonymous"
       />
-      <link
-        rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
-        integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
-        crossorigin="anonymous"
-      />
     </Helmet>
 
     <Header />
