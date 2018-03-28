71f430a733528d3c97b6336cadd1a58ce5a5f406
TatyanaRyzh <tatyana.ryzhenkova@devexpress.com>
2018-03-28 17:31:55 +0300

    Add title for hello-world

--- examples/hello-world/client/components/App.js
+++ examples/hello-world/client/components/App.js
@@ -11,6 +11,7 @@ const RootComponent = () => (
         integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
         crossorigin="anonymous"
       />
+      <title>reSolve Hello World</title>
     </Helmet>
 
     <Header />
