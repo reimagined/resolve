274d324478f4242b624df4b5be22c88b17cf4f16
Vladislav Vlasov <vladislav.vlasov@devexpress.com>
2018-04-10 15:11:04 +0300

    Fix JWT production mode condition

--- examples/auth/auth/jwtSecret.js
+++ examples/auth/auth/jwtSecret.js
@@ -1,11 +1,10 @@
-let jwtSecret =
-  process.env.JWT_SECRET ||
-  (process.env.NODE_ENV === 'development' && 'SECRETJWT')
-
-if (!jwtSecret) {
+if (
+  process.env.NODE_ENV === 'production' &&
+  !process.env.hasOwnProperty('JWT_SECRET')
+) {
   throw new Error(
     'In production mode you must specify jwt secret key in JWT_SECRET environment variable'
   )
 }
 
-export default jwtSecret
+export default process.env.JWT_SECRET || 'SECRETJWT'
