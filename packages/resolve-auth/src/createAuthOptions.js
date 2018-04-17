a3cfc0a3b61aa933d2c4a1d7376266270b0529a8
mrcheater <slayerx@narod.ru>
2018-04-17 11:28:38 +0300

    Add getRootableUrl to local strategy / Fix infinite waiting in onFail

--- packages/resolve-auth/src/createAuthOptions.js
+++ packages/resolve-auth/src/createAuthOptions.js
@@ -1,14 +1,25 @@
+import { getRootableUrl } from './helpers'
+
 export default (authReq, authRes, next) => ({
   onSuccess: (options, user /*, info*/) => {
-    authRes.applyJwtValue(user, authRes.expressRes, options.successRedirect)
+    authRes.applyJwtValue(
+      user,
+      authRes.expressRes,
+      getRootableUrl(options.successRedirect || '/')
+    )
   },
-  onFail: (options /*, challenge , status*/) => {
+  onFail: (options, error, status) => {
+    const res = authRes.expressRes
     if (options.failureRedirect) {
-      const res = authRes.expressRes
+      // eslint-disable-next-line
+      console.warn(error)
       res.statusCode = 302
-      res.setHeader('Location', options.failureRedirect)
+      res.setHeader('Location', getRootableUrl(options.failureRedirect))
       res.setHeader('Content-Length', '0')
       res.end()
+    } else {
+      res.statusCode = status
+      res.end(JSON.stringify(error))
     }
   },
   onRedirect: (options, url, status) => {
