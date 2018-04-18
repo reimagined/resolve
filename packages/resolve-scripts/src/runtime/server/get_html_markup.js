727be870271d668899672d3df23703be558fdefd
mrcheater <slayerx@narod.ru>
2018-04-18 14:39:04 +0300

    Add support reactxp

--- packages/resolve-scripts/src/runtime/server/get_html_markup.js
+++ packages/resolve-scripts/src/runtime/server/get_html_markup.js
@@ -40,7 +40,7 @@ export default ({ markup, styleTags, initialState, env, clientUrl }) => {
     `${helmet.script.toString()}` +
     '</head>' +
     `<body ${helmet.bodyAttributes.toString()}>` +
-    `<div id="resolve-application-container">${markup}</div>` +
+    `<div id="app-container" class="app-container">${markup}</div>` +
     `<script src="${clientUrl}"></script>` +
     '</body>' +
     '</html>'
