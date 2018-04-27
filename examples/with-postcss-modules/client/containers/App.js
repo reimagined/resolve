933bc05f5f1f49d37e5a80f7340d18aff50b37e0
Anton Zhukov <slayerx@narod.ru>
2018-04-27 16:31:07 +0300

    Add resolve.build.config.js for extend webpack config / Add example "with-postcss-modules" (#597)
    Add resolve.build.config.js extending webpack config
    Add example "with-postcss-modules"

---
+++ examples/with-postcss-modules/client/containers/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+import styles from './App.css'
+
+export const App = () => (
+  <div className={styles.wrapper}>
+    <Helmet>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="stylesheet" href="/styles/bundle.css" />
+    </Helmet>
+    <div className={styles.title}>
+      Hello World, this is my first component with postcss-modules!
+    </div>
+  </div>
+)
+
+export default App
