18d8de73a4010c427d63df1e3893a36c064ec2b6
Alexander Ralkov <ralkov@users.noreply.github.com>
2018-05-03 16:49:52 +0300

    Feature/extendable auth (#590)
    make auth extendable

--- packages/resolve-scripts/src/runtime/server/assign_auth_routes.js
+++ packages/resolve-scripts/src/runtime/server/assign_auth_routes.js
@@ -12,41 +12,40 @@ import executeReadModelQuery from './execute_read_model_query'
 import executeCommand from './command_executor'
 
 const authStrategiesConfigs = require($resolve.auth.strategies)
-const authStrategies = authStrategiesConfigs.map(({ strategy, options }) =>
-  resolveAuth(strategy, options)
+const authStrategies = authStrategiesConfigs.map(
+  ({ strategyConstructor, options }) =>
+    resolveAuth(strategyConstructor, options)
 )
 
 const assignAuthRoutes = app => {
-  authStrategies.forEach(strategy => {
-    strategy.forEach(({ route, callback }) => {
-      app[route.method.toLowerCase()](
-        getRootableUrl(route.path),
-        (req, res, next) => {
-          const safeReq = createRequest(req)
+  authStrategies.forEach(({ route, callback }) => {
+    app[route.method.toLowerCase()](
+      getRootableUrl(route.path),
+      (req, res, next) => {
+        const safeReq = createRequest(req)
 
-          Object.assign(safeReq, {
-            resolve: {
-              executeReadModelQuery: args =>
-                executeReadModelQuery({
-                  ...args,
-                  jwtToken: req.jwtToken
-                }),
-              executeViewModelQuery: args =>
-                executeViewModelQuery({
-                  ...args,
-                  jwtToken: req.jwtToken
-                }),
-              executeCommand
-            }
-          })
-          const safeRes = {
-            applyJwtValue,
-            ...createResponse(res)
+        Object.assign(safeReq, {
+          resolve: {
+            executeReadModelQuery: args =>
+              executeReadModelQuery({
+                ...args,
+                jwtToken: req.jwtToken
+              }),
+            executeViewModelQuery: args =>
+              executeViewModelQuery({
+                ...args,
+                jwtToken: req.jwtToken
+              }),
+            executeCommand
           }
-          callback(safeReq, safeRes, createAuthOptions(safeReq, safeRes, next))
+        })
+        const safeRes = {
+          applyJwtValue,
+          ...createResponse(res)
         }
-      )
-    })
+        callback(safeReq, safeRes, createAuthOptions(safeReq, safeRes, next))
+      }
+    )
   })
 }
 
