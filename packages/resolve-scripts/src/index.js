6bc62ff641411337fb66a64829afe526d3da2d0d
mrcheater <slayerx@narod.ru>
2018-07-20 01:28:15 +0300

    Refactoring

--- packages/resolve-scripts/src/index.js
+++
@@ -1,6 +0,0 @@
-import Routes from './runtime/client/components/Routes'
-import AppContainer from './runtime/client/components/AppContainer'
-import deserializeInitialState from './runtime/client/store/deserialize_initial_state'
-import createStore from './runtime/client/store/create_store'
-
-export { AppContainer, Routes, createStore, deserializeInitialState }
--- packages/resolve-scripts/src/runtime/client/index.js
+++ packages/resolve-scripts/src/runtime/client/index.js
@@ -1,55 +1,6 @@
-import React from 'react'
-import { render } from 'react-dom'
-import createHistory from 'history/createBrowserHistory'
-
+import Routes from './components/Routes'
 import AppContainer from './components/AppContainer'
-import createStore from './store/create_store'
 import deserializeInitialState from './store/deserialize_initial_state'
+import createStore from './store/create_store'
 
-import routes from '$resolve.routes'
-import rootPath from '$resolve.rootPath'
-import staticPath from '$resolve.staticPath'
-import aggregateActions from '$resolve.aggregateActions'
-
-import redux from '$resolve.redux'
-import viewModels from '$resolve.viewModels'
-import readModels from '$resolve.readModels'
-import aggregates from '$resolve.aggregates'
-import subscribeAdapter from '$resolve.subscribeAdapter'
-
-const initialState = deserializeInitialState(window.__INITIAL_STATE__)
-
-const origin = window.location.origin
-
-const history = createHistory({
-  basename: rootPath
-})
-
-const isClient = true
-
-const store = createStore({
-  redux,
-  viewModels,
-  readModels,
-  aggregates,
-  subscribeAdapter
-})({
-  initialState,
-  history,
-  origin,
-  rootPath,
-  isClient
-})
-
-render(
-  <AppContainer
-    origin={origin}
-    rootPath={rootPath}
-    staticPath={staticPath}
-    aggregateActions={aggregateActions}
-    store={store}
-    history={history}
-    routes={routes}
-  />,
-  document.getElementsByClassName('app-container')[0]
-)
+export { AppContainer, Routes, createStore, deserializeInitialState }
