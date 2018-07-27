bf90dac3560e17e29478bced3e5b33a5975ca5d2
vladv <IhostVlad>
2018-07-27 16:28:48 +0300

    Config factory WIP

---
+++ examples/todo/resolve.config.js
@@ -0,0 +1,51 @@
+import { defaultResolveConfig } from 'resolve-scripts'
+
+export default () => {
+  const config = {
+    ...defaultResolveConfig,
+    port: 3000,
+    routes: 'client/routes.js',
+    aggregates: [
+      {
+        name: 'Todo',
+        commands: 'common/aggregates/todo.commands.js'
+      }
+    ],
+    viewModels: [
+      {
+        name: 'Todos',
+        projection: 'common/view-models/todos.projection.js'
+      }
+    ]
+  }
+
+  const launchMode = process.argv[2]
+
+  switch (launchMode) {
+    case 'dev':
+      return {
+        ...config,
+        mode: 'development'
+      }
+
+    case 'start':
+    case 'build':
+      return {
+        ...config,
+        mode: 'production'
+      }
+
+    case 'runready':
+      return {
+        ...config,
+        storageAdapter: {
+          module: 'resolve-storage-lite',
+          options: {}
+        },
+        mode: 'test'
+      }
+
+    default:
+      return config
+  }
+}
