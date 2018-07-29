0c07b371b628902a34e48b9f1834ebee228c86bf
IhostVlad <ihost@mail.ru>
2018-07-29 03:15:03 +0300

    Update index.js

--- packages/core/resolve-scripts/src/core/index.js
+++ packages/core/resolve-scripts/src/core/index.js
@@ -1,3 +1,4 @@
 import defaultResolveConfig from './default.resolve.config'
+import declareRuntimeEnv from './declare_runtime_env'
 
-export { defaultResolveConfig }
+export { defaultResolveConfig, declareRuntimeEnv }
