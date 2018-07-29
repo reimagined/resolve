ee5a5f0f539211030c933141d9efb5a686c35345
IhostVlad <ihost@mail.ru>
2018-07-29 03:26:19 +0300

    Update declare_runtime_env.js

--- packages/core/resolve-scripts/src/core/declare_runtime_env.js
+++ packages/core/resolve-scripts/src/core/declare_runtime_env.js
@@ -1,8 +1,22 @@
-export const runtimeEnvSymbol = Symbol('@@resolve/runtime_env')
+const runtimeEnvSymbol = Symbol('@@resolve/runtime_env')
 
-const declareRuntimeEnv = (envName) => Object.create(null, {
-  type: { value: runtimeEnvSymbol },
-  name: { value: envName }
-})
+const declareRuntimeEnv = (envName) => {
+  if(envName == null || envName.constructor !== String) {
+    throw new Error('Runtime environment variable should be an string')
+  }
+  
+  return Object.create(null, {
+    type: { value: runtimeEnvSymbol },
+    name: { value: envName }
+  })
+}
+
+export const checkRuntimeEnv = (value) => {
+  if(value == null || value.type !== runtimeEnvSymbol) {
+    return null
+  }
+  
+  return value.name
+}
 
 export default declareRuntimeEnv
