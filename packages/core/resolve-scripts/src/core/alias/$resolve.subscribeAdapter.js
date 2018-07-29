8f6cbfe14347de61eb354f6f49afbdcac845a69a
IhostVlad <ihost@mail.ru>
2018-07-29 03:54:05 +0300

    Update $resolve.subscribeAdapter.js

--- packages/core/resolve-scripts/src/core/alias/$resolve.subscribeAdapter.js
+++ packages/core/resolve-scripts/src/core/alias/$resolve.subscribeAdapter.js
@@ -1,10 +1,35 @@
 import { message } from '../constants'
+import { checkRuntimeEnv } from '../declare_runtime_env'
 
 export default ({ resolveConfig, isClient }) => {
   if (!resolveConfig.subscribeAdapter) {
     throw new Error(`${message.configNotContainSectionError}.subscribeAdapter`)
   }
 
+  if (checkRuntimeEnv(resolveConfig.subscribeAdapter.module) != null) {
+    throw new Error(`${message.clientEnvError}.subscribeAdapter.module`)
+  }
+
+  for (const option of Object.keys(resolveConfig.subscribeAdapter.options)) {
+    if (
+      checkRuntimeEnv(resolveConfig.subscribeAdapter.options[option]) != null
+    ) {
+      throw new Error(
+        `${message.clientEnvError}.subscribeAdapter.options.${option}`
+      )
+    }
+  }
+
+  for (const option of Object.keys(resolveConfig.subscribeAdapter.options)) {
+    if (
+      checkRuntimeEnv(resolveConfig.subscribeAdapter.options[option]) != null
+    ) {
+      throw new Error(
+        `${message.clientEnvError}.subscribeAdapter.options.${option}`
+      )
+    }
+  }
+  
   const options = {
     client: resolveConfig.subscribeAdapter.options.client || {},
     server: resolveConfig.subscribeAdapter.options.server || {}
