090f4ad2f02a64bc1c8f385ae0c42a22029b606a
IhostVlad <ihost@mail.ru>
2018-07-29 04:20:39 +0300

    Update $resolve.jwtCookie.js

--- packages/core/resolve-scripts/src/core/alias/$resolve.jwtCookie.js
+++ packages/core/resolve-scripts/src/core/alias/$resolve.jwtCookie.js
@@ -1,10 +1,17 @@
 import { message } from '../constants'
+import { checkRuntimeEnv } from '../declare_runtime_env'
 
 export default ({ resolveConfig }) => {
   if (!resolveConfig.jwtCookie) {
     throw new Error(`${message.configNotContainSectionError}.jwtCookie`)
   }
 
+  for (const optionsKey of Object.keys(resolveConfig.jwtCookie)) {
+    if (checkRuntimeEnv(resolveConfig.jwtCookie[optionsKey]) != null) {
+      throw new Error(`${message.clientEnvError}.jwtCookie.${optionsKey}`)
+    }
+  }
+  
   const exports = []
 
   exports.push(
