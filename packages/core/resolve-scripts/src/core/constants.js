6bde88a1b694f5400f9470a11f615df8e4c3e799
vladv <IhostVlad>
2018-07-31 12:43:41 +0300

    Config factory WIP

--- packages/core/resolve-scripts/src/core/constants.js
+++ packages/core/resolve-scripts/src/core/constants.js
@@ -1,7 +1,6 @@
 const env = require('../../configs/env.list.json')
-const commands = require('../../configs/command.list.json')
 const statsConfig = require('../../configs/stats.config.json')
 const schemaResolveConfig = require('../../configs/schema.resolve.config.json')
 const message = require('../../configs/message')
 
-export { env, commands, statsConfig, schemaResolveConfig, message }
+export { env, statsConfig, schemaResolveConfig, message }
