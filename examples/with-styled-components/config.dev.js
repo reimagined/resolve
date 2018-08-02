8adc835643a12992a29f70a9a052524c618eb213
vladv <IhostVlad>
2018-08-02 11:42:04 +0300

    Resolve Config Factory WIP

---
+++ examples/with-styled-components/config.dev.js
@@ -0,0 +1,12 @@
+import { defaultResolveConfig } from 'resolve-scripts'
+import appConfig from './config.app'
+
+const devConfig = {
+  ...defaultResolveConfig,
+  ...appConfig,
+
+  port: 3000,
+  mode: 'development'
+}
+
+export default devConfig
--- examples/with-styled-components/index.js
+++ examples/with-styled-components/index.js
@@ -1,38 +1,34 @@
-import { execSync } from 'child_process'
-import { getInstallations } from 'testcafe-browser-tools'
+import { build, start, watch, runTestcafe } from 'resolve-scripts'
 
-import { defaultResolveConfig, build, start, watch } from 'resolve-scripts'
+import devConfig from './config.dev'
+import prodConfig from './config.prod'
+import testFunctionalConfig from './config.test_functional'
 
-const config = {
-  ...defaultResolveConfig,
+const launchMode = process.argv[2]
 
-  port: 3000,
-  routes: 'client/routes.js'
-}
-
-async function main() {
-  const launchMode = process.argv[2]
-
+void (async () => {
   switch (launchMode) {
     case 'dev': {
-      await watch({
-        ...config,
-        mode: 'development'
-      })
+      await watch(devConfig)
       break
     }
 
     case 'build': {
-      await build({
-        ...config,
-        mode: 'production'
-      })
+      await build(prodConfig)
       break
     }
 
     case 'start': {
-      await start(config)
+      await start(prodConfig)
+      break
+    }
 
+    case 'test:functional': {
+      await runTestcafe({
+        resolveConfig: testFunctionalConfig,
+        functionalTestsDir: 'test/functional',
+        browser: process.argv[3]
+      })
       break
     }
 
@@ -40,9 +36,7 @@ async function main() {
       throw new Error('Unknown option')
     }
   }
-}
-
-main().catch(error => {
+})().catch(error => {
   // eslint-disable-next-line no-console
   console.log(error)
 })
