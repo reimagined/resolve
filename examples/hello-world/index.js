74e49cef2e161a1e0bfbf6ab340dd9de220e9ac0
TatyanaRyzh <tatyana.ryzhenkova@devexpress.com>
2018-08-09 12:35:30 +0300

    Try to find error in CI after config changes

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -24,6 +24,8 @@ void (async () => {
     }
 
     case 'test:functional': {
+      // eslint-disable-next-line no-console
+      console.log(process.argv)
       await runTestcafe({
         resolveConfig: testFunctionalConfig,
         functionalTestsDir: 'test/functional',
