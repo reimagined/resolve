4c7aa61a70b7fde9a9111c601d560a2ad196e9c4
Anton Zhukov <slayerx@narod.ru>
2018-08-29 15:36:21 +0300

    Feature/fix auth (#760)

--- packages/core/resolve-auth/src/index.js
+++ packages/core/resolve-auth/src/index.js
@@ -1,59 +1,26 @@
-import defaultAuthOptions from './defaultAuthOptions'
+import authenticate from './authenticate'
+import createResponse from './create_response'
 
-const createResponse = () => {
-  const response = {
-    statusCode: 200,
-    headers: {},
-    cookies: {},
-    cookie: (name, value, opts) => {
-      response.cookies[name] = { name, value, opts }
-    },
-    clearCookie: name => {
-      response.cookies[name] = {
-        name,
-        value: 'deleted',
-        options: { expires: new Date(0) }
-      }
+const createStrategy = (
+  strategyConstructor,
+  options,
+  { jwtCookie, rootPath }
+) => {
+  const strategy = strategyConstructor(options)
+
+  return {
+    route: options.route,
+    callback: async req => {
+      const res = createResponse()
+
+      await authenticate(strategy, req, res, options, {
+        jwtCookie,
+        rootPath
+      })
+
+      return res
     }
   }
-  return response
 }
 
-const resolveAuth = (strategyConstructor, options, config) => ({
-  route: options.route,
-  callback: async (req, res, next) => {
-    const extendedOptions = { ...options, $resolveConfig: config }
-    const strategy = strategyConstructor(extendedOptions)
-
-    const fakeResponse = {
-      statusCode: 200,
-      headers: {},
-      cookies: {}
-    }
-
-    const resResponse = res || createResponse()
-    await new Promise(resolve => {
-      Object.keys(defaultAuthOptions).forEach(key => {
-        strategy[key] = async (...args) => {
-          await defaultAuthOptions[key](
-            extendedOptions,
-            req,
-            fakeResponse,
-            next,
-            ...args
-          )
-          resolve()
-        }
-      })
-      strategy.authenticate(req, { response: resResponse })
-    })
-    return res
-      ? fakeResponse
-      : Object.assign({}, resResponse, fakeResponse, {
-          headers: Object.assign({}, res.headers, fakeResponse.headers),
-          cookies: Object.assign({}, res.cookies, fakeResponse.cookies)
-        })
-  }
-})
-
-export default resolveAuth
+export default createStrategy
