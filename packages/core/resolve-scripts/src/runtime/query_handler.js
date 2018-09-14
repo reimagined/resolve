cbe9678bf20551e7993e6c6b1f9571c74a2db887
IhostVlad <ihost@mail.ru>
2018-09-14 17:36:17 +0300

    Feature/Api handlers (#780)

--- packages/core/resolve-scripts/src/runtime/query_handler.js
+++ packages/core/resolve-scripts/src/runtime/query_handler.js
@@ -1,26 +1,32 @@
 import { constants } from 'resolve-query'
+
 import readModelHandler from './read_model_handler'
 import viewModelHandler from './view_model_handler'
 import queryExecutor from './query_executor'
+import println from './utils/println'
 
 const { modelTypes } = constants
 
 const message = require('../../configs/message.json')
 
 const queryHandler = (req, res) => {
-  const { modelName } = req.params
-
   let modelType = null
+
   try {
+    const { modelName } = req.params
     modelType = queryExecutor.getModelType(modelName)
   } catch (error) {
     res.status(422).end(message.incorrectQuery)
+    println.error(error)
+    return
   }
 
   if (modelType === modelTypes.viewModel) {
     viewModelHandler(req, res)
   } else if (modelType === modelTypes.readModel) {
     readModelHandler(req, res)
+  } else {
+    res.status(422).end(message.incorrectQuery)
   }
 }
 
