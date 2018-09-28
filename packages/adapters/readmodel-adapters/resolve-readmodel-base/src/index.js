a4cd1f3b7df1f8f2358395dbb5bed4f1c9ce028d
IhostVlad <ihost@mail.ru>
2018-09-28 14:05:27 +0300

    Increase coverage for resolve read-model base adapter (#816)
    * Increase coverage for resolve read-model base adapter
    * Add Regression test. Pool should be contain .connectPromise after .reset(pool) [To many connection]

--- packages/adapters/readmodel-adapters/resolve-readmodel-base/src/index.js
+++ packages/adapters/readmodel-adapters/resolve-readmodel-base/src/index.js
@@ -1,6 +1,8 @@
 import buildProjection from './build-projection'
 import checkStoreApi from './check-store-api'
 import createAdapter from './create-adapter'
+import checkTableSchema from './check-table-schema'
+import wrapApis from './wrap-apis'
 import init from './init'
 import reset from './reset'
 
@@ -8,6 +10,8 @@ export default createAdapter.bind(
   null,
   buildProjection,
   checkStoreApi,
+  checkTableSchema,
+  wrapApis,
   init,
   reset
 )
