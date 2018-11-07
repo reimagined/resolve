494b1ac9e8509cebfde962ce24d63787deee6cdd
IhostVlad <ihost@mail.ru>
2018-11-07 12:27:10 +0300

    Update cloud_entry.js

--- packages/core/resolve-scripts/src/runtime/cloud_entry.js
+++ packages/core/resolve-scripts/src/runtime/cloud_entry.js
@@ -68,6 +68,9 @@ const lambdaWorker = async (assemblies, resolveBase, event, context) => {
     executorResult = await executor(event, context)
   }
   // DynamoDB trigger event
+  // AWS DynamoDB streams guarantees that changesets from one table partition will 
+  // be delivered strictly into one lambda instance, i.e. following code works in
+  // single-thread mode for one event storage - see https://amzn.to/2LkKXAV
   else if (event.Records != null) {
     const events = event.Records.map(record =>
       Converter.unmarshall(record.dynamodb.NewImage)
