bece5099fe4aa92e01ddeb59f03438b14c6eae73
IhostVlad <ihost@mail.ru>
2018-11-22 16:34:57 +0300

    Fix HackerNews importer to support module-comments and use storage adapter directly from resolve.config via API route. Fix resolve-storage-lite dispose functions. (#879)

--- packages/adapters/storage-adapters/resolve-storage-lite/src/dispose.js
+++ packages/adapters/storage-adapters/resolve-storage-lite/src/dispose.js
@@ -1,7 +1,7 @@
 const dispose = async ({ database, promiseInvoke }, { dropEvents }) => {
   if (dropEvents) {
     await promiseInvoke(database.remove.bind(database), {}, { multi: true })
-    await database.clearIndexes()
+    await database.resetIndexes()
   }
 }
 
