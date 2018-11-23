d945897b435ea865428e0e0d9bfab0370d31d405
Eugeniy Burmistrov <eugeniy.burmistrov@devexpress.com>
2018-11-23 12:46:33 +0300

    Lesson 4 - Add comment about react-bootstrap to the  code example

--- examples/shopping-list-tutorial/lesson-4/client/containers/ShoppingList.js
+++ examples/shopping-list-tutorial/lesson-4/client/containers/ShoppingList.js
@@ -1,28 +1,29 @@
-import React from "react";
-import { connectViewModel } from "resolve-redux";
+import React from 'react'
+import { connectViewModel } from 'resolve-redux'
 
-import { ListGroup, ListGroupItem, Checkbox } from "react-bootstrap";
+// The example code uses components from the react-bootstrap library to keep the markup compact.
+import { ListGroup, ListGroupItem, Checkbox } from 'react-bootstrap'
 
 export class ShoppingList extends React.PureComponent {
   render() {
-    const list = this.props.data.list;
+    const list = this.props.data.list
     return (
-      <ListGroup style={{ maxWidth: "500px", margin: "auto" }}>
+      <ListGroup style={{ maxWidth: '500px', margin: 'auto' }}>
         {list.map(todo => (
           <ListGroupItem key={todo.id}>
             <Checkbox inline>{todo.text}</Checkbox>
           </ListGroupItem>
         ))}
       </ListGroup>
-    );
+    )
   }
 }
 
 export const mapStateToOptions = (state, ownProps) => {
   return {
-    viewModelName: "ShoppingList",
-    aggregateIds: ["shopping-list-1"]
-  };
-};
+    viewModelName: 'ShoppingList',
+    aggregateIds: ['shopping-list-1']
+  }
+}
 
-export default connectViewModel(mapStateToOptions)(ShoppingList);
+export default connectViewModel(mapStateToOptions)(ShoppingList)
