8ad0f8cd28da2f0ad48b8301b1ee1ff3a801948e
vladv <vladv@resolves-mbp.corp.devexpress.com>
2018-12-13 13:53:03 +0300

    Implement client-side event propagation from command handler and perform filtering duplication old events

--- packages/core/resolve-redux/src/constants.js
+++ packages/core/resolve-redux/src/constants.js
@@ -1,2 +1,3 @@
+export const aggregateVersionsMap = '@@resolve/aggregateVersionsMap'
 export const lastTimestampMap = '@@resolve/lastTimestampMap'
 export const connectorMetaMap = '@@resolve/connectorMetaMap'
