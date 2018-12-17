5ffe1ac8ca9875b2043aa3edbcc774b5c7a851c0
mrcheater <slayerx@narod.ru>
2018-12-17 17:46:13 +0300

    Split adjustWebpackReactNative/adjustWebpackCommonPackages

--- packages/core/resolve-scripts/src/adjust_webpack_react_native.js
+++ packages/core/resolve-scripts/src/adjust_webpack_react_native.js
@@ -5,59 +5,14 @@ import getModulesDirs from './get_modules_dirs'
 
 const isString = val => val != null && val.constructor === String
 
-const adjustWebpackReactNative = ({
-  resolveConfig,
-  reactNativeDir,
-  commonPackages
-}) => (webpackConfigs, { alias }) => {
+const adjustWebpackReactNative = ({ resolveConfig, reactNativeDir }) => (
+  webpackConfigs,
+  { alias }
+) => {
   const isClient = true
 
   if (!isString(reactNativeDir)) {
     throw new Error('The `reactNativeDir` field must be String')
-  }
-  if (
-    Object(commonPackages) !== commonPackages ||
-    !Object.keys(commonPackages).every(isString) ||
-    !Object.values(commonPackages).every(isString)
-  ) {
-    throw new Error('The `commonPackages` field must be Object<String, String>')
-  }
-
-  for (const webpackConfig of webpackConfigs) {
-    webpackConfig.resolve.modules = ['node_modules']
-    webpackConfig.module.rules[1].use.options.plugins = [
-      ...(webpackConfig.module.rules[1].use.options.plugins || []),
-      [
-        require.resolve('babel-plugin-module-resolver'),
-        {
-          alias: commonPackages
-        }
-      ]
-    ]
-    webpackConfig.module.rules.push({
-      test: /\.js$/,
-      include: Object.values(commonPackages),
-      exclude: [
-        /node_modules/,
-        ...getModulesDirs({ isAbsolutePath: true }),
-        path.resolve(__dirname, '../lib'),
-        path.resolve(__dirname, '../es')
-      ],
-      use: {
-        loader: require.resolve('babel-loader'),
-        options: {
-          cacheDirectory: true,
-          plugins: [
-            [
-              require.resolve('babel-plugin-module-resolver'),
-              {
-                alias: commonPackages
-              }
-            ]
-          ]
-        }
-      }
-    })
   }
 
   const webpackNativeConfig = {
