c8c32c09e91168fcd627dfc729d277f0aff52680
mrcheater <slayerx@narod.ru>
2019-01-04 22:18:42 +0300

    Cancel running pipelines

--- trigger-on-pull-request/index.js
+++ trigger-on-pull-request/index.js
@@ -1,64 +1,57 @@
-const fs = require('fs')
-const path = require('path')
-const request = require('request')
 const octokit = require('@octokit/rest')()
+const Gitlab = require('gitlab')
+const request = require('request')
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-const triggers = {
-  set: (pullRequestNumber, mergeCommitSha) => {
-    try {
-      const state = fs.existsSync(path.join(__dirname, 'state.json'))
-        ? JSON.parse(
-          fs
-            .readFileSync(path.join(__dirname, 'state.json'), {
-              encoding: 'utf-8'
-            })
-            .toString('utf-8')
-        )
-        : {}
-      state[pullRequestNumber] = mergeCommitSha
-      fs.writeFileSync(
-        path.join(__dirname, 'state.json'),
-        JSON.stringify(state, null, 2),
-        {
-          encoding: 'utf-8'
-        }
-      )
-    } catch (error) {
-      console.error(error)
-    }
-  },
-  get: pullRequestNumber => {
-    try {
-      const state = fs.existsSync(path.join(__dirname, 'state.json'))
-        ? JSON.parse(
-          fs
-            .readFileSync(path.join(__dirname, 'state.json'), {
-              encoding: 'utf-8'
-            })
-            .toString('utf-8')
-        )
-        : {}
-      const mergeCommitSha = state[pullRequestNumber]
-      return mergeCommitSha
-    } catch (error) {
-      console.error(error)
-    }
+const triggers = new Map()
+
+const gitlabProjectId = 'reimagined/resolve'
+
+async function cancelPipelines(PipelinesService) {
+  const pipelines = await PipelinesService.all(gitlabProjectId)
+
+  const activePipelines = pipelines.filter(
+    ({ status }) => status === 'running' || status === 'pending'
+  )
+
+  for (const { id: pipelineId } of activePipelines) {
+    await PipelinesService.cancel(gitlabProjectId, pipelineId)
+    console.log(`stop pipeline "${pipelineId}"`)
   }
 }
 
 async function main({ gitlab, github, refreshTime }) {
+  const PipelinesService = new Gitlab.Pipelines({
+    token: gitlab.personalAccessToken
+  })
+
+  try {
+    await cancelPipelines(PipelinesService)
+  } catch (_) {
+    try {
+      await cancelPipelines(PipelinesService)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   octokit.authenticate({
     type: 'token',
     token: github.token
   })
 
   while (true) {
-    const { data: pullRequests } = await octokit.pulls.list({
-      owner: github.owner,
-      repo: github.repo
-    })
+    let pullRequests = []
+    try {
+      pullRequests = (await octokit.pulls.list({
+        owner: github.owner,
+        repo: github.repo
+      })).data
+    } catch (error) {
+      console.error(error)
+      continue
+    }
 
     for (const pullRequest of pullRequests) {
       const prevMergeCommitSha = triggers.get(pullRequest.number)
@@ -68,11 +61,13 @@ async function main({ gitlab, github, refreshTime }) {
       ) {
         triggers.set(pullRequest.number, pullRequest.merge_commit_sha)
 
+        // TODO. Use require('gitlab'). https://github.com/jdalrymple/node-gitlab/issues/254
         request(
           {
+            url: gitlab.pipelineUrl,
             method: 'post',
             body: {
-              token: gitlab.token,
+              token: gitlab.pipelineToken,
               ref: gitlab.ref,
               variables: {
                 PULL_REQUEST_NAME: `${pullRequest.title}`,
@@ -85,20 +80,29 @@ async function main({ gitlab, github, refreshTime }) {
                 TARGET_BRANCH_SHA: `${pullRequest.base.sha}`
               }
             },
-            json: true,
-            url: gitlab.url
+            json: true
           },
-          error => {
+          (error, { statusCode, statusMessage }) => {
             if (error) {
               triggers.set(pullRequest.number, prevMergeCommitSha)
               console.error(error)
               return
             }
 
+            if (!(statusCode >= 200 && statusCode < 300)) {
+              console.error({
+                statusCode,
+                statusMessage
+              })
+              return
+            }
+
             console.log(
               `pull request from "${pullRequest.head.ref}" to "${
                 pullRequest.base.ref
-              }"`
+              }" { number: ${pullRequest.number}, head: "${
+                pullRequest.head.sha
+              }" }`
             )
           }
         )
@@ -110,11 +114,12 @@ async function main({ gitlab, github, refreshTime }) {
 }
 
 main({
-  refreshTime: 10000,
+  refreshTime: 15000,
   gitlab: {
-    url: process.env.GITLAB_PIPELINE_URL,
-    token: process.env.GITLAB_PIPELINE_TOKEN,
-    ref: 'ci/cd'
+    ref: 'ci/cd',
+    pipelineUrl: process.env.GITLAB_PIPELINE_URL,
+    pipelineToken: process.env.GITLAB_PIPELINE_TOKEN,
+    personalAccessToken: process.env.GITLAB_PERSONAL_ACCESS_TOKEN
   },
   github: {
     owner: 'reimagined',
