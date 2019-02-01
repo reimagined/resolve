456c5438b3669ee79e762fa443f29bf435b75497
timbset <timophey.trainenkov@devexpress.com>
2019-02-01 12:13:15 +0300

    Implement possibility to reset read model or all read models by invoking of AWS lambda

---
+++ packages/core/resolve-runtime/src/handlers/resolve_event_handler.js
@@ -0,0 +1,34 @@
+import { constants } from 'resolve-query'
+
+const handleResolveReadModelEvent = async (lambdaEvent, resolve) => {
+  switch (lambdaEvent.operation) {
+    case 'reset': {
+      const executors = lambdaEvent.name
+        ? [resolve.executeQuery.getExecutor(lambdaEvent.name)]
+        : resolve.executeQuery.getExecutors(constants.modelTypes.readModel)
+
+      for (const executor of executors) {
+        await executor.read({ isBulkRead: true })
+        await executor.dispose()
+      }
+
+      return 'ok'
+    }
+    default: {
+      return null
+    }
+  }
+}
+
+const handleResolveEvent = async (lambdaEvent, resolve) => {
+  switch (lambdaEvent.part) {
+    case 'readModel': {
+      return handleResolveReadModelEvent(lambdaEvent, resolve)
+    }
+    default: {
+      return null
+    }
+  }
+}
+
+export default handleResolveEvent
---
+++ packages/core/resolve-runtime/test/resolve_event_handler.test.js
@@ -0,0 +1,95 @@
+import handleResolveEvent from '../src/handlers/resolve_event_handler'
+
+describe('resolve event handler', () => {
+  const executor = {
+    read: jest.fn(),
+    dispose: jest.fn()
+  }
+
+  const resolve = {
+    executeQuery: {
+      getExecutor: jest.fn(),
+      getExecutors: jest.fn()
+    }
+  }
+
+  afterEach(() => {
+    executor.dispose.mockClear()
+    resolve.executeQuery.getExecutors.mockClear()
+    resolve.executeQuery.getExecutor.mockClear()
+  })
+
+  describe('common', () => {
+    it('returns null for unknown operation', async () => {
+      const lambdaEvent = {
+        part: 'readModel',
+        operation: 'someUnknownOperation'
+      }
+
+      const result = await handleResolveEvent(lambdaEvent, resolve)
+      expect(result).toEqual(null)
+
+      expect(resolve.executeQuery.getExecutors).toHaveBeenCalledTimes(0)
+      expect(resolve.executeQuery.getExecutor).toHaveBeenCalledTimes(0)
+    })
+
+    it('returns null for unknown part', async () => {
+      const lambdaEvent = {
+        part: 'someUnknownPart'
+      }
+
+      const result = await handleResolveEvent(lambdaEvent, resolve)
+      expect(result).toEqual(null)
+
+      expect(resolve.executeQuery.getExecutors).toHaveBeenCalledTimes(0)
+      expect(resolve.executeQuery.getExecutor).toHaveBeenCalledTimes(0)
+    })
+  })
+
+  describe('read model', () => {
+    it('handles read model reset correctly', async () => {
+      const lambdaEvent = {
+        part: 'readModel',
+        operation: 'reset'
+      }
+
+      resolve.executeQuery.getExecutors.mockReturnValueOnce([executor])
+
+      const result = await handleResolveEvent(lambdaEvent, resolve)
+
+      expect(result).toEqual('ok')
+
+      expect(resolve.executeQuery.getExecutors).toHaveBeenCalledTimes(1)
+      expect(resolve.executeQuery.getExecutor).toHaveBeenCalledTimes(0)
+
+      expect(executor.read).toHaveBeenCalledWith({
+        isBulkRead: true
+      })
+
+      expect(executor.dispose).toHaveBeenCalledTimes(1)
+    })
+
+    it('handles specific read model reset correctly', async () => {
+      const lambdaEvent = {
+        part: 'readModel',
+        operation: 'reset',
+        name: 'default'
+      }
+
+      resolve.executeQuery.getExecutor.mockReturnValueOnce(executor)
+
+      const result = await handleResolveEvent(lambdaEvent, resolve)
+
+      expect(result).toEqual('ok')
+
+      expect(resolve.executeQuery.getExecutor).toHaveBeenCalledWith('default')
+      expect(resolve.executeQuery.getExecutors).toHaveBeenCalledTimes(0)
+
+      expect(executor.read).toHaveBeenCalledWith({
+        isBulkRead: true
+      })
+
+      expect(executor.dispose).toHaveBeenCalledTimes(1)
+    })
+  })
+})
