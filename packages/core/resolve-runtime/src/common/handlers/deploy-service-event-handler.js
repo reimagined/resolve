9e1b2f7c4d53024be505db145e3bd1247299ad6b
Vasily Sokolov <vasiliy.sokolov@devexpress.com>
2019-04-04 16:05:06 +0300

    Reset read model (cloud): lambda to reset meta data is invoked

--- packages/core/resolve-runtime/src/common/handlers/deploy-service-event-handler.js
+++ packages/core/resolve-runtime/src/common/handlers/deploy-service-event-handler.js
@@ -1,7 +1,20 @@
+const Lambda = require('aws-sdk/clients/lambda')
+const lambda = new Lambda()
+
 const handleResolveReadModelEvent = async (lambdaEvent, resolve) => {
   switch (lambdaEvent.operation) {
     case 'reset': {
+      const { DEPLOYMENT_ID } = process.env
       for (const { name } of resolve.readModels) {
+        await lambda
+          .invoke({
+          FunctionName: `${DEPLOYMENT_ID}-meta-lock`,
+          Payload: JSON.stringify({
+            listenerId: name,
+            command: 'reset'
+          })
+        })
+        .promise()
         await resolve.executeQuery.drop(name)
       }
 
