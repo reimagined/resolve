8cad0c5c31e01896abe6e2b0e71867705f4634e6
max <mozg.mayhem@gmail.com>
2019-04-05 17:43:38 +0300

    Cloud scheduler step function interface implemented

---
+++ examples/hacker-news/resolve-scheduler-cloud/__mocks__/aws-sdk/clients/stepfunctions.js
@@ -0,0 +1,9 @@
+function StepFunctions() {}
+
+const wrap = () => jest.fn().mockReturnValue({ promise: () => Promise.resolve() })
+
+StepFunctions.prototype.startExecution = wrap()
+StepFunctions.prototype.stopExecution = wrap()
+StepFunctions.prototype.listExecutions = wrap()
+
+module.exports = StepFunctions
---
+++ examples/hacker-news/resolve-scheduler-cloud/test/step-function.test.js
@@ -0,0 +1,128 @@
+import {
+  start,
+  stopAll
+} from '../step-function'
+import StepFunctions from 'aws-sdk/clients/stepfunctions'
+
+const {
+  startExecution: awsStartExecution,
+  stopExecution: awsStopExecution,
+  listExecutions: awsListExecutions
+} = StepFunctions.prototype
+
+const mockReturnPromiseOnce = (fn, value) => fn.mockReturnValueOnce({
+  promise: () => Promise.resolve(value)
+})
+
+beforeEach(() => {
+  process.env['RESOLVE_CLOUD_SCHEDULER_STEP_FUNCTION_ARN'] = 'step-function-arn'
+})
+
+afterEach(() => {
+  awsStartExecution.mockClear()
+  awsStopExecution.mockClear()
+  awsListExecutions.mockClear()
+})
+
+describe('start', () => {
+  let createEntry = (salt) => ({
+    date: new Date(2019, 4, 5, 17, 30, 5, 15).getTime(),
+    taskId: `taskId_${salt}`,
+    command: { salt }
+  })
+
+  test('start execution for an entry', async () => {
+    const entry = createEntry('a')
+
+    await start(createEntry('a'))
+
+    expect(awsStartExecution).toHaveBeenCalledWith({
+      stateMachineArn: 'step-function-arn',
+      name: 'taskId_a',
+      input: JSON.stringify({
+        date: '2019-05-05T14:30:05.015Z',
+        event: {
+          resolveSource: 'Scheduler',
+          entry
+        }
+      })
+    })
+  })
+})
+
+describe('stopAll', () => {
+  const createExecution = (executionArn, name) => ({
+    name,
+    executionArn,
+    stateMachineArn: 'state-machine-arn',
+    status: 'RUNNING',
+    startDate: new Date()
+  })
+
+  test('stop one execution', async () => {
+    mockReturnPromiseOnce(awsListExecutions, {
+      executions: [createExecution('execution-arn', 'name_a')]
+    })
+
+    await stopAll()
+
+    expect(awsListExecutions).toHaveBeenCalledWith({
+      stateMachineArn: 'step-function-arn',
+      maxResults: expect.any(Number),
+      statusFilter: 'RUNNING'
+    })
+    expect(awsStopExecution).toHaveBeenCalledWith({
+      executionArn: 'execution-arn',
+      cause: expect.any(String),
+      error: expect.any(String)
+    })
+  })
+
+  test('stop multiple executions', async () => {
+    mockReturnPromiseOnce(awsListExecutions, {
+      executions: [
+        createExecution('execution-arn-a', 'name_a'),
+        createExecution('execution-arn-b', 'name_b')
+      ]
+    })
+
+    await stopAll()
+
+    expect(awsStopExecution).toHaveBeenCalledWith(expect.objectContaining({
+      executionArn: 'execution-arn-a'
+    }))
+    expect(awsStopExecution).toHaveBeenCalledWith(expect.objectContaining({
+      executionArn: 'execution-arn-b'
+    }))
+  })
+
+  test('follow paged executions list', async () => {
+    mockReturnPromiseOnce(awsListExecutions, {
+      executions: [
+        createExecution('execution-arn-a', 'name_a')
+      ],
+      nextToken: 'next-token'
+    })
+    mockReturnPromiseOnce(awsListExecutions, {
+      executions: [
+        createExecution('execution-arn-b', 'name_b')
+      ]
+    })
+
+    await stopAll()
+
+    expect(awsListExecutions.mock.calls.length).toEqual(2)
+    expect(awsListExecutions).toHaveBeenCalledWith(expect.objectContaining({
+      nextToken: 'next-token'
+    }))
+    expect(awsStopExecution).toHaveBeenCalledWith(expect.objectContaining({
+      executionArn: 'execution-arn-a'
+    }))
+    expect(awsStopExecution).toHaveBeenCalledWith(expect.objectContaining({
+      executionArn: 'execution-arn-b'
+    }))
+  })
+})
+
+
+
