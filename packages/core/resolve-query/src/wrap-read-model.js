6ce6f4893e69aded36aeb644a9d17d15a60a51b9
MrCheater <mister.cheater@list.ru>
2019-05-16 17:46:59 +0300

    Refactoring

--- packages/core/resolve-query/src/wrap-read-model.js
+++ packages/core/resolve-query/src/wrap-read-model.js
@@ -1,7 +1,123 @@
-import makeAutoPromise from './make-auto-promise'
+const maybeConnect = pool => {
+  if (pool.hasOwnProperty('connectionPromise')) {
+    return pool.connectionPromise
+  }
+
+  pool.connectionPromise = Promise.resolve(null)
+  if (typeof pool.connector.connect === 'function') {
+    pool.connectionPromise = pool.connector.connect(pool.readModel.name)
+  }
+
+  return pool.connectionPromise
+}
+
+const read = async (pool, resolverName, resolverArgs, jwtToken) => {
+  if (pool.isDisposed) {
+    throw new Error(`Read model "${pool.readModel.name}" is disposed`)
+  }
+  if (typeof pool.readModel.resolvers[resolverName] !== 'function') {
+    throw new Error(`Resolver ${resolverName} does not exist`)
+  }
+  await pool.doUpdateRequest(pool.readModel.name)
+  const connection = await maybeConnect(pool)
+
+  return await pool.readModel.resolvers[resolverName](
+    connection,
+    resolverArgs,
+    jwtToken
+  )
+}
+
+const updateByEvents = async (pool, events) => {
+  if (pool.isDisposed) {
+    throw new Error(`Read model "${pool.readModel.name}" is disposed`)
+  }
+
+  const projection = pool.readModel.projection
+
+  if (projection == null) {
+    throw new Error(
+      `Updating by events is prohibited when "${
+        pool.readModel.name
+      }" projection is not specified`
+    )
+  }
+
+  let lastError = null
+  let lastEvent = null
+
+  try {
+    for (const event of events) {
+      if (pool.isDisposed) {
+        throw new Error(
+          `Read model "${pool.readModel.name}" updating had been interrupted`
+        )
+      }
+
+      if (event != null && typeof projection[event.type] === 'function') {
+        const connection = await maybeConnect(pool)
+        const executor = projection[event.type]
+        await executor(connection, event)
+        lastEvent = event
+      }
+    }
+  } catch (error) {
+    lastError = Object.create(Error.prototype, {
+      message: { value: error.message, enumerable: true },
+      stack: { value: error.stack, enumerable: true }
+    })
+  }
+
+  const result = {
+    listenerId: pool.readModel.name,
+    lastError,
+    lastEvent
+  }
+
+  if (lastError != null) {
+    throw result
+  } else {
+    return result
+  }
+}
+
+const readAndSerialize = async (pool, resolverName, resolverArgs, jwtToken) => {
+  if (pool.isDisposed) {
+    throw new Error(`Read model "${pool.readModel.name}" is disposed`)
+  }
+
+  const result = await read(pool, resolverName, resolverArgs, jwtToken)
+
+  return JSON.stringify(result, null, 2)
+}
+
+const drop = async pool => {
+  if (pool.isDisposed) {
+    throw new Error(`Read model "${pool.readModel.name}" is disposed`)
+  }
+  if (typeof pool.connector.drop !== 'function') {
+    return
+  }
+
+  const connection = await maybeConnect(pool)
+  await pool.connector.drop(connection, pool.readModel.name)
+}
+
+const dispose = async pool => {
+  if (pool.isDisposed) {
+    throw new Error(`Read model "${pool.readModel.name}" is disposed`)
+  }
+  if (typeof pool.connector.disconnect !== 'function') {
+    return
+  }
+
+  const connection = await maybeConnect(pool)
+  await pool.connector.disconnect(connection, pool.readModel.name)
+
+  pool.isDisposed = true
+}
 
 const wrapReadModel = (readModel, readModelConnectors, doUpdateRequest) => {
-  let isDisposed = false
   const connector = readModelConnectors[readModel.connectorName]
   if (connector == null) {
     throw new Error(
@@ -11,125 +127,19 @@ const wrapReadModel = (readModel, readModelConnectors, doUpdateRequest) => {
     )
   }
 
-  const connectionPromise = makeAutoPromise(async () => {
-    if (typeof connector.connect === 'function') {
-      return await connector.connect(readModel.name)
-    }
-    return null
-  })
-
-  const read = async (resolverName, resolverArgs, jwtToken) => {
-    if (isDisposed) {
-      throw new Error(`Read model "${readModel.name}" is disposed`)
-    }
-    if (typeof readModel.resolvers[resolverName] !== 'function') {
-      throw new Error(`Resolver ${resolverName} does not exist`)
-    }
-    await doUpdateRequest(readModel.name)
-    const connection = await connectionPromise
-
-    return await readModel.resolvers[resolverName](
-      connection,
-      resolverArgs,
-      jwtToken
-    )
-  }
-
-  const updateByEvents = async events => {
-    if (isDisposed) {
-      throw new Error(`Read model "${readModel.name}" is disposed`)
-    }
-
-    const projection = readModel.projection
-
-    if (projection == null) {
-      throw new Error(
-        `Updating by events is prohibited when "${
-          readModel.name
-        }" projection is not specified`
-      )
-    }
-
-    let lastError = null
-    let lastEvent = null
-
-    try {
-      for (const event of events) {
-        if (isDisposed) {
-          throw new Error(
-            `Read model "${readModel.name}" updating had been interrupted`
-          )
-        }
-
-        if (event != null && typeof projection[event.type] === 'function') {
-          const connection = await connectionPromise
-          const executor = projection[event.type]
-          await executor(connection, event)
-          lastEvent = event
-        }
-      }
-    } catch (error) {
-      lastError = Object.create(Error.prototype, {
-        message: { value: error.message, enumerable: true },
-        stack: { value: error.stack, enumerable: true }
-      })
-    }
-
-    const result = {
-      listenerId: readModel.name,
-      lastError,
-      lastEvent
-    }
-
-    if (lastError != null) {
-      throw result
-    } else {
-      return result
-    }
-  }
-
-  const readAndSerialize = async (resolverName, resolverArgs, jwtToken) => {
-    if (isDisposed) {
-      throw new Error(`Read model "${readModel.name}" is disposed`)
-    }
-
-    const result = await read(resolverName, resolverArgs, jwtToken)
-
-    return JSON.stringify(result, null, 2)
-  }
-
-  const drop = async () => {
-    if (isDisposed) {
-      throw new Error(`Read model "${readModel.name}" is disposed`)
-    }
-    if (typeof connector.drop !== 'function') {
-      return
-    }
-
-    const connection = await connectionPromise
-    await connector.drop(connection, readModel.name)
-  }
-
-  const dispose = async () => {
-    if (isDisposed) {
-      throw new Error(`Read model "${readModel.name}" is disposed`)
-    }
-    if (typeof connector.disconnect !== 'function') {
-      return
-    }
-
-    const connection = await connectionPromise
-    await connector.disconnect(connection, readModel.name)
-
-    isDisposed = true
+  const pool = {
+    readModel,
+    doUpdateRequest,
+    isDisposed: false,
+    connector
   }
 
   return Object.freeze({
-    read,
-    readAndSerialize,
-    updateByEvents,
-    drop,
-    dispose
+    read: read.bind(null, pool),
+    readAndSerialize: readAndSerialize.bind(null, pool),
+    updateByEvents: updateByEvents.bind(null, pool),
+    drop: drop.bind(null, pool),
+    dispose: dispose.bind(null, pool)
   })
 }
 
