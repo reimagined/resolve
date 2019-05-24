22533f25dd89434410a6c41c3e44990838f4155c
VladIhost <ihost@mail.ru>
2019-05-24 16:18:05 +0300

    Add test for get-webpack-configs

--- packages/core/resolve-scripts/test/get-webpack-configs.test.js
+++ packages/core/resolve-scripts/test/get-webpack-configs.test.js
@@ -52,6 +52,23 @@ const resolveConfig = {
   customConstants: {}
 }
 
+test('should throw on wrong target', async () => {
+  try {
+    await getWebpackConfigs({
+      resolveConfig: {
+        ...resolveConfig,
+        target: 'wrong'
+      },
+      nodeModulesByAssembly: new Map()
+    })
+
+    return Promise.reject('Test failed')
+  } catch(error) {
+    expect(error).toBeInstanceOf(Error)
+  }
+})
+  
+
 test('should make webpack configs for local mode', async () => {
   const nodeModulesByAssembly = new Map()
 
@@ -82,6 +99,28 @@ test('should make webpack configs for cloud mode', async () => {
   })
 
   expect(normalizePaths(JSON.stringify(webpackConfigs))).toMatchSnapshot()
+
+  expect(
+    normalizePaths(JSON.stringify(Array.from(nodeModulesByAssembly)))
+  ).toMatchSnapshot()
+})
+
+test('should make external package.json resolver', async () => {
+  const nodeModulesByAssembly = new Map()
+
+  const webpackConfigs = await getWebpackConfigs({
+    resolveConfig: {
+      ...resolveConfig,
+      target: 'local'
+    },
+    nodeModulesByAssembly
+  })
+
+  const externalResolver = webpackConfigs[1].externals[0]
+  externalResolver(null, './resource', () => {})
+  externalResolver(null, '/resource', () => {})
+  externalResolver(null, '@org/package', () => {})
+  externalResolver(null, 'package', () => {})
 
   expect(
     normalizePaths(JSON.stringify(Array.from(nodeModulesByAssembly)))
