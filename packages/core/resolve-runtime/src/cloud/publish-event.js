75de531f051a1c1a1d75893bc1251e1e554d01c7
MrCheater <mister.cheater@list.ru>
2019-07-11 14:44:07 +0300

    Improve resolve-runtime

--- packages/core/resolve-runtime/src/cloud/publish-event.js
+++ packages/core/resolve-runtime/src/cloud/publish-event.js
@@ -10,12 +10,27 @@ const publishEvent = async (resolve, event) => {
   }
 
   try {
+    const promises = []
+    for(const { name: modelName, projection } of resolve.readModels) {
+      if(projection.hasOwnProperty(event.type)) {
+        promises.push(resolve.doUpdateRequest(modelName))
+      }
+    }
+    await Promise.all(promises)
+
+    log.info('Lambda pushed event into step function updater successfully')
+  } catch (error) {
+    log.warn('Lambda can not publish event into step function updater', error)
+  }
+
+  try {
     await resolve.mqtt.publish(eventDescriptor).promise()
 
     log.info('Lambda pushed event into MQTT successfully', eventDescriptor)
   } catch (error) {
     log.warn('Lambda can not publish event into MQTT', eventDescriptor, error)
   }
+
 }
 
 export default publishEvent
