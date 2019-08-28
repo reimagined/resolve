fd6e91455f4ed06565dbbb70a5565776c7384c26
MrCheater <mister.cheater@list.ru>
2019-08-28 14:17:21 +0300

    Improve import

--- packages/adapters/storage-adapters/resolve-storage-postgresql-serverless/src/import.js
+++ packages/adapters/storage-adapters/resolve-storage-postgresql-serverless/src/import.js
@@ -1,5 +1,4 @@
 import stream from 'stream'
-import { ConcurrentError } from 'resolve-storage-base'
 
 import {
   RESERVED_EVENT_SIZE,
@@ -17,95 +16,69 @@ function EventStream(pool, byteOffset = 0, eventId = 1) {
   this.beginPosition = 0
   this.endPosition = 0
   this.vacantSize = BUFFER_SIZE
+  this.saveEventPromiseSet = new Set()
+  this.timestamp = 0
 }
 
 EventStream.prototype = Object.create(stream.Writable.prototype)
 EventStream.prototype.constructor = stream.Writable
-EventStream.prototype.saveEvent = async function(stringifiedEvent) {
-  await this.pool.waitConnectAndInit()
-
-  let event
-  try {
-  event = JSON.parse(stringifiedEvent)
-  } catch (e) {
-    console.error(e)
-    console.log(stringifiedEvent)
-  }
+EventStream.prototype.saveEvent = async function(event) {
   const {
     databaseName,
     tableName,
     executeStatement,
-    fullJitter,
     escapeId,
     escape
   } = this.pool
 
-  for (let retry = 0; ; retry++) {
-    try {
-      const serializedEvent = [
-        `${escape(event.aggregateId)},`,
-        `${+event.aggregateVersion},`,
-        `${escape(event.type)},`,
-        escape(JSON.stringify(event.payload != null ? event.payload : null))
-      ].join('')
+  const serializedEvent = [
+    `${escape(event.aggregateId)},`,
+    `${+event.aggregateVersion},`,
+    `${escape(event.type)},`,
+    escape(JSON.stringify(event.payload != null ? event.payload : null))
+  ].join('')
 
-      const byteLength =
-        Buffer.byteLength(serializedEvent) + RESERVED_EVENT_SIZE
+  const byteLength = Buffer.byteLength(serializedEvent) + RESERVED_EVENT_SIZE
 
-      await executeStatement(
-        [
-          `START TRANSACTION;`,
-          `UPDATE ${escapeId(databaseName)}.${escapeId(
-            `${tableName}-sequence`
-          )} `,
-          `SET ${escapeId('eventId')} = ${+this.eventId},`,
-          `${escapeId(
-            'transactionId'
-          )} = CAST(txid_current() AS ${LONG_STRING_SQL_TYPE}),`,
-          `${escapeId('timestamp')} = ${+event.timestamp}`,
-          `WHERE ${escapeId('key')} = 0;`,
-          `INSERT INTO ${escapeId(databaseName)}.${escapeId(tableName)}(`,
-          `${escapeId('eventId')},`,
-          `${escapeId('timestamp')},`,
-          `${escapeId('aggregateId')},`,
-          `${escapeId('aggregateVersion')},`,
-          `${escapeId('type')},`,
-          `${escapeId('payload')},`,
-          `${escapeId('eventSize')}`,
-          `) VALUES (`,
-          `  ${+this.eventId},`,
-          `  ${+event.timestamp},`,
-          `  ${serializedEvent},`,
-          `  ${byteLength}`,
-          `);`,
-          `COMMIT;`
-        ].join('')
-      )
-      console.log('this.eventId',this.eventId)
-      this.eventId++
+  await executeStatement(
+    [
+      `INSERT INTO ${escapeId(databaseName)}.${escapeId(tableName)}(`,
+      `${escapeId('eventId')},`,
+      `${escapeId('timestamp')},`,
+      `${escapeId('aggregateId')},`,
+      `${escapeId('aggregateVersion')},`,
+      `${escapeId('type')},`,
+      `${escapeId('payload')},`,
+      `${escapeId('eventSize')}`,
+      `) VALUES (`,
+      `  ${+event.eventId},`,
+      `  ${+event.timestamp},`,
+      `  ${serializedEvent},`,
+      `  ${byteLength}`,
+      `)`
+    ].join('')
+  )
+}
 
+EventStream.prototype.saveSequence = async function() {
+  const { databaseName, tableName, executeStatement, escapeId } = this.pool
 
-
-      break
-    } catch (error) {
-      try {
-        await executeStatement(`ROLLBACK;`)
-      } catch (e) {}
-
-      if (error.message == null || error.message.indexOf('duplicate key') < 0) {
-        throw error
-      }
-      if (error.message.indexOf('aggregateIdAndVersion') < 0) {
-        await new Promise(resolve => setTimeout(resolve, fullJitter(retry)))
-        continue
-      }
-
-      throw new ConcurrentError(event.aggregateId)
-    }
-  }
+  await executeStatement(
+    [
+      `UPDATE ${escapeId(databaseName)}.${escapeId(`${tableName}-sequence`)} `,
+      `SET ${escapeId('eventId')} = ${+this.eventId},`,
+      `${escapeId(
+        'transactionId'
+      )} = CAST(txid_current() AS ${LONG_STRING_SQL_TYPE}),`,
+      `${escapeId('timestamp')} = ${+this.timestamp}`,
+      `WHERE ${escapeId('key')} = 0;`
+    ].join('')
+  )
 }
 
 EventStream.prototype._write = async function(chunk, encoding, callback) {
+  await this.pool.waitConnectAndInit()
+
   if (this.encoding == null) {
     this.encoding = encoding
   } else if (this.encoding !== encoding) {
@@ -160,41 +133,63 @@ EventStream.prototype._write = async function(chunk, encoding, callback) {
 
     this.beginPosition = endEventPosition
 
-    await this.saveEvent(stringifiedEvent)
+    const event = JSON.parse(stringifiedEvent)
+    event.eventId = this.eventId++
+    this.timestamp = Math.max(this.timestamp, event.timestamp)
+
+    const saveEventPromise = this.saveEvent(event)
+    void saveEventPromise.then(
+      this.saveEventPromiseSet.delete.bind(
+        this.saveEventPromiseSet,
+        saveEventPromise
+      )
+    )
+    this.saveEventPromiseSet.add(saveEventPromise)
   }
 
   callback()
 }
 
 EventStream.prototype._final = async function(callback) {
-  if (this.vacantSize === BUFFER_SIZE) {
-    this.buffer = null
-    callback()
-    return
+  await this.pool.waitConnectAndInit()
+
+  if (this.vacantSize !== BUFFER_SIZE) {
+    let stringifiedEvent = null
+    if (this.beginPosition < this.endPosition) {
+      stringifiedEvent = this.buffer
+        .slice(this.beginPosition, this.endPosition)
+        .toString(this.encoding)
+    } else {
+      stringifiedEvent = this.buffer
+        .slice(this.beginPosition, BUFFER_SIZE)
+        .toString(this.encoding)
+      stringifiedEvent += this.buffer
+        .slice(0, this.endPosition)
+        .toString(this.encoding)
+    }
+
+    const event = JSON.parse(stringifiedEvent)
+    event.eventId = this.eventId++
+    this.timestamp = Math.max(this.timestamp, event.timestamp)
+
+    const saveEventPromise = this.saveEvent(event)
+    void saveEventPromise.then(
+      this.saveEventPromiseSet.delete.bind(
+        this.saveEventPromiseSet,
+        saveEventPromise
+      )
+    )
+    this.saveEventPromiseSet.add(saveEventPromise)
   }
 
-  let stringifiedEvent = null
-  if (this.beginPosition < this.endPosition) {
-    stringifiedEvent = this.buffer
-      .slice(this.beginPosition, this.endPosition)
-      .toString(this.encoding)
-  } else {
-    stringifiedEvent = this.buffer
-      .slice(this.beginPosition, BUFFER_SIZE)
-      .toString(this.encoding)
-    stringifiedEvent += this.buffer
-      .slice(0, this.endPosition)
-      .toString(this.encoding)
-  }
-
-  await this.saveEvent(stringifiedEvent)
+  await Promise.all([...this.saveEventPromiseSet, this.saveSequence()])
 
   this.buffer = null
 
   callback()
 }
 
-const importStream = (pool, { byteOffset } = {}) =>
-  new EventStream(pool, byteOffset)
+const importStream = (pool, { byteOffset, eventId } = {}) =>
+  new EventStream(pool, byteOffset, eventId)
 
 export default importStream
