de37c5924e53dcff3008c1a9ba029dfd10727cfa
Lykoi18 <aiden.ghost666@gmail.com>
2019-09-11 15:44:15 +0300

    Implement save-event in mysql storage adapter

--- packages/adapters/storage-adapters/resolve-storage-mysql/src/freeze.js
+++ packages/adapters/storage-adapters/resolve-storage-mysql/src/freeze.js
@@ -2,7 +2,7 @@ const deactivate = async ({ connection, tableName, escapeId }) => {
   await connection.execute(
     `CREATE TABLE ${escapeId(`${tableName}-freeze`)}(
       ${escapeId('surrogate')} BIGINT NOT NULL,
-      PRIMARY KEY(${escapeId('surrogate')}
+      PRIMARY KEY(${escapeId('surrogate')})
     )
     COMMENT = "RESOLVE EVENT STORE ${escapeId(tableName)} FREEZE MARKER" 
     `
--- packages/adapters/storage-adapters/resolve-storage-mysql/src/save-event-only.js
+++ packages/adapters/storage-adapters/resolve-storage-mysql/src/save-event-only.js
@@ -1,6 +1,23 @@
-const saveEventOnly = async function(pool, event) {
-  // TODO: split saveEvent and saveEventOnly
-  return await pool.saveEvent(event)
+const saveEventOnly = async function({ tableName, connection, escapeId }, event) {
+  await connection.query(
+    `INSERT INTO ${escapeId(tableName)}(
+      ${escapeId('timestamp')},
+      ${escapeId('aggregateId')},
+      ${escapeId('aggregateVersion')},
+      ${escapeId('type')},
+      ${escapeId('payload')}
+    ) VALUES(
+      ${+event.timestamp},
+      ${escape(event.aggregateId)},
+      ${+event.aggregateVersion},
+      ${escape(event.type)},
+      (CAST(${
+        event.payload != null
+          ? escape(JSON.stringify(event.payload))
+          : escape('null')
+        } AS JSON)),
+    )`
+  )
 }
 
 export default saveEventOnly
