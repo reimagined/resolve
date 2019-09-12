7a5cd124b1bc103466a682ecbcf5ed1a799f1720
Lykoi18 <aiden.ghost666@gmail.com>
2019-09-12 16:08:40 +0300

    Fix prettier

--- packages/adapters/storage-adapters/resolve-storage-mongo/src/is-frozen.js
+++ packages/adapters/storage-adapters/resolve-storage-mongo/src/is-frozen.js
@@ -1,10 +1,10 @@
 const isFrozen = async ({ database, collectionName }) => {
   try {
-    await new Promise((resolve, reject) => database.collection(
-      `${collectionName}-freeze`,
-      { strict: true },
-      error => error != null ? reject(error) : resolve()
-    ))
+    await new Promise((resolve, reject) =>
+      database.collection(`${collectionName}-freeze`, { strict: true }, error =>
+        error != null ? reject(error) : resolve()
+      )
+    )
 
     return true
   } catch (error) {
