92eb54089fc254f5e8ec79c764bda8bf24ef67e0
IhostVlad <ihost@mail.ru>
2019-10-04 18:02:27 +0300

    Update run.js

--- examples/hacker-news/run.js
+++ examples/hacker-news/run.js
@@ -84,134 +84,17 @@ void (async () => {
         )
 
         const clientCompiling = new Promise((resolve, reject) => {
-          clientCompiler.run((err, { stats }) => {
+          clientCompiler.watch({ aggregateTimeout: 1000, poll: 1000 }, (err, { stats }) => {
             stats.forEach(showBuildInfo.bind(null, err))
-            const hasNoErrors = stats.reduce(
-              (acc, val) => acc && (val != null && !val.hasErrors()),
-              true
+            const hasErrors = stats.reduce(
+              (acc, val) => acc || (val != null && val.hasErrors()),
+              false
             )
             void (hasNoErrors ? resolve() : reject(stats.toString('')))
           })
         })
 
         await Promise.all([watch(resolveConfig), clientCompiling])
-        break
-      }
-
-      case 'build': {
-        await build(merge(baseConfig, prodConfig))
-        break
-      }
-
-      case 'cloud': {
-        await build(merge(baseConfig, cloudConfig))
-        break
-      }
-
-      case 'start': {
-        await start(merge(baseConfig, prodConfig))
-        break
-      }
-
-      case 'reset': {
-        const resolveConfig = merge(baseConfig, devConfig)
-        await reset(resolveConfig, {
-          dropEventStore: true,
-          dropSnapshots: true,
-          dropReadModels: true,
-          dropSagas: true
-        })
-        break
-      }
-
-      case 'import-event-store': {
-        const resolveConfig = merge(baseConfig, devConfig)
-
-        const importFile = process.argv[3]
-
-        await importEventStore(resolveConfig, { importFile })
-        break
-      }
-
-      case 'export-event-store': {
-        const resolveConfig = merge(baseConfig, devConfig)
-
-        const exportFile = process.argv[3]
-
-        await exportEventStore(resolveConfig, { exportFile })
-        break
-      }
-
-      case 'test:functional': {
-        const resolveConfig = merge(baseConfig, testFunctionalConfig)
-
-        await reset(resolveConfig, {
-          dropEventStore: true,
-          dropSnapshots: true,
-          dropReadModels: true,
-          dropSagas: true
-        })
-
-        await runTestcafe({
-          resolveConfig,
-          functionalTestsDir: 'test/functional',
-          browser: process.argv[3]
-        })
-        break
-      }
-
-      case 'import': {
-        const config = merge(baseConfig, devConfig)
-
-        await reset(config, {
-          dropEventStore: true,
-          dropSnapshots: true,
-          dropReadModels: true,
-          dropSagas: true
-        })
-
-        const importConfig = merge(config, {
-          eventBroker: { launchBroker: false }
-        })
-        Object.assign(importConfig, {
-          apiHandlers: [
-            {
-              method: 'POST',
-              path: '/api/import_events',
-              controller: {
-                module: 'import/import_api_handler.js',
-                options: {}
-              }
-            }
-          ],
-          aggregates: [],
-          readModels: [],
-          viewModels: [],
-          sagas: [],
-          readModelConnectors: {},
-          schedulers: {}
-        })
-
-        if (process.env.hasOwnProperty(String(importConfig.port))) {
-          process.env.PORT = +String(process.env.PORT)
-        } else if (
-          process.env.PORT != null &&
-          process.env.PORT.defaultValue != null
-        ) {
-          process.env.PORT = +process.env.PORT.defaultValue
-        } else {
-          process.env.PORT = 3000
-        }
-
-        Object.assign(process.env, {
-          RESOLVE_SERVER_OPEN_BROWSER: 'false',
-          ROOT_PATH: importConfig.rootPath
-        })
-
-        await build(importConfig)
-
-        await Promise.all([start(importConfig), runImport()])
-
         break
       }
 
