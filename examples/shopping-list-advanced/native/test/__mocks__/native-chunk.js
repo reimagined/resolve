b57a3f575acc3169784a208c64a8ee008f685bb8
VladIhost <ihost@mail.ru>
2019-11-05 18:03:41 +0300

    Update mocks

---
+++ examples/shopping-list-advanced/native/test/__mocks__/native-chunk.js
@@ -0,0 +1,58 @@
+const viewModels = []
+const readModels = []
+const aggregates = []
+
+const rootPath = ''
+const staticPath = ''
+const customConstants = {
+  backend: {
+    protocol: 'http',
+    hostname: '127.0.0.1',
+    port: 3000
+  },
+  remoteReduxDevTools: {
+    hostname: '127.0.0.1',
+    port: 19042
+  }
+}
+const port = '3000'
+const applicationName = 'react-native-shopping-list'
+
+export const subscribeAdapter = {
+  async init() {},
+  async close() {},
+  async subscribeToTopics() {},
+  async unsubscribeFromTopics() {},
+  isConnected() {
+    return true
+  }
+}
+
+const jwtCookie = { name: 'jwt' }
+
+const resolveRedux = {
+  actions: {},
+  actionTypes: {},
+  connectViewModel: () => {},
+  connectReadModel: () => {},
+  Providers: {},
+  createStore: () => {},
+  sendAggregateAction: () => {},
+  getOrigin: () => {}
+}
+
+const nativeChunk = {
+  viewModels,
+  readModels,
+  aggregates,
+  rootPath,
+  staticPath,
+  customConstants,
+  port,
+  applicationName,
+  subscribeAdapter,
+  jwtCookie,
+  resolveRedux
+}
+
+export default nativeChunk
