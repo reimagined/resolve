516601500073496efe09485e13c37bf9573b686b
Lykoi18 <aiden.ghost666@gmail.com>
2019-12-20 13:41:52 +0300

    Fix image-gallery example

--- packages/modules/resolve-module-uploader/src/api-handlers/getFormUpload.js
+++ packages/modules/resolve-module-uploader/src/api-handlers/getFormUpload.js
@@ -1,17 +1,13 @@
-import jwt from 'jsonwebtoken'
-
-const getFormUpload = ({ jwtSecret }) => async (req, res) => {
+const getFormUpload = () => async (req, res) => {
   const adapter = req.resolve.uploader
   try {
-    jwt.verify(req.jwtToken, jwtSecret)
-
     const { dir } = req.query
     const signedPost = await adapter.getSignedPost(dir)
 
     await res.json(signedPost)
   } catch (error) {
     await res.status(405)
-    await res.end(error)
+    await res.end(error.toString())
   }
 }
 
