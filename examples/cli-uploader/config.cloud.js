c2e335fb1e800d212bb755ef062783fa33f10b35
Lykoi18 <aiden.ghost666@gmail.com>
2020-02-10 18:05:25 +0300

    Modify resolve-upload-cloud / update cloud config in example

--- examples/cli-uploader/config.cloud.js
+++ examples/cli-uploader/config.cloud.js
@@ -3,5 +3,44 @@ import { declareRuntimeEnv } from 'resolve-scripts'
 export default {
   target: 'cloud',
   mode: 'production',
-  staticPath: declareRuntimeEnv('RESOLVE_CLOUD_STATIC_URL')
+  staticPath: declareRuntimeEnv('RESOLVE_CLOUD_STATIC_URL'),
+  storageAdapter: {
+    module: 'resolve-storage-postgresql-serverless',
+    options: {
+      awsSecretStoreArn: declareRuntimeEnv('RESOLVE_ES_SECRET_STORE_ARN'),
+      dbClusterOrInstanceArn: declareRuntimeEnv('RESOLVE_ES_CLUSTER_ARN'),
+      databaseName: declareRuntimeEnv('RESOLVE_ES_DATABASE'),
+      tableName: declareRuntimeEnv('RESOLVE_ES_TABLE'),
+      region: declareRuntimeEnv('AWS_REGION')
+    }
+  },
+  readModelConnectors: {
+    users: {
+      module: 'resolve-readmodel-mysql',
+      options: {
+        host: declareRuntimeEnv('RESOLVE_READMODEL_SQL_HOST'),
+        database: declareRuntimeEnv('RESOLVE_READMODEL_SQL_DATABASE'),
+        user: declareRuntimeEnv('RESOLVE_READMODEL_SQL_USER'),
+        password: declareRuntimeEnv('RESOLVE_READMODEL_SQL_PASSWORD')
+      }
+    },
+    files: {
+      module: 'resolve-readmodel-mysql',
+      options: {
+        host: declareRuntimeEnv('RESOLVE_READMODEL_SQL_HOST'),
+        database: declareRuntimeEnv('RESOLVE_READMODEL_SQL_DATABASE'),
+        user: declareRuntimeEnv('RESOLVE_READMODEL_SQL_USER'),
+        password: declareRuntimeEnv('RESOLVE_READMODEL_SQL_PASSWORD')
+      }
+    }
+  },
+  uploadAdapter: {
+    module: 'resolve-upload-cloud',
+    options: {
+      encryptedDeploymentId: declareRuntimeEnv('RESOLVE_ENCRYPTED_DEPLOYMENT_ID'),
+      deploymentId: declareRuntimeEnv('RESOLVE_DEPLOYMENT_ID'),
+      CDN: declareRuntimeEnv('RESOLVE_UPLOADER_URL'),
+      uploaderArn: declareRuntimeEnv('RESOLVE_UPLOADER_LAMBDA_ARN')
+    }
+  }
 }
