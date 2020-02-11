1d9e88e1055f7123ab4da7ec6fffcba60696fb66
mrcheater <mister.cheater@list.ru>
2020-02-11 16:51:39 +0300

    Fix Prettier

--- packages/adapters/readmodel-adapters/resolve-readmodel-lite/src/connect.js
+++ packages/adapters/readmodel-adapters/resolve-readmodel-lite/src/connect.js
@@ -77,9 +77,9 @@ const connect = async (imports, pool, options) => {
 
   let connector = null
   if (databaseFile === ':memory:') {
-    if(process.env.RESOLVE_LAUNCH_ID != null) {  
+    if (process.env.RESOLVE_LAUNCH_ID != null) {
       const tmpName = `${os.tmpdir()}/${+process.env.RESOLVE_LAUNCH_ID}}.db`
-      if(!imports.fs.existSync(tmpName)) {
+      if (!imports.fs.existSync(tmpName)) {
         imports.fs.writeFileSync(tmpName, '')
         process.on('exit', imports.fs.unlinkSync.bind(imports.fs, tmpName))
       }
