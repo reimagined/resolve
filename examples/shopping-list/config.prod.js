2545165feeaf87f718079abbfbddd9ce36c95fc8
mrcheater <mister.cheater@list.ru>
2020-02-12 14:57:27 +0300

    Update examples

--- examples/shopping-list/config.prod.js
+++ examples/shopping-list/config.prod.js
@@ -4,6 +4,12 @@ const prodConfig = {
   target: 'local',
   port: declareRuntimeEnv('PORT', '3000'),
   mode: 'production',
+  storageAdapter: {
+    module: 'resolve-storage-lite',
+    options: {
+      databaseFile: 'data/event-store.db'
+    }
+  },
   readModelConnectors: {
     default: {
       module: 'resolve-readmodel-lite',
