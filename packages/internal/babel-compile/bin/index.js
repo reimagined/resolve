c88bdd3188313e8848b4ce6975759e9006bd38ba
mrcheater <mister.cheater@list.ru>
2020-02-21 14:16:51 +0300

    Prepare compile

--- packages/internal/babel-compile/bin/index.js
+++ packages/internal/babel-compile/bin/index.js
@@ -9,6 +9,7 @@ for (const config of configs) {
   babel({
     babelOptions: {
       ...getBabelConfig({
+        sourceType: config.sourceType,
         moduleType: config.moduleType,
         moduleTarget: config.moduleTarget
       }),
@@ -16,9 +17,11 @@ for (const config of configs) {
       babelrc: false
     },
     cliOptions: {
+      extensions: config.extensions,
+      outFileExtension: config.outFileExtension,
       filenames: [config.inputDir],
       outDir: config.outDir,
-      deleteDirOnStart: true
+      deleteDirOnStart: config.deleteDirOnStart
     }
   })
     .then(() => {
