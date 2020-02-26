55dbf5abe9a938c3723638499937d708f6fced8d
mrcheater <mister.cheater@list.ru>
2020-02-26 16:41:31 +0300

    Small fixes

---
+++ packages/core/ts-package-example/__tests__/index.unit.test.ts
@@ -0,0 +1,12 @@
+import { a } from '../index'
+import { f3 } from '../b/index'
+
+test('test', () => {
+  const sum: number = a.f1(1,2)
+
+  expect(sum).toEqual(3)
+  expect(f3(false, false)).toEqual(false)
+  expect(f3(false, true)).toEqual(false)
+  expect(f3(true, false)).toEqual(false)
+  expect(f3(true, true)).toEqual(true)
+})
