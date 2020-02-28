f217dcd70cb6eec37639d871dd6175e2433fec05
max <mozg.mayhem@gmail.com>
2020-02-28 18:01:29 +0300

    Migrate vanillajs to resolve-api

--- examples/with-vanillajs/client/index.js
+++ examples/with-vanillajs/client/index.js
@@ -1,67 +1,51 @@
 import domready from 'domready'
-import initSubscription from './init_subscription'
-import createApi from './create_api'
+import { getApi } from 'resolve-api'
 import initUI from './init_ui'
 import updateUI from './update_ui'
 
-const main = async ({
-  subscribeAdapter: createSubscribeAdapter,
-  rootPath,
-  viewModels
-}) => {
-  try {
-    await new Promise(resolve => domready(resolve))
-    const chatViewModel = viewModels.find(({ name }) => name === 'chat')
-    const api = createApi(rootPath)
+const main = async resolveContext => {
+  await new Promise(resolve => domready(resolve))
+  const { viewModels } = resolveContext
+  const chatViewModel = viewModels.find(({ name }) => name === 'chat')
+  const api = getApi(resolveContext)
 
-    const sendMessage = (userName, message) =>
-      api
-        .sendCommand({
-          aggregateName: 'Chat',
-          commandType: 'postMessage',
-          aggregateId: userName,
-          payload: message
-        })
-        .catch(error => {
+  const sendMessage = (userName, message) =>
+    api.command(
+      {
+        aggregateName: 'Chat',
+        type: 'postMessage',
+        aggregateId: userName,
+        payload: message
+      },
+      err => {
+        if (err) {
           // eslint-disable-next-line no-console
-          console.warn(`Error while sending command: ${error}`)
-        })
-
-    let chatViewModelState = window.__INITIAL_STATE__.chat
-
-    initUI(chatViewModelState, sendMessage)
-
-    const chatViewModelUpdater = event => {
-      const eventType = event != null && event.type != null ? event.type : null
-      const eventHandler = chatViewModel.projection[eventType]
-
-      if (typeof eventHandler === 'function') {
-        chatViewModelState = eventHandler(chatViewModelState, event)
+          console.warn(`Error while sending command: ${err}`)
+        }
       }
-
-      setImmediate(updateUI.bind(null, chatViewModelState))
-    }
-
-    chatViewModelUpdater(null)
-
-    const { attachSubscriber, subscribeToTopics } = await initSubscription(
-      createSubscribeAdapter,
-      rootPath,
-      api
     )
 
-    attachSubscriber(chatViewModelUpdater)
+  const { data } = await api.query({
+    name: 'chat',
+    aggregateIds: '*'
+  })
 
-    await subscribeToTopics([
-      {
-        topicName: 'MESSAGE_POSTED',
-        topicId: '*'
-      }
-    ])
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error(`Fatal application error: ${error}`)
+  let chatViewModelState = data
+
+  initUI(data, sendMessage)
+
+  const chatViewModelUpdater = event => {
+    const eventType = event != null && event.type != null ? event.type : null
+    const eventHandler = chatViewModel.projection[eventType]
+
+    if (typeof eventHandler === 'function') {
+      chatViewModelState = eventHandler(chatViewModelState, event)
+    }
+
+    setImmediate(updateUI.bind(null, chatViewModelState))
   }
+
+  await api.subscribeTo('chat', '*', chatViewModelUpdater)
 }
 
 export default main
--- examples/with-vanillajs/common/api-handlers/markup.js
+++ examples/with-vanillajs/common/api-handlers/markup.js
@@ -5,19 +5,12 @@ export default async (req, res) => {
   const { rootPath, staticPath, seedClientEnvs } = req.resolve
   const scriptUrl = getStaticBasedPath(rootPath, staticPath, 'index.js')
   const styleUrl = getStaticBasedPath(rootPath, staticPath, 'style.css')
-  const initialState = {
-    chat: await req.resolve.executeQuery({
-      modelName: 'chat',
-      aggregateIds: '*'
-    })
-  }
 
   const markupHtml = `<!doctype html>
   <html>
     <head>
       <link href="${styleUrl}" rel="stylesheet" />
       <script>
-      window.__INITIAL_STATE__ = ${jsonUtfStringify(initialState)}
       window.__RESOLVE_RUNTIME_ENV__=${jsonUtfStringify(seedClientEnvs)}
       </script>
       <script src="${scriptUrl}"></script>
