cdf4aeb82b520193f624c6cbee1f29b78ff3f598
Vasily Sokolov <vasiliy.sokolov@devexpress.com>
2020-03-06 10:44:45 +0300

    unused

--- packages/core/resolve-hooks/test/use_query.unit.test.tsx
+++ packages/core/resolve-hooks/test/use_query.unit.test.tsx
@@ -2,13 +2,7 @@ import React, { FunctionComponent, ReactNode } from 'react'
 import { renderHook, act } from '@testing-library/react-hooks'
 
 import { mocked } from 'ts-jest/utils'
-import {
-  getApi,
-  Query,
-  QueryOptions,
-  QueryCallback,
-  QueryResult
-} from 'resolve-api'
+import { getApi } from 'resolve-api'
 import { useQuery } from '../src/use_query'
 import { ResolveContext } from '../src/context'
 
