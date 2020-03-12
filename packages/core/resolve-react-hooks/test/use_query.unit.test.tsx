78cbc29da65a3ed128dd933ceb201fdf974cb42d
max <mozg.mayhem@gmail.com>
2020-03-12 17:15:18 +0300

    useQuery tests

--- packages/core/resolve-react-hooks/test/use_query.unit.test.tsx
+++ packages/core/resolve-react-hooks/test/use_query.unit.test.tsx
@@ -1,144 +1,180 @@
-import React, { FunctionComponent, ReactNode } from 'react'
-import { renderHook, act } from '@testing-library/react-hooks'
-
+import { useContext, useCallback } from 'react'
 import { mocked } from 'ts-jest/utils'
-import { getClient } from 'resolve-client'
+import { Query, QueryCallback, getClient, QueryOptions } from 'resolve-client'
 import { useQuery } from '../src/use_query'
-import { ResolveContext } from '../src/context'
 
-jest.mock('resolve-api')
-const mockedGetApi = mocked(getClient)
-const validatorMock = jest.fn()
-const callbackMock = jest.fn()
-const apiQueryMock = jest.fn().mockReturnValue({})
+jest.mock('resolve-client')
+jest.mock('react', () => ({
+  useContext: jest.fn(() => 'mocked-context'),
+  useCallback: jest.fn(cb => cb)
+}))
+jest.mock('../src/context', () => ({
+  ResolveContext: 'mocked-context-selector'
+}))
 
-const clearMocks = () => {
-  mockedGetApi.mockClear()
-  apiQueryMock.mockClear()
+const mockedGetClient = mocked(getClient)
+const mockedUseContext = mocked(useContext)
+const mockedUseCallback = mocked(useCallback)
+
+const mockedClient = {
+  command: jest.fn(),
+  query: jest.fn(() => Promise.resolve({ data: 'query-result', timestamp: 1 })),
+  getStaticAssetUrl: jest.fn(),
+  subscribeTo: jest.fn(),
+  unsubscribe: jest.fn()
+}
+const basicQuery = (): Query => ({
+  name: 'model',
+  resolver: 'resolver',
+  args: {}
+})
+const customOptions = (): QueryOptions => ({
+  method: 'GET'
+})
+
+const clearMocks = (): void => {
+  mockedGetClient.mockClear()
+
+  mockedUseContext.mockClear()
+  mockedUseCallback.mockClear()
+
+  mockedClient.command.mockClear()
 }
 
-const contextValue = {
-  origin: '',
-  rootPath: '',
-  staticPath: '',
-  viewModels: [
-    { name: 'view-model-name', projection: [], deserializeState: () => ({}) },
-    {
-      name: 'another-view-model-name',
-      projection: [],
-      deserializeState: () => ({})
-    }
-  ]
-}
+beforeAll(() => {
+  mockedGetClient.mockReturnValue(mockedClient)
+})
 
-type ContextChildrenProps = {
-  children?: ReactNode
-}
+afterEach(() => {
+  clearMocks()
+})
 
-const contextWrapper: FunctionComponent<ContextChildrenProps> = ({
-  children
-}) => (
-  <ResolveContext.Provider value={contextValue}>
-    {children}
-  </ResolveContext.Provider>
-)
+describe('common', () => {
+  test('client requested for specified context', () => {
+    useQuery(basicQuery())
 
-describe('useQuery', () => {
-  beforeAll(() => {
-    mockedGetApi.mockReturnValue({
-      command: jest.fn(),
-      query: apiQueryMock,
-      getStaticAssetUrl: jest.fn(),
-      subscribeTo: jest.fn(),
-      unsubscribe: jest.fn()
-    })
+    expect(mockedUseContext).toHaveBeenCalledWith('mocked-context-selector')
+    expect(getClient).toHaveBeenCalledWith('mocked-context')
   })
+
+  test('fail if not context found', () => {
+    mockedUseContext.mockReturnValueOnce(null)
+
+    expect(() => useQuery(basicQuery())).toThrow()
+    expect(mockedGetClient).not.toHaveBeenCalled()
+  })
+})
+
+describe('async mode', () => {
+  test('just a query', async () => {
+    const query = basicQuery()
+
+    await useQuery(query)()
+
+    expect(mockedClient.query).toHaveBeenCalledWith(query, undefined, undefined)
+    expect(mockedUseCallback).toHaveBeenCalledWith(expect.any(Function), [
+      'mocked-context',
+      query
+    ])
+  })
+
+  test('query and dependencies', async () => {
+    const query = basicQuery()
+
+    await useQuery(query, ['dependency'])()
+
+    expect(mockedClient.query).toHaveBeenCalledWith(query, undefined, undefined)
+    expect(mockedUseCallback).toHaveBeenCalledWith(expect.any(Function), [
+      'mocked-context',
+      'dependency'
+    ])
+  })
+
+  test('query and options', async () => {
+    const query = basicQuery()
+    const options = customOptions()
+
+    await useQuery(query, options)()
+
+    expect(mockedClient.query).toHaveBeenCalledWith(query, options, undefined)
+    expect(mockedUseCallback).toHaveBeenCalledWith(expect.any(Function), [
+      'mocked-context',
+      query,
+      options
+    ])
+  })
+
+  test('query, options and dependencies', async () => {
+    const query = basicQuery()
+    const options = customOptions()
+
+    await useQuery(query, options, ['dependency'])()
+
+    expect(mockedClient.query).toHaveBeenCalledWith(query, options, undefined)
+    expect(mockedUseCallback).toHaveBeenCalledWith(expect.any(Function), [
+      'mocked-context',
+      'dependency'
+    ])
+  })
+})
+
+describe('callback mode', () => {
+  let callback: QueryCallback
 
   beforeEach(() => {
-    clearMocks()
+    callback = jest.fn()
   })
 
-  test('use view model query', async () => {
-    const { result } = renderHook(
-      () =>
-        useQuery(
-          {
-            name: 'model-name',
-            aggregateIds: '*',
-            args: {
-              arg1: 'value-1'
-            }
-          },
-          {
-            waitFor: {
-              validator: validatorMock,
-              period: 5,
-              attempts: 5
-            }
-          },
-          callbackMock
-        ),
-      { wrapper: contextWrapper }
-    )
+  test('just a query', () => {
+    const query = basicQuery()
 
-    await act(() => Promise.resolve())
-    expect(apiQueryMock).toBeCalledTimes(1)
-    expect(typeof result.current[1]).toBe('function')
-    expect(apiQueryMock).toBeCalledWith(
-      { aggregateIds: '*', args: { arg1: 'value-1' }, name: 'model-name' },
-      { waitFor: { attempts: 5, period: 5, validator: validatorMock } },
-      callbackMock
-    )
+    useQuery(query, callback)()
 
-    act(() => {
-      const [data, getData] = result.current
-      getData()
-    })
-    await act(() => Promise.resolve())
-    expect(apiQueryMock).toBeCalledTimes(2)
+    expect(mockedClient.query).toHaveBeenCalledWith(query, undefined, callback)
+    expect(mockedUseCallback).toHaveBeenCalledWith(expect.any(Function), [
+      'mocked-context',
+      query,
+      callback
+    ])
   })
 
-  test('use read model query', async () => {
-    const { result } = renderHook(
-      () =>
-        useQuery(
-          {
-            name: 'model-name',
-            resolver: 'resolver-name',
-            args: {
-              arg1: 'value-1'
-            }
-          },
-          {
-            waitFor: {
-              validator: validatorMock,
-              period: 5,
-              attempts: 5
-            }
-          },
-          callbackMock
-        ),
-      { wrapper: contextWrapper }
-    )
-    await act(() => Promise.resolve())
+  test('query, callback and dependencies', () => {
+    const query = basicQuery()
 
-    expect(apiQueryMock).toBeCalledTimes(1)
-    expect(typeof result.current[1]).toBe('function')
-    expect(apiQueryMock).toBeCalledWith(
-      {
-        args: { arg1: 'value-1' },
-        name: 'model-name',
-        resolver: 'resolver-name'
-      },
-      { waitFor: { attempts: 5, period: 5, validator: validatorMock } },
-      callbackMock
-    )
+    useQuery(query, callback, ['dependency'])()
 
-    act(() => {
-      const [data, getData] = result.current
-      getData()
-    })
-    await act(() => Promise.resolve())
-    expect(apiQueryMock).toBeCalledTimes(2)
+    expect(mockedClient.query).toHaveBeenCalledWith(query, undefined, callback)
+    expect(mockedUseCallback).toHaveBeenCalledWith(expect.any(Function), [
+      'mocked-context',
+      'dependency'
+    ])
+  })
+
+  test('query, options and callback', () => {
+    const query = basicQuery()
+    const options = customOptions()
+
+    useQuery(query, options, callback)()
+
+    expect(mockedClient.query).toHaveBeenCalledWith(query, options, callback)
+    expect(mockedUseCallback).toHaveBeenCalledWith(expect.any(Function), [
+      'mocked-context',
+      query,
+      options,
+      callback
+    ])
+  })
+
+  test('query, options, callback and dependencies', () => {
+    const query = basicQuery()
+    const options = customOptions()
+
+    useQuery(query, options, callback, ['dependency'])()
+
+    expect(mockedClient.query).toHaveBeenCalledWith(query, options, callback)
+    expect(mockedUseCallback).toHaveBeenCalledWith(expect.any(Function), [
+      'mocked-context',
+      'dependency'
+    ])
   })
 })
