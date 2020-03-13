adb5543524a17f16f3393f3986f846e488045af5
Vasily Sokolov <vasiliy.sokolov@devexpress.com>
2020-03-13 16:32:22 +0300

    hook update

--- packages/core/resolve-react-hooks/src/use_subscription.ts
+++ packages/core/resolve-react-hooks/src/use_subscription.ts
@@ -1,43 +1,40 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useEffect, useContext, useCallback } from 'react'
 
 import {
   getClient,
   SubscribeCallback,
-  SubscribeHandler,
   ResubscribeCallback,
   Subscription
 } from 'resolve-client'
 
 import { ResolveContext } from './context'
+type SubscriptionOptions = {
+  viewModelName: string
+  aggregateIds: Array<string> | '*'
+  onSubscribe?: SubscribeCallback
+}
 
-const useSubscription = (
-  viewModelName: string,
-  aggregateIds: Array<string> | '*',
-  onEvent: SubscribeHandler,
-  onSubscribe?: SubscribeCallback,
-  onResubscribe?: ResubscribeCallback
-): void => {
+const useSubscription = (options: SubscriptionOptions): any | undefined => {
+  const { viewModelName, aggregateIds, onSubscribe } = options
+
   const context = useContext(ResolveContext)
   if (!context) {
     throw Error('You cannot use resolve effects outside Resolve context')
   }
   const { viewModels } = context
-  const api = getClient(context)
+  const client = getClient(context)
 
-  useEffect(() => {
-    const viewModel = viewModels.find(
-      ({ name }: { name: string }) => name === viewModelName
-    )
+  const viewModel = viewModels.find(
+    ({ name }: { name: string }) => name === viewModelName
+  )
 
-    if (!viewModel) {
-      return undefined
-    }
+  if (!viewModel) {
+    return undefined
+  }
 
-    let subscription: Subscription | null
-    const onSubscribeCallback = (
-      err: Error | null,
-      result: Subscription | null
-    ): void => {
+  let subscription: Subscription | null
+  const onSubscribeCallback = useCallback(
+    (err: Error | null, result: Subscription | null): void => {
       if (!err) {
         subscription = result
       }
@@ -45,27 +42,39 @@ const useSubscription = (
       if (typeof onSubscribe === 'function') {
         onSubscribe(err, result)
       }
-    }
+    },
+    [onSubscribe]
+  )
 
-    api.subscribeTo(
-      viewModelName,
-      aggregateIds,
-      onEvent,
-      onSubscribeCallback,
-      onResubscribe
-    )
+  const subscriptions = {
+    onEvent: (event: any): any => {},
+    onResubscribe: (event: any): any => {}
+  }
 
-    return (): void => {
-      console.log('use_subcription: unmount, subscription:', subscription)
-      if (!viewModel) {
-        return
-      }
+  return {
+    subscribe: useCallback(
+      (): Promise<Subscription> | void =>
+        client.subscribeTo(
+          viewModelName,
+          aggregateIds,
+          event => subscriptions.onEvent(event),
+          onSubscribeCallback,
+          event => subscriptions.onResubscribe(event)
+        ),
+      []
+    ),
+    unsubscribe: useCallback((): void => {
       if (subscription) {
-        console.log('use_subcription: unsubscribe')
-        api.unsubscribe(subscription)
+        client.unsubscribe(subscription)
       }
-    }
-  }, [onEvent, onSubscribe, onResubscribe])
+    }, []),
+    setEventHandler: useCallback((handler: any): any => {
+      subscriptions.onEvent = handler
+    }, []),
+    setResubscribeHandler: useCallback((handler: any): any => {
+      subscriptions.onResubscribe = handler
+    }, [])
+  }
 }
 
 export { useSubscription }
