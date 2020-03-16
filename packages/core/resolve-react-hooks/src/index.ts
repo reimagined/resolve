8ee8df8f6061c5ba70847f54b8f28877b86a2131
max <mozg.mayhem@gmail.com>
2020-03-16 15:19:09 +0300

    useClient hook

--- packages/core/resolve-react-hooks/src/index.ts
+++ packages/core/resolve-react-hooks/src/index.ts
@@ -3,6 +3,7 @@ import { useQuery } from './use_query'
 import { useCommand } from './use_command'
 import { useStaticResolver } from './use_static_resolver'
 import { useViewModel } from './use_view_model'
+import { useClient } from './use_client'
 import { ResolveContext } from './context'
 
 export {
@@ -11,5 +12,6 @@ export {
   useCommand,
   useQuery,
   useStaticResolver,
-  useViewModel
+  useViewModel,
+  useClient
 }
