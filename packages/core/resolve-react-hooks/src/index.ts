be80caad3fb064cc08e13d7d5cab1f3b76d88d10
max <mozg.mayhem@gmail.com>
2020-03-17 18:44:47 +0300

    Missed export

--- packages/core/resolve-react-hooks/src/index.ts
+++ packages/core/resolve-react-hooks/src/index.ts
@@ -3,5 +3,13 @@ import { useCommand } from './use_command'
 import { useStaticResolver } from './use_static_resolver'
 import { useViewModel } from './use_view_model'
 import { useClient } from './use_client'
+import { ResolveContext } from './context'
 
-export { useCommand, useQuery, useStaticResolver, useViewModel, useClient }
+export {
+  useCommand,
+  useQuery,
+  useStaticResolver,
+  useViewModel,
+  useClient,
+  ResolveContext
+}
