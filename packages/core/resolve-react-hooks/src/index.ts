87f2343b07b4083d3f41cc21e2dc1961a3e5e5c1
max <mozg.mayhem@gmail.com>
2020-03-25 12:02:04 +0300

    Export

--- packages/core/resolve-react-hooks/src/index.ts
+++ packages/core/resolve-react-hooks/src/index.ts
@@ -4,6 +4,8 @@ import { useStaticResolver } from './use_static_resolver'
 import { useViewModel } from './use_view_model'
 import { useClient } from './use_client'
 import { ResolveContext } from './context'
+import { useCommandBuilder } from './use_command_builder'
+import { useQueryBuilder } from './use_query_builder'
 
 export {
   useCommand,
@@ -11,5 +13,7 @@ export {
   useStaticResolver,
   useViewModel,
   useClient,
-  ResolveContext
+  ResolveContext,
+  useCommandBuilder,
+  useQueryBuilder
 }
