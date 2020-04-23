de21feff119cae887a85b17ebf5412b152a505e8
max <mozg.mayhem@gmail.com>
2020-04-23 13:29:37 +0300

    Fix types

--- examples/personal-data/common/read-models/blog-posts.resolvers.ts
+++ examples/personal-data/common/read-models/blog-posts.resolvers.ts
@@ -2,13 +2,15 @@ import { ReadModelResolvers } from 'resolve-core'
 import { ResolveStore } from 'resolve-readmodel-base'
 
 const resolvers: ReadModelResolvers<ResolveStore> = {
-  feedByAuthor: async (store, params) => {
-    return [
-      { title: 'title1 - ' + params.authorId, content: 'content1' },
-      { title: 'title2 - ' + params.authorId, content: 'content2' },
-      { title: 'title3 - ' + params.authorId, content: 'content3' }
-    ]
-    // TODO: return store.find('BlogPosts', { author: params.authorId })
+  feedByAuthor: async (
+    store,
+    params: {
+      authorId: string
+    }
+  ) => {
+    return store.find('BlogPosts', {
+      author: params.authorId
+    })
   }
 }
 
