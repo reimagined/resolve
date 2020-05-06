ec2b2dfbba293c5321ce3b065c6c77ec3d1324e6
max <mozg.mayhem@gmail.com>
2020-05-06 17:35:00 +0300

    Embedding secrets to adapter: drop

--- packages/adapters/storage-adapters/resolve-storage-postgresql-serverless/src/drop.ts
+++ packages/adapters/storage-adapters/resolve-storage-postgresql-serverless/src/drop.ts
@@ -6,12 +6,8 @@ const dropSecretsStore = async (pool: AdapterPool): Promise<any> => {
   const log = getLog('dropSecretsStore')
 
   log.debug(`dropping secrets store database tables`)
-  const { secretsDatabase, secretsTableName, escapeId } = pool
+  const { secretsTableName, escapeId } = pool
   log.verbose(`secretsTableName: ${secretsTableName}`)
-
-  await secretsDatabase.exec(
-    `DROP TABLE IF EXISTS ${escapeId(secretsTableName)}`
-  )
 
   log.debug(`secrets store database tables are dropped`)
 }
@@ -19,8 +15,20 @@ const dropSecretsStore = async (pool: AdapterPool): Promise<any> => {
 const drop = async (pool: AdapterPool): Promise<any> => {
   const log = getLog('drop')
 
+  const { databaseName, tableName, executeStatement, escapeId } = pool
+
+  const createDropEventStorePromise = (): Promise<any> =>
+    databaseName && tableName && executeStatement && escapeId
+      ? dropEventStore({
+          databaseName,
+          tableName,
+          executeStatement,
+          escapeId
+        })
+      : Promise.resolve()
+
   log.debug(`dropping the event store`)
-  await Promise.all([dropEventStore(pool), dropSecretsStore(pool)])
+  await Promise.all([createDropEventStorePromise(), dropSecretsStore(pool)])
   log.debug(`the event store dropped`)
 }
 
