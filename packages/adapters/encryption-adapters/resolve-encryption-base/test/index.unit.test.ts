cec356577c31e7638d47d1ae32a1c2ea61085810
max <mozg.mayhem@gmail.com>
2020-05-18 13:23:15 +0300

    remove encryption adapters

--- packages/adapters/encryption-adapters/resolve-encryption-base/test/index.unit.test.ts
+++
@@ -1,7 +0,0 @@
-import { encrypt, decrypt, forget } from '../src'
-
-describe('event encryption cloud', () => {
-  test('test', async () => {
-    // TODO: later
-  })
-})
--- packages/adapters/encryption-adapters/resolve-encryption-cloud/src/resource/dispose.ts
+++
@@ -1,8 +0,0 @@
-const dispose = async (pool: any, options: any): Promise<void> => {
-  const { destroyResource, createResource } = pool
-
-  await destroyResource(options)
-  await createResource(options)
-}
-
-export default dispose
