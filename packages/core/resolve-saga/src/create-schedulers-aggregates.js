dae0a7711ee45d49bd137dce93e821cc1b63a481
Vladislav Ihost <ihost@mail.ru>
2020-05-25 12:57:06 +0300

    Enhance sagas

--- packages/core/resolve-saga/src/create-schedulers-aggregates.js
+++ packages/core/resolve-saga/src/create-schedulers-aggregates.js
@@ -4,15 +4,24 @@ import createSchedulerAggregateProjection from './scheduler-aggregate-projection
 
 const createSchedulersAggregates = schedulers => {
   const aggregates = []
-  for (const { name: schedulerName, invariantHash, encryption } of schedulers) {
+  for (const { name: schedulerName, invariantHash } of schedulers) {
     const eventTypes = createSchedulerEventTypes({ schedulerName })
 
     aggregates.push({
       name: schedulerName,
       commands: createSchedulerAggregateCommands({ eventTypes }),
       projection: createSchedulerAggregateProjection({ eventTypes }),
+      serializeState: JSON.stringify.bind(JSON),
+      deserializeState: JSON.parse.bind(JSON),
       invariantHash,
-      encryption
+      encryption: () => ({
+        encrypt: () => {
+          throw Error(`encryption disabled, please check your configuration`)
+        },
+        decrypt: () => {
+          throw Error(`encryption disabled, please check your configuration`)
+        }
+      })
     })
   }
 
