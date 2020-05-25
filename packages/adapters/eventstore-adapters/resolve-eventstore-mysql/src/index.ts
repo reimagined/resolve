89e3e2f02154acceded85e4f0f1be34f5792c7e3
max <mozg.mayhem@gmail.com>
2020-05-25 14:12:06 +0300

    Fix index

--- packages/adapters/eventstore-adapters/resolve-eventstore-mysql/src/index.ts
+++ packages/adapters/eventstore-adapters/resolve-eventstore-mysql/src/index.ts
@@ -3,12 +3,10 @@ import { escape, escapeId } from 'mysql2'
 
 import createAdapter from 'resolve-eventstore-base'
 
-import connect from './js/connect'
 import loadEventsByCursor from './js/load-events-by-cursor'
 import loadEventsByTimestamp from './js/load-events-by-timestamp'
 import getLatestEvent from './js/get-latest-event'
 import saveEvent from './js/save-event'
-import init from './js/init'
 import drop from './js/drop'
 import dispose from './js/dispose'
 import paginateEvents from './js/paginate-events'
@@ -16,6 +14,10 @@ import saveEventOnly from './js/save-event-only'
 import freeze from './js/freeze'
 import unfreeze from './js/unfreeze'
 import shapeEvent from './js/shape-event'
+
+import connect from './connect'
+import init from './init'
+
 
 export default createAdapter.bind(null, {
   connect,
