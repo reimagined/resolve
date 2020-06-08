02ab1368cf2cc37fac25b9d32e8cc252092cfaae
Vasily Sokolov <vasiliy.sokolov@devexpress.com>
2020-06-08 15:00:53 +0300

    remove mocking paginate-events

--- packages/adapters/eventstore-adapters/resolve-eventstore-lite/test/index.unit.test.ts
+++ packages/adapters/eventstore-adapters/resolve-eventstore-lite/test/index.unit.test.ts
@@ -14,16 +14,13 @@ import freeze from '../src/js/freeze'
 import unfreeze from '../src/js/unfreeze'
 import getLatestEvent from '../src/js/get-latest-event'
 import saveEvent from '../src/js/save-event'
-
 import saveEventOnly from '../src/js/save-event-only'
-
 import shapeEvent from '../src/js/shape-event'
 import connect from '../src/connect'
 import init from '../src/init'
 import drop from '../src/drop'
 import dispose from '../src/dispose'
 import getSecretsManager from '../src/secrets-manager'
-
 import createAdapter from '../src/index'
 
 jest.mock('../src/js/load-events-by-cursor', () => jest.fn())
@@ -32,7 +29,6 @@ jest.mock('../src/js/unfreeze', () => jest.fn())
 jest.mock('../src/js/get-latest-event', () => jest.fn())
 jest.mock('../src/js/save-event', () => jest.fn())
 jest.mock('../src/js/save-event-only', () => jest.fn())
-jest.mock('../src/js/paginate-events', () => jest.fn())
 jest.mock('../src/js/shape-event', () => jest.fn())
 jest.mock('../src/connect', () => jest.fn())
 jest.mock('../src/init', () => jest.fn())
