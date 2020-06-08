7adc100034ecc0219a7c87fc39a3d5c71f9adc1a
Anton Zhukov <mister.cheater@list.ru>
2020-06-08 15:41:00 +0300

    Improve "build" (#1398)

--- packages/internal/babel-compile/bin/prepare.js
+++ packages/internal/babel-compile/bin/prepare.js
@@ -1,11 +1,19 @@
-const { execSync } = require('child_process')
+const { exec: execCallback } = require('child_process')
 
 const tscPath = require.resolve('typescript/bin/tsc')
+
+const exec = (cmd, opts = {}) =>
+  new Promise((resolve, reject) => {
+    const child = execCallback(cmd, opts, err => (err ? reject('') : resolve()))
+
+    child.stdout.pipe(process.stdout)
+    child.stderr.pipe(process.stderr)
+  })
 
 const prepare = async ({ directory, sourceType }) => {
   if (sourceType === 'ts') {
     try {
-      execSync(`node "${tscPath}"`, { cwd: directory, stdio: 'inherit' })
+      return exec(`node "${tscPath}"`, { cwd: directory })
     } catch (error) {
       throw Error('')
     }
