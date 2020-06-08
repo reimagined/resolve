02ea22f133164df5a86879d165967b3566c2f6d4
mrcheater <mister.cheater@list.ru>
2020-06-08 15:19:50 +0300

    Improve build

--- packages/internal/babel-compile/bin/index.js
+++ packages/internal/babel-compile/bin/index.js
@@ -1,57 +1,78 @@
 #!/usr/bin/env node
 
+const chalk = require('chalk')
 const babel = require('@babel/cli/lib/babel/dir').default
 const { getBabelConfig, getCompileConfigs } = require('@internal/helpers')
 const { prepare } = require('./prepare')
 
 const configs = getCompileConfigs()
 
-for (const config of configs) {
-  const cliOptions = {
-    extensions: config.extensions,
-    outFileExtension: config.outFileExtension,
-    relative: config.relative,
-    filenames: config.filenames,
-    outDir: config.outDir,
-    deleteDirOnStart: config.deleteDirOnStart
-  }
-
-  for (let key in cliOptions) {
-    if (cliOptions[key] === undefined) {
-      delete cliOptions[key]
+async function main() {
+  for (const config of configs) {
+    const cliOptions = {
+      extensions: config.extensions,
+      outFileExtension: config.outFileExtension,
+      relative: config.relative,
+      filenames: config.filenames,
+      outDir: config.outDir,
+      deleteDirOnStart: config.deleteDirOnStart
     }
-  }
 
-  prepare(config)
-    .then(() =>
-      babel({
-        babelOptions: {
-          ...getBabelConfig({
-            sourceType: config.sourceType,
-            moduleType: config.moduleType,
-            moduleTarget: config.moduleTarget
-          }),
-          sourceMaps: true,
-          babelrc: false
-        },
-        cliOptions
-      })
-        .then(() => {
-          // eslint-disable-next-line no-console
-          console.log(
-            `↑ [${config.name}] { moduleType: "${config.moduleType}", moduleType: "${config.moduleTarget}" }`
-          )
+    for (let key in cliOptions) {
+      if (cliOptions[key] === undefined) {
+        delete cliOptions[key]
+      }
+    }
+
+    const promise = prepare(config)
+      .then(() =>
+        babel({
+          babelOptions: {
+            ...getBabelConfig({
+              sourceType: config.sourceType,
+              moduleType: config.moduleType,
+              moduleTarget: config.moduleTarget
+            }),
+            sourceMaps: true,
+            babelrc: false
+          },
+          cliOptions
         })
-        .catch(error => {
+          .then(() => {
+            // eslint-disable-next-line no-console
+            console.log(
+              `↑ [${chalk.green(config.name)}] { moduleType: "${
+                config.moduleType
+              }", moduleType: "${config.moduleTarget}" }`
+            )
+          })
+          .catch(error => {
+            // eslint-disable-next-line no-console
+            console.error(error)
+            process.exit(1)
+          })
+      )
+      .catch(error => {
+        // eslint-disable-next-line no-console
+        console.log(
+          `↑ [${chalk.red(config.name)}] { moduleType: "${
+            config.moduleType
+          }", moduleType: "${config.moduleTarget}" }`
+        )
+        if (error != null && error !== '') {
           // eslint-disable-next-line no-console
           console.error(error)
-          process.exit(1)
-        })
-    )
-    .catch(error => {
-      if (error != null && error !== '') {
-        // eslint-disable-next-line no-console
-        console.error(error)
-      }
-    })
+        }
+      })
+
+    if (config.sync) {
+      await promise
+    }
+  }
 }
+
+main().catch(error => {
+  // eslint-disable-next-line no-console
+  console.error(error)
+  process.exit(1)
+})
--- packages/internal/helpers/get-compile-configs.js
+++ packages/internal/helpers/get-compile-configs.js
@@ -3,6 +3,21 @@ const find = require('glob').sync
 
 const { getResolveDir } = require('./get-resolve-dir')
 
+function changeOrder(configs, packageName) {
+  const includes = configs
+    .map((config, index) => ({ config, index }))
+    .filter(({ config: { name } }) => name === packageName)
+
+  const configLength = configs.length
+  for (const { config, index } of includes) {
+    for (let subIndex = index; subIndex < configLength - 1; subIndex++) {
+      configs[subIndex] = configs[subIndex + 1]
+    }
+    configs[configLength - 1] = config
+    config.sync = true
+  }
+}
+
 let _configs
 const getCompileConfigs = () => {
   if (_configs) {
@@ -72,6 +87,10 @@ const getCompileConfigs = () => {
       configs.push(config)
     }
   }
+
+  changeOrder(configs, 'resolve-core')
+  changeOrder(configs, 'resolve-client')
+  changeOrder(configs, 'resolve-react-hooks')
 
   _configs = configs
 
