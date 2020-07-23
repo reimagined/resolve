f1bc15733614276a088885519addd98760d0c320
timbset <timbset@gmail.com>
2020-07-23 15:59:29 +0300

    Prevent redundant ts compilation in each package

--- packages/internal/babel-compile/bin/index.js
+++ packages/internal/babel-compile/bin/index.js
@@ -7,18 +7,27 @@ const { prepare } = require('./prepare')
 
 const configs = getCompileConfigs()
 
-async function main() {
-  const builds = []
-  const maxParallelBuilds = +process.env.MAX_PARALLEL_BUILDS
+async function compilePackage(config) {
+  try {
+    await prepare(config)
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(`↑ [${chalk.red(config.name)}] preparing failed`)
+    if (e != null && e !== '') {
+      // eslint-disable-next-line no-console
+      console.error(e)
+    }
+    throw e
+  }
 
-  for (const config of configs) {
+  for (const babelConfig of config.babelCompile) {
     const cliOptions = {
-      extensions: config.extensions,
-      outFileExtension: config.outFileExtension,
-      relative: config.relative,
-      filenames: config.filenames,
-      outDir: config.outDir,
-      deleteDirOnStart: config.deleteDirOnStart
+      extensions: babelConfig.extensions,
+      outFileExtension: babelConfig.outFileExtension,
+      relative: babelConfig.relative,
+      filenames: babelConfig.filenames,
+      outDir: babelConfig.outDir,
+      deleteDirOnStart: babelConfig.deleteDirOnStart
     }
 
     for (let key in cliOptions) {
@@ -27,66 +36,37 @@ async function main() {
       }
     }
 
-    if (!isNaN(maxParallelBuilds)) {
-      while (true) {
-        const pendingBuilds = builds.filter((build) => build.pending)
-
-        if (pendingBuilds.length < maxParallelBuilds) {
-          break
-        }
-
-        await Promise.race(pendingBuilds.map((build) => build.promise))
-      }
-    }
-
-    const promise = prepare(config)
-      .then(() =>
-        babel({
-          babelOptions: {
-            ...getBabelConfig({
-              sourceType: config.sourceType,
-              moduleType: config.moduleType,
-              moduleTarget: config.moduleTarget
-            }),
-            sourceMaps: true,
-            babelrc: false
-          },
-          cliOptions
-        })
-          .then(() => {
-            // eslint-disable-next-line no-console
-            console.log(
-              `↑ [${chalk.green(config.name)}] { moduleType: "${
-                config.moduleType
-              }", moduleType: "${config.moduleTarget}" }`
-            )
-          })
-          .catch(error => {
-            // eslint-disable-next-line no-console
-            console.error(error)
-            process.exit(1)
-          })
-      )
-      .catch(error => {
+    babel({
+      babelOptions: {
+        ...getBabelConfig({
+          sourceType: config.sourceType,
+          moduleType: babelConfig.moduleType,
+          moduleTarget: babelConfig.moduleTarget
+        }),
+        sourceMaps: true,
+        babelrc: false
+      },
+      cliOptions
+    })
+      .then(() => {
         // eslint-disable-next-line no-console
         console.log(
-          `↑ [${chalk.red(config.name)}] { moduleType: "${
-            config.moduleType
-          }", moduleType: "${config.moduleTarget}" }`
+          `↑ [${chalk.green(config.name)}] { moduleType: "${
+            babelConfig.moduleType
+          }", moduleType: "${babelConfig.moduleTarget}" }`
         )
-        if (error != null && error !== '') {
-          // eslint-disable-next-line no-console
-          console.error(error)
-        }
       })
+      .catch(error => {
+        // eslint-disable-next-line no-console
+        console.error(error)
+        process.exit(1)
+      })
+  }
+}
 
-    const promiseConfig = {
-      promise,
-      pending: true
-    }
-
-    promise.then(() => (promiseConfig.pending = false))
-    builds.push(promiseConfig)
+async function main() {
+  for (const config of configs) {
+    const promise = compilePackage(config)
 
     if (config.sync || process.env.RESOLVE_ALLOW_PARALLEL_BUILDS != null) {
       await promise
--- packages/internal/helpers/get-compile-configs.js
+++ packages/internal/helpers/get-compile-configs.js
@@ -16,6 +16,11 @@ function makeSyncAtEnd(configs, packageName) {
   }
 }
 
+const externalDependencies = ['resolve-cloud-common']
+
+const isInternalDependency = (name) => name.startsWith('resolve-')
+  && !externalDependencies.includes(name)
+
 let _configs
 const getCompileConfigs = () => {
   if (_configs) {
@@ -39,51 +44,62 @@ const getCompileConfigs = () => {
       continue
     }
     if (
-      filePath.includes(`optional\\${'dependencies'}`) ||
-      filePath.includes(`optional/${'dependencies'}`)
+      filePath.includes('optional\\dependencies') ||
+      filePath.includes('optional/dependencies')
     ) {
       continue
     }
 
-    const { version, name, babelCompile } = require(filePath)
+    const { version, name, sourceType, babelCompile, dependencies = {} } = require(filePath)
 
     if (!Array.isArray(babelCompile)) {
       throw new Error(`[${name}] package.json "babelCompile" must be an array`)
     }
 
+    const config = {
+      name,
+      version,
+      babelCompile,
+      directory: path.dirname(filePath),
+      sourceType,
+      dependencies: Object.keys(dependencies).reduce((acc, dependencyName) => (
+        isInternalDependency(dependencyName) ? acc.concat(dependencyName) : acc
+      ), [])
+    }
+
+    if (config.sourceType == null) {
+      config.sourceType = 'js'
+    }
+
+    if (config.sourceType.constructor !== String) {
+      throw new Error(`.sourceType must be a string`)
+    }
+
     for (let index = 0; index < babelCompile.length; index++) {
-      const config = babelCompile[index]
-      if (config.sourceType == null) {
-        config.sourceType = 'js'
-      }
-      if (config.sourceType.constructor !== String) {
-        throw new Error(`.babelCompile[${index}].sourceType must be a string`)
-      }
-      if (config.moduleType.constructor !== String) {
+      const babelConfig = babelCompile[index]
+
+      if (babelConfig.moduleType.constructor !== String) {
         throw new Error(`.babelCompile[${index}].moduleType must be a string`)
       }
-      if (config.moduleTarget.constructor !== String) {
+      if (babelConfig.moduleTarget.constructor !== String) {
         throw new Error(`.babelCompile[${index}].moduleTarget must be a string`)
       }
-      if (config.inputDir.constructor !== String) {
+      if (babelConfig.inputDir.constructor !== String) {
         throw new Error(`.babelCompile[${index}].inputDir must be a string`)
       }
-      if (config.outDir.constructor !== String) {
+      if (babelConfig.outDir.constructor !== String) {
         throw new Error(`.babelCompile[${index}].outDir must be a string`)
       }
 
-      config.name = name
-      config.version = version
-      config.directory = path.dirname(filePath)
-      config.inputDir = path.join(config.directory, config.inputDir)
-      config.outDir = path.join(config.directory, config.outDir)
-      config.outFileExtension = config.moduleType === 'mjs' ? '.mjs' : '.js'
-      config.extensions = config.sourceType === 'ts' ? ['.ts', '.js'] : '.js'
-      config.deleteDirOnStart = false
-      config.filenames = [config.inputDir]
-
-      configs.push(config)
+      babelConfig.inputDir = path.join(config.directory, babelConfig.inputDir)
+      babelConfig.outDir = path.join(config.directory, babelConfig.outDir)
+      babelConfig.outFileExtension = babelConfig.moduleType === 'mjs' ? '.mjs' : '.js'
+      babelConfig.extensions = config.sourceType === 'ts' ? ['.ts', '.js'] : '.js'
+      babelConfig.deleteDirOnStart = false
+      babelConfig.filenames = [babelConfig.inputDir]
     }
+
+    configs.push(config)
   }
 
   makeSyncAtEnd(configs, 'resolve-core')
