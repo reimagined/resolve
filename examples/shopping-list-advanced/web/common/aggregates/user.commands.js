e3b041800f96067344fddd37a4e7748a5a56eabf
max <mozg.mayhem@gmail.com>
2020-08-03 23:41:25 +0300

    Oops

--- examples/shopping-list-advanced/web/common/aggregates/user.commands.js
+++ examples/shopping-list-advanced/web/common/aggregates/user.commands.js
@@ -5,8 +5,8 @@ import validation from './validation'
 import { USER_CREATED, USER_NAME_UPDATED } from '../event-types'
 
 export default {
-  createUser: (state, command, { jwt }) => {
-    const jwt = JWT.verify(jwt, jwtSecret)
+  createUser: (state, command, { jwt: token }) => {
+    const jwt = JWT.verify(token, jwtSecret)
 
     validation.stateIsAbsent(state, 'User')
     validation.fieldRequired(command.payload, 'username')
