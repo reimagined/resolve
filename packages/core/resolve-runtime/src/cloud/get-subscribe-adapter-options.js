0779c789d77ebc408a81d98a348470907e21ff4e
Lykoi18 <aiden.ghost666@gmail.com>
2020-08-13 17:05:50 +0300

    Update cloud token

--- packages/core/resolve-runtime/src/cloud/get-subscribe-adapter-options.js
+++ packages/core/resolve-runtime/src/cloud/get-subscribe-adapter-options.js
@@ -1,13 +1,21 @@
 import jwt from 'jsonwebtoken'
 
-const getSubscribeAdapterOptions = async (resolve, origin, topics) => {
+const getSubscribeAdapterOptions = async (
+  resolve,
+  origin,
+  eventTypes,
+  aggregateIds
+) => {
   const {
     RESOLVE_DEPLOYMENT_ID,
     RESOLVE_WS_URL,
     RESOLVE_ENCRYPTED_DEPLOYMENT_ID
   } = process.env
 
-  const token = jwt.sign({ topics }, RESOLVE_ENCRYPTED_DEPLOYMENT_ID)
+  const token = jwt.sign(
+    { eventTypes, aggregateIds },
+    RESOLVE_ENCRYPTED_DEPLOYMENT_ID
+  )
 
   const subscribeUrl = `${RESOLVE_WS_URL}?deploymentId=${RESOLVE_DEPLOYMENT_ID}&token=${token}`
 
