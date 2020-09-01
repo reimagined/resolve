7920633573d20d8a13532e8688b69400bcd58d79
mrcheater <mister.cheater@list.ru>
2020-09-01 16:31:01 +0300

    Add "shutdown" to resolve-runtime

--- packages/core/resolve-runtime/src/local/start-express.js
+++ packages/core/resolve-runtime/src/local/start-express.js
@@ -1,6 +1,10 @@
 import initResolve from '../common/init-resolve'
 import disposeResolve from '../common/dispose-resolve'
 import bootstrap from './bootstrap'
+import invokeFilterErrorTypes from '../common/utils/invoke-filter-error-types'
+import { EventstoreResourceAlreadyExistError } from 'resolve-eventstore-base'
+import { PublisherResourceAlreadyExistError } from 'resolve-local-event-broker'
+import shutdown from '../common/shutdown'
 
 const host = '0.0.0.0'
 const startExpress = async resolve => {
@@ -15,7 +19,17 @@ const startExpress = async resolve => {
   const currentResolve = Object.create(resolve)
   try {
     await initResolve(currentResolve)
-    await bootstrap(currentResolve)
+    const { eventstoreAdapter, publisher } = currentResolve
+
+    await invokeFilterErrorTypes(
+      eventstoreAdapter.init.bind(eventstoreAdapter),
+      [EventstoreResourceAlreadyExistError]
+    )
+    await invokeFilterErrorTypes(publisher.init.bind(publisher), [
+      PublisherResourceAlreadyExistError
+    ])
+
+    await bootstrap(currentResolve, upstream)
 
     const notReadyListeners = new Set([...resolve.eventListeners.keys()])
 
@@ -52,6 +66,16 @@ const startExpress = async resolve => {
       return resolve()
     })
   )
+
+  server.on('exit', async () => {
+    try {
+      const currentResolve = Object.create(resolve)
+      await initResolve(currentResolve)
+      await shutdown(currentResolve, upstream)
+    } finally {
+      await disposeResolve(currentResolve)
+    }
+  })
 
   server.on('error', err => {
     throw err
