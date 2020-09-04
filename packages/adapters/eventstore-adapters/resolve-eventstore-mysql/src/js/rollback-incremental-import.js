1bf2e8e72c34e1b12deb4c207fa87a696a56540c
mrcheater <mister.cheater@list.ru>
2020-09-04 11:27:41 +0300

    Fix #1504 ```js const eventstoreAdapter = request.resolve.eventstoreAdapter; try { 	await eventstoreAdapter.commitIncrementalImport(importId); } catch (error) { 	await eventstoreAdapter.rollbackIncrementalImport(); 	throw error; } ``` When the eventstoreAdapter.commitIncrementalImport call is failed by any reason, the eventstoreAdapter.rollbackIncrementalImport call raises the following exception: "SQLITE_ERROR: no such table: events-incremental-import".

--- packages/adapters/eventstore-adapters/resolve-eventstore-mysql/src/js/rollback-incremental-import.js
+++ packages/adapters/eventstore-adapters/resolve-eventstore-mysql/src/js/rollback-incremental-import.js
@@ -1,23 +1,11 @@
-import { ER_NO_SUCH_TABLE } from './constants'
-
 const rollbackIncrementalImport = async ({
   events: { eventsTableName, connection },
   escapeId
 }) => {
-  try {
-    const incrementalImportTableAsId = escapeId(
-      `${eventsTableName}-incremental-import`
-    )
-    await connection.query(`DROP TABLE ${incrementalImportTableAsId};`)
-  } catch (error) {
-    const errno = error != null && error.errno != null ? error.errno : 0
-
-    if (errno === ER_NO_SUCH_TABLE) {
-      throw new Error(`Previous incremental import does not exist`)
-    } else {
-      throw error
-    }
-  }
+  const incrementalImportTableAsId = escapeId(
+    `${eventsTableName}-incremental-import`
+  )
+  await connection.query(`DROP TABLE IF EXISTS ${incrementalImportTableAsId};`)
 }
 
 export default rollbackIncrementalImport
