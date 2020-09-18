5fde5b6edfe410558e1756a1187343edfead9b15
Max Vasin <maksim.vasin@devexpress.com>
2020-09-18 13:06:28 +0300

    api runner

--- functional-tests/api/resolve-client/get-client.ts
+++ functional-tests/api/resolve-client/get-client.ts
@@ -1,12 +1,12 @@
 import { getClient as getClientInternal, Context } from 'resolve-client'
 
-const origin = process.env.RESOLVE_API_TESTS_ORIGIN || 'http://0.0.0.0:3000'
+const origin = process.env.RESOLVE_API_TESTS_TARGET_URL || 'http://0.0.0.0:3000'
 
 const context: Context = {
   viewModels: [],
   origin,
   rootPath: '',
-  staticPath: 'static'
+  staticPath: 'static',
 }
 
 export const getClient = getClientInternal.bind(null, context)
