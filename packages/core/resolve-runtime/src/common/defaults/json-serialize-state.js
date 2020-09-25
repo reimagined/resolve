c17eb1a48d3aacb363b86e25b40f5d0af1a47d9c
Max Vasin <maksim.vasin@devexpress.com>
2020-09-25 12:51:34 +0300

    Add built-in flag to default serializers

--- packages/core/resolve-runtime/src/common/defaults/json-serialize-state.js
+++ packages/core/resolve-runtime/src/common/defaults/json-serialize-state.js
@@ -1,5 +1,9 @@
+import { IS_BUILT_IN } from 'resolve-core'
+
 function serializeState(state) {
   return JSON.stringify(state, null, 2)
 }
+
+serializeState[IS_BUILT_IN] = true
 
 export default serializeState
