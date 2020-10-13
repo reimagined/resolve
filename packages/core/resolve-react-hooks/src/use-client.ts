ba95741ac485c5fe7e3dd36dd6039b32b2c7e3ca
Max Vasin <maksim.vasin@devexpress.com>
2020-10-13 16:36:35 +0300

    Context assertion

--- packages/core/resolve-react-hooks/src/use-client.ts
+++ packages/core/resolve-react-hooks/src/use-client.ts
@@ -1,12 +1,10 @@
 import { useContext, useMemo } from 'react'
 import { Client, getClient } from 'resolve-client'
-import { ResolveContext } from './context'
+import { assertContext, ResolveContext } from './context'
 
 const useClient = (): Client => {
   const context = useContext(ResolveContext)
-  if (context == null) {
-    throw Error('ResolveContext.Provider value is required to use hooks')
-  }
+  assertContext(context)
   return useMemo(() => getClient(context), [context])
 }
 
