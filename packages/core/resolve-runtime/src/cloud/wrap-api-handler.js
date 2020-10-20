fd52a3abf1da7afe1fbb3e48bdd6d57f9f58fda6
Alexander Safronov (DevExpress) <safronov.alexander@corp.devexpress.com>
2020-10-20 16:47:44 +0300

    Update cloud wrap handler

--- packages/core/resolve-runtime/src/cloud/wrap-api-handler.js
+++ packages/core/resolve-runtime/src/cloud/wrap-api-handler.js
@@ -1,140 +1,55 @@
 import binaryCase from 'binary-case'
 import contentDisposition from 'content-disposition'
 import cookie from 'cookie'
+import { parse as parseQuery } from 'query-string'
+import { getReasonPhrase } from 'http-status-codes'
 
 const COOKIE_CLEAR_DATE = new Date(0)
-const INTERNAL = Symbol('INTERNAL')
-const isTrailingBracket = /\[\]$/
-
-const normalizeKey = (key, mode) => {
-  switch (mode) {
-    case 'upper-dash-case':
-      return key
-        .toLowerCase()
-        .split(/-/g)
-        .map((part) => `${part.charAt(0).toUpperCase()}${part.slice(1)}`)
-        .join('-')
-    case 'dash-case':
-      return `${key.charAt(0).toUpperCase()}${key.slice(1).toLowerCase()}`
-    case 'lower-case':
-      return key.toLowerCase()
-    default:
-      throw new Error(`Wrong normalize mode ${mode}`)
-  }
-}
-
-const wrapHeadersCaseInsensitive = (headersMap) =>
-  Object.create(
-    Object.prototype,
-    Object.keys(headersMap).reduce((acc, key) => {
-      const value = headersMap[key]
-      const [upperDashKey, dashKey, lowerKey] = [
-        normalizeKey(key, 'upper-dash-case'),
-        normalizeKey(key, 'dash-case'),
-        normalizeKey(key, 'lower-case'),
-      ]
-
-      acc[upperDashKey] = { value, enumerable: true }
-      if (upperDashKey !== dashKey) {
-        acc[dashKey] = { value, enumerable: false }
-      }
-      acc[lowerKey] = { value, enumerable: false }
-
-      return acc
-    }, {})
-  )
-
-const createRequest = async (lambdaEvent, customParameters) => {
-  let {
-    path,
+const createRequest = async (lambdaEvent, context, customParameters) => {
+  const {
+    uri,
     httpMethod,
-    headers: { 'x-proxy-headers': proxyHeadersString, ...originalHeaders },
-    multiValueQueryStringParameters,
+    headers: originalHeaders,
+    querystring,
     body,
   } = lambdaEvent
-
-  if (proxyHeadersString != null) {
-    for (const [headerName, headerValue] of Object.entries(
-      JSON.parse(proxyHeadersString)
-    )) {
-      const normalizedHeaderName = normalizeKey(headerName, 'upper-dash-case')
-      if (headerName.toLowerCase() === 'x-uri') {
-        path = headerValue
-      } else {
-        for (const originalHeaderName of Object.keys(originalHeaders)) {
-          if (
-            normalizeKey(originalHeaderName, 'upper-dash-case') ===
-            normalizedHeaderName
-          ) {
-            delete originalHeaders[originalHeaderName]
-          }
-        }
-
-        originalHeaders[normalizedHeaderName] = headerValue
-      }
-    }
-  }
-
-  const headers = wrapHeadersCaseInsensitive(originalHeaders)
-  const cookies =
-    headers.cookie != null && headers.cookie.constructor === String
-      ? cookie.parse(headers.cookie)
-      : {}
-
-  const req = Object.create(null)
-
-  const query =
-    multiValueQueryStringParameters != null
-      ? multiValueQueryStringParameters
-      : {}
-  for (const [queryKey, queryValue] of Object.entries(query)) {
-    if (isTrailingBracket.test(queryKey)) {
-      delete query[queryKey]
-      query[queryKey.replace(isTrailingBracket, '')] = queryValue
-    } else if (queryValue.length === 1) {
-      query[queryKey] = queryValue[0]
-    }
-  }
-
-  const reqProperties = {
-    adapter: 'awslambda',
+  const cookieHeader = originalHeaders.find(
+    (item) => item.key.toLowerCase() === 'cookie'
+  )
+  const headers = originalHeaders
+    .filter((item) => item !== cookieHeader)
+    .reduce((acc, item) => {
+      acc[item.key.toLowerCase()] = item.value
+      return acc
+    }, {})
+  const cookies = cookieHeader != null ? cookie.parse(cookieHeader.value) : {}
+  const query = parseQuery(querystring, { arrayFormat: 'bracket' })
+  const req = {
+    context,
     method: httpMethod,
+    path: uri,
     query,
-    path,
     headers,
     cookies,
-    body,
+    body: Buffer.from(body, 'base64').toString(),
     ...customParameters,
   }
-
-  for (const name of Object.keys(reqProperties)) {
-    Object.defineProperty(req, name, {
-      enumerable: true,
-      get: () => reqProperties[name],
-      set: () => {
-        throw new Error(`Request parameters can't be modified`)
-      },
-    })
-  }
-
-  return Object.freeze(req)
+  Object.freeze(req)
+  return req
 }
-
 const createResponse = () => {
-  const internalRes = {
+  const internal = {
     status: 200,
     headers: {},
     cookies: [],
     body: '',
     closed: false,
   }
-
   const validateResponseOpened = () => {
-    if (internalRes.closed) {
+    if (internal.closed) {
       throw new Error('Response already sent')
     }
   }
-
   const validateOptionShape = (fieldName, option, types, nullable = false) => {
     const isValidValue =
       (nullable && option == null) ||
@@ -150,159 +65,156 @@ const createResponse = () => {
       )
     }
   }
-
-  const res = Object.create(null, { [INTERNAL]: { value: internalRes } })
-
-  const defineResponseMethod = (name, value) =>
-    Object.defineProperty(res, name, {
-      enumerable: true,
-      value,
-    })
-
-  defineResponseMethod('cookie', (name, value, options) => {
-    validateResponseOpened()
-    const serializedCookie = cookie.serialize(name, value, options)
-
-    internalRes.cookies.push(serializedCookie)
-    return res
-  })
-
-  defineResponseMethod('clearCookie', (name, options) => {
-    validateResponseOpened()
-    const serializedCookie = cookie.serialize(name, '', {
-      ...options,
-      expires: COOKIE_CLEAR_DATE,
-    })
-
-    internalRes.cookies.push(serializedCookie)
-    return res
-  })
-
-  defineResponseMethod('status', (code) => {
-    validateResponseOpened()
-    validateOptionShape('Status code', code, [Number])
-    internalRes.status = code
-    return res
-  })
-
-  defineResponseMethod('redirect', (path, code) => {
-    validateResponseOpened()
-    validateOptionShape('Status code', code, [Number], true)
-    validateOptionShape('Location path', path, [String])
-    internalRes.headers['Location'] = path
-    internalRes.status = code != null ? code : 302
-
-    internalRes.closed = true
-    return res
-  })
-
-  defineResponseMethod('getHeader', (searchKey) => {
-    validateOptionShape('Header name', searchKey, [String])
-    const normalizedKey = normalizeKey(searchKey, 'upper-dash-case')
-    return internalRes.headers[normalizedKey]
-  })
-
-  defineResponseMethod('setHeader', (key, value) => {
-    validateResponseOpened()
-    validateOptionShape('Header name', key, [String])
-    validateOptionShape('Header value', value, [String])
-
-    internalRes.headers[normalizeKey(key, 'upper-dash-case')] = value
-    return res
-  })
-
-  defineResponseMethod('text', (content, encoding) => {
-    validateResponseOpened()
-    validateOptionShape('Text', content, [String])
-    validateOptionShape('Encoding', encoding, [String], true)
-    internalRes.body = Buffer.from(content, encoding)
-    internalRes.closed = true
-    return res
-  })
-
-  defineResponseMethod('json', (content) => {
-    validateResponseOpened()
-    internalRes.headers[normalizeKey('Content-Type', 'upper-dash-case')] =
-      'application/json'
-    internalRes.body = JSON.stringify(content)
-    internalRes.closed = true
-    return res
-  })
-
-  defineResponseMethod('end', (content = '', encoding) => {
-    validateResponseOpened()
-    validateOptionShape('Content', content, [String, Buffer])
-    validateOptionShape('Encoding', encoding, [String], true)
-    internalRes.body =
-      content.constructor === String ? Buffer.from(content, encoding) : content
-
-    internalRes.closed = true
-    return res
-  })
-
-  defineResponseMethod('file', (content, filename, encoding) => {
-    validateResponseOpened()
-    validateOptionShape('Content', content, [String, Buffer])
-    validateOptionShape('Encoding', encoding, [String], true)
-    internalRes.body =
-      content.constructor === String ? Buffer.from(content, encoding) : content
-
-    internalRes.headers['Content-Disposition'] = contentDisposition(filename)
-
-    internalRes.closed = true
-    return res
-  })
-
-  return Object.freeze(res)
+  const res = {
+    INTERNAL: internal,
+    cookie(name, value, options) {
+      validateResponseOpened()
+      const serializedCookie = cookie.serialize(name, value, options)
+      internal.cookies.push(serializedCookie)
+      return res
+    },
+    clearCookie(name, options) {
+      validateResponseOpened()
+      const serializedCookie = cookie.serialize(
+        name,
+        '',
+        Object.assign(Object.assign({}, options), {
+          expires: COOKIE_CLEAR_DATE,
+        })
+      )
+      internal.cookies.push(serializedCookie)
+      return res
+    },
+    status(code) {
+      validateResponseOpened()
+      validateOptionShape('Status code', code, [Number])
+      internal.status = code
+      return res
+    },
+    redirect(path, code) {
+      validateResponseOpened()
+      validateOptionShape('Status code', code, [Number], true)
+      validateOptionShape('Location path', path, [String])
+      internal.headers.Location = path
+      internal.status = code != null ? code : 302
+      internal.closed = true
+      return res
+    },
+    getHeader(searchKey) {
+      var _a
+      validateOptionShape('Header name', searchKey, [String])
+      return (_a = internal.headers[searchKey]) !== null && _a !== void 0
+        ? _a
+        : null
+    },
+    setHeader(key, value) {
+      validateResponseOpened()
+      validateOptionShape('Header name', key, [String])
+      validateOptionShape('Header value', value, [String])
+      internal.headers[key] = value
+      return res
+    },
+    text(content, encoding) {
+      validateResponseOpened()
+      validateOptionShape('Text', content, [String])
+      validateOptionShape('Encoding', encoding, [String], true)
+      internal.body = Buffer.from(content, encoding)
+      internal.closed = true
+      return res
+    },
+    json(content) {
+      validateResponseOpened()
+      internal.headers['Content-Type'] = 'application/json'
+      internal.body = JSON.stringify(content)
+      internal.closed = true
+      return res
+    },
+    end(content = '', encoding) {
+      validateResponseOpened()
+      validateOptionShape('Content', content, [String, Buffer])
+      validateOptionShape('Encoding', encoding, [String], true)
+      internal.body =
+        content.constructor === String
+          ? Buffer.from(content, encoding)
+          : content
+      internal.closed = true
+      return res
+    },
+    file(content, filename, encoding) {
+      validateResponseOpened()
+      validateOptionShape('Content', content, [String, Buffer])
+      validateOptionShape('Encoding', encoding, [String], true)
+      internal.body =
+        content.constructor === String
+          ? Buffer.from(content, encoding)
+          : content
+      internal.headers['Content-Disposition'] = contentDisposition(filename)
+      internal.closed = true
+      return res
+    },
+  }
+  Object.freeze(res)
+  return res
 }
 
-const wrapApiHandler = (handler, getCustomParameters) => async (
+const wrapClientApiHandler = async (
+  handler,
+  getCustomParameters,
   lambdaEvent,
-  lambdaContext,
-  lambdaCallback
+  lambdaContext
 ) => {
-  let result
   try {
     const customParameters =
       typeof getCustomParameters === 'function'
-        ? await getCustomParameters(lambdaEvent, lambdaContext, lambdaCallback)
+        ? await getCustomParameters(lambdaEvent, lambdaContext)
         : {}
 
-    const req = await createRequest(lambdaEvent, customParameters)
+    const req = await createRequest(
+      lambdaEvent,
+      lambdaContext,
+      customParameters
+    )
     const res = createResponse()
-
     await handler(req, res)
+    const {
+      status: httpStatus,
+      headers: { ...internalHeaders },
+      cookies,
+      body: internalBody,
+    } = res.INTERNAL
 
-    const { status: statusCode, headers, cookies, body: bodyBuffer } = res[
-      INTERNAL
-    ]
-    const body = bodyBuffer.toString()
-
+    const body = Buffer.from(internalBody).toString('base64')
+    // TODO @IhostVlad binaryCase
     for (let idx = 0; idx < cookies.length; idx++) {
-      headers[binaryCase('Set-cookie', idx)] = cookies[idx]
+      internalHeaders[binaryCase('Set-cookie', idx)] = cookies[idx]
     }
-
-    result = { statusCode, headers, body }
+    const headers = Object.entries(internalHeaders).map(([key, value]) => ({
+      key,
+      value,
+    }))
+    return {
+      httpStatus,
+      httpStatusText: getReasonPhrase(httpStatus),
+      headers,
+      body,
+    }
   } catch (error) {
     const outError =
       error != null && error.stack != null
         ? `${error.stack}`
         : `Unknown error ${error}`
-
     // eslint-disable-next-line no-console
     console.error(outError)
-
-    result = {
-      statusCode: 500,
+    return {
+      httpStatus: 500,
+      httpStatusText: getReasonPhrase(500),
       body: '',
+      headers: [],
     }
   }
-
-  if (typeof lambdaCallback === 'function') {
-    return lambdaCallback(null, result)
-  } else {
-    return result
-  }
 }
+
+const wrapApiHandler = (handler, getCustomParameters) =>
+  wrapClientApiHandler.bind(null, handler, getCustomParameters)
 
 export default wrapApiHandler
