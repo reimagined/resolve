4e57dea24f98938ca0dfdc560205d4a9d7cb350d
Max Vasin <maksim.vasin@devexpress.com>
2020-10-23 15:00:13 +0300

    Fix names

--- functional-tests/app/client/redux-hooks/components/App.js
+++ functional-tests/app/client/redux-hooks/components/App.js
@@ -5,7 +5,7 @@ import Header from './Header'
 const App = ({ children }) => (
   <div>
     <Header
-      title="HOC tests"
+      title="Redux Hooks Tests"
       favicon="/favicon.ico"
       css={['/bootstrap.min.css', '/fontawesome.min.css', '/style.css']}
     />
