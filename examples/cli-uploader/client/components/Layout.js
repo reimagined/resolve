4b284c7f156dd6dcabcf79aea124c850382bbad4
Max Vasin <maksim.vasin@devexpress.com>
2020-10-29 14:08:36 +0300

    Refactor cli-uploader

--- examples/cli-uploader/client/components/Layout.js
+++ examples/cli-uploader/client/components/Layout.js
@@ -1,48 +1,53 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import { useStaticResolver } from 'resolve-react-hooks'
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink, Button } from 'reactstrap'
 
 import Form from '../containers/Form'
 
-export default ({ children, staticPath, jwt }) => (
-  <div>
-    <Helmet>
-      <title>reSolve cli uploader</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="stylesheet" href={`${staticPath}/bootstrap.min.css`} />
-      <link rel="icon" href={`${staticPath}/favicon.ico`} />
-    </Helmet>
+export default ({ children, staticPath, jwt }) => {
+  const staticResolver = useStaticResolver()
 
-    <Navbar color="light" light expand="md">
-      <NavbarBrand className="mr-auto" href="/">
-        <img src={`${staticPath}/resolve-logo.png`} alt="resolve-logo" />
-        Cli uploader
-      </NavbarBrand>
+  return (
+    <div>
+      <Helmet>
+        <title>reSolve cli uploader</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href={`${staticPath}/bootstrap.min.css`} />
+        <link rel="icon" href={`${staticPath}/favicon.ico`} />
+      </Helmet>
 
-      <Nav navbar>
-        <Form method="post" action="/api/logout">
-          <Button hidden={jwt == null} color="link">
-            logout
-          </Button>
-        </Form>
-        <NavItem>
-          <NavLink href="https://facebook.com/resolvejs/">
-            <img src={`${staticPath}/fb-logo.png`} alt="fb-logo" />
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="https://twitter.com/resolvejs">
-            <img src={`${staticPath}/twitter-logo.png`} alt="twitter-logo" />
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="https://github.com/reimagined/resolve">
-            <img src={`${staticPath}/github-logo.png`} alt="github-logo" />
-          </NavLink>
-        </NavItem>
-      </Nav>
-    </Navbar>
-    <br />
-    {children}
-  </div>
-)
+      <Navbar color="light" light expand="md">
+        <NavbarBrand className="mr-auto" href="/">
+          <img src={`${staticPath}/resolve-logo.png`} alt="resolve-logo" />
+          Cli uploader
+        </NavbarBrand>
+
+        <Nav navbar>
+          <Form method="post" action="/api/logout">
+            <Button hidden={jwt == null} color="link">
+              logout
+            </Button>
+          </Form>
+          <NavItem>
+            <NavLink href="https://facebook.com/resolvejs/">
+              <img src={staticResolver(`/fb-logo.png`)} alt="fb-logo" />
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="https://twitter.com/resolvejs">
+              <img src={staticResolver(`/twitter-logo.png`)} alt="twitter-logo" />
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="https://github.com/reimagined/resolve">
+              <img src={staticResolver(`/github-logo.png`)} alt="github-logo" />
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+      <br />
+      {children}
+    </div>
+  )
+}
