86b2df5026d885ea1d7757ae970a5d37a8cde00c
Max Vasin <maksim.vasin@devexpress.com>
2020-11-13 00:56:15 +0300

    Lint

--- packages/core/resolve-client/test/func/client.func.test.ts
+++ packages/core/resolve-client/test/func/client.func.test.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 import fetch from 'node-fetch'
 import express from 'express'
 import {
--- packages/core/resolve-client/test/func/refresh-token-middleware.ts
+++ packages/core/resolve-client/test/func/refresh-token-middleware.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 import {
   RequestMiddleware,
   RequestMiddlewareParameters,
