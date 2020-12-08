29aaa8f3bbc81162674acfc40e01f95e80455d66
timbset <timbset@gmail.com>
2020-12-08 14:14:00 +0300

    Add extra utils for more specific metrics collecting

--- packages/core/resolve-runtime/src/cloud/metrics.js
+++ packages/core/resolve-runtime/src/cloud/metrics.js
@@ -84,6 +84,322 @@ export const putDurationMetrics = async (
   }
 }
 
+const getErrorMessage = (error) => {
+  let errorMessage = error.message
+
+  if (errorMessage.length > MAX_METRICS_DIMENSION_VALUE_LENGTH) {
+    const messageEnd = '...'
+
+    errorMessage = `${errorMessage.slice(
+      0,
+      MAX_METRICS_DIMENSION_VALUE_LENGTH - messageEnd.length
+    )}${messageEnd}`
+  }
+
+  return errorMessage
+}
+
+const putDataMetrics = async (dataMap, commonMap, errorMap) => {
+  const log = debugLevels('resolve:resolve-runtime:cloud-entry:putDataMetrics')
+  const cw = new CloudWatch()
+
+  if (dataMap.deploymentId == null) {
+    log.warn('Deployment ID not found')
+    return
+  }
+
+  const now = new Date()
+  const metricName = dataMap.Error != null ? 'Errors' : 'Executions'
+
+  const metricData = commonMap.map((dimensionNames) => ({
+    MetricName: metricName,
+    Timestamp: now,
+    Unit: 'count',
+    Value: 1,
+    Dimensions: dimensionNames.map((name) => ({
+      Name: name,
+      Value: dataMap[name],
+    })),
+  }))
+
+  if (dataMap.Error != null) {
+    metricData.push(
+      ...errorMap.map((dimensionNames) => ({
+        MetricName: 'Errors',
+        Timestamp: now,
+        Unit: 'count',
+        Value: 1,
+        Dimensions: dimensionNames.map((name) => ({
+          Name: name,
+          Value: dataMap[name],
+        })),
+      }))
+    )
+  }
+
+  try {
+    await cw
+      .putMetricData({
+        Namespace: 'RESOLVE_METRICS',
+        MetricData: metricData,
+      })
+      .promise()
+
+    log.verbose('Put metrics succeeded')
+  } catch (e) {
+    log.verbose('Put metrics failed')
+    log.warn(e)
+  }
+}
+
+export const putCommandMetrics = async (
+  aggregateName,
+  commandType,
+  aggregateId,
+  error
+) => {
+  const metricDataMap = {
+    DeploymentId: process.env.RESOLVE_DEPLOYMENT_ID,
+    Part: 'Command',
+    AggregateName: aggregateName,
+    CommandType: commandType,
+    AggregateId: aggregateId,
+  }
+
+  if (error != null) {
+    metricDataMap.Error = getErrorMessage(error)
+  }
+
+  return putDataMetrics(
+    metricDataMap,
+    [
+      ['DeploymentId'],
+      ['DeploymentId', 'Part'],
+      ['DeploymentId', 'Part', 'AggregateName'],
+      ['DeploymentId', 'Part', 'AggregateName', 'CommandType'],
+      ['DeploymentId', 'Part', 'AggregateName', 'CommandType', 'AggregateId'],
+    ],
+    [
+      ['DeploymentId', 'Error'],
+      ['DeploymentId', 'Part', 'Error'],
+      [
+        'DeploymentId',
+        'Part',
+        'AggregateName',
+        'CommandType',
+        'AggregateId',
+        'Error',
+      ],
+    ]
+  )
+}
+
+export const putReadModelProjectionMetrics = async (
+  readModelName,
+  eventType,
+  error
+) => {
+  const metricDataMap = {
+    DeploymentId: process.env.RESOLVE_DEPLOYMENT_ID,
+    Part: 'ReadModelProjection',
+    ReadModel: readModelName,
+    EventType: eventType,
+  }
+
+  if (error != null) {
+    metricDataMap.Error = getErrorMessage(error)
+  }
+
+  return putDataMetrics(
+    metricDataMap,
+    [
+      ['DeploymentId'],
+      ['DeploymentId', 'Part'],
+      ['DeploymentId', 'Part', 'ReadModel'],
+      ['DeploymentId', 'Part', 'ReadModel', 'EventType'],
+    ],
+    [
+      ['DeploymentId', 'Error'],
+      ['DeploymentId', 'Part', 'Error'],
+      ['DeploymentId', 'Part', 'ReadModel', 'EventType', 'Error'],
+    ]
+  )
+}
+
+export const putReadModelResolverMetrics = async (
+  readModelName,
+  resolverName,
+  error
+) => {
+  const metricDataMap = {
+    DeploymentId: process.env.RESOLVE_DEPLOYMENT_ID,
+    Part: 'ReadModelResolver',
+    ReadModel: readModelName,
+    Resolver: resolverName,
+  }
+
+  if (error != null) {
+    metricDataMap.Error = getErrorMessage(error)
+  }
+
+  return putDataMetrics(
+    metricDataMap,
+    [
+      ['DeploymentId'],
+      ['DeploymentId', 'Part'],
+      ['DeploymentId', 'Part', 'ReadModel'],
+      ['DeploymentId', 'Part', 'ReadModel', 'Resolver'],
+    ],
+    [
+      ['DeploymentId', 'Error'],
+      ['DeploymentId', 'Part', 'Error'],
+      ['DeploymentId', 'Part', 'ReadModel', 'Resolver', 'Error'],
+    ]
+  )
+}
+
+export const putReadModelResolverMetrics = async (
+  readModelName,
+  resolverName,
+  error
+) => {
+  const metricDataMap = {
+    DeploymentId: process.env.RESOLVE_DEPLOYMENT_ID,
+    Part: 'ReadModelResolver',
+    ReadModel: readModelName,
+    Resolver: resolverName,
+  }
+
+  if (error != null) {
+    metricDataMap.Error = getErrorMessage(error)
+  }
+
+  return putDataMetrics(
+    metricDataMap,
+    [
+      ['DeploymentId'],
+      ['DeploymentId', 'Part'],
+      ['DeploymentId', 'Part', 'ReadModel'],
+      ['DeploymentId', 'Part', 'ReadModel', 'Resolver'],
+    ],
+    [
+      ['DeploymentId', 'Error'],
+      ['DeploymentId', 'Part', 'Error'],
+      ['DeploymentId', 'Part', 'ReadModel', 'Resolver', 'Error'],
+    ]
+  )
+}
+
+export const putViewModelProjectionMetrics = async (
+  viewModelName,
+  eventType,
+  error
+) => {
+  const metricDataMap = {
+    DeploymentId: process.env.RESOLVE_DEPLOYMENT_ID,
+    Part: 'ViewModelProjection',
+    ViewModel: viewModelName,
+    EventType: eventType,
+  }
+
+  if (error != null) {
+    metricDataMap.Error = getErrorMessage(error)
+  }
+
+  return putDataMetrics(
+    metricDataMap,
+    [
+      ['DeploymentId'],
+      ['DeploymentId', 'Part'],
+      ['DeploymentId', 'Part', 'ViewModel'],
+      ['DeploymentId', 'Part', 'ViewModel', 'EventType'],
+    ],
+    [
+      ['DeploymentId', 'Error'],
+      ['DeploymentId', 'Part', 'Error'],
+      ['DeploymentId', 'Part', 'ViewModel', 'EventType', 'Error'],
+    ]
+  )
+}
+
+export const putViewModelResolverMetrics = async (
+  viewModelName,
+  error
+) => {
+  const metricDataMap = {
+    DeploymentId: process.env.RESOLVE_DEPLOYMENT_ID,
+    Part: 'ViewModelResolver',
+    ViewModel: viewModelName,
+  }
+
+  if (error != null) {
+    metricDataMap.Error = getErrorMessage(error)
+  }
+
+  return putDataMetrics(
+    metricDataMap,
+    [
+      ['DeploymentId'],
+      ['DeploymentId', 'Part'],
+      ['DeploymentId', 'Part', 'ViewModel'],
+    ],
+    [
+      ['DeploymentId', 'Error'],
+      ['DeploymentId', 'Part', 'Error'],
+      ['DeploymentId', 'Part', 'ViewModel', 'Error'],
+    ]
+  )
+}
+
+export const putSagaMetrics = async (
+  sagaName,
+  eventType,
+  error
+) => {
+  const metricDataMap = {
+    DeploymentId: process.env.RESOLVE_DEPLOYMENT_ID,
+    Part: 'ViewModelResolver',
+    Saga: sagaName,
+  }
+
+  if (error != null) {
+    metricDataMap.Error = getErrorMessage(error)
+  }
+
+  return putDataMetrics(
+    metricDataMap,
+    [
+      ['DeploymentId'],
+      ['DeploymentId', 'Part'],
+      ['DeploymentId', 'Part', 'Saga'],
+      ['DeploymentId', 'Part', 'Saga', 'EventType'],
+    ],
+    [
+      ['DeploymentId', 'Error'],
+      ['DeploymentId', 'Part', 'Error'],
+      ['DeploymentId', 'Part', 'Saga', 'EventType', 'Error'],
+    ]
+  )
+}
+
+export const putInternalError = async (
+  error
+) => {
+  const metricDataMap = {
+    DeploymentId: process.env.RESOLVE_DEPLOYMENT_ID,
+    Part: 'Internal',
+    Error: getErrorMessage(error)
+  }
+
+  return putDataMetrics(
+    metricDataMap,
+    [
+      ['DeploymentId', 'Error'],
+      ['DeploymentId', 'Part', 'Error'],
+    ]
+  )
+}
+
 export const putErrorMetrics = async (error, part) => {
   const cw = new CloudWatch()
   const log = debugLevels('resolve:resolve-runtime:cloud-entry:putErrorMetrics')
