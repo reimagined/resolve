95cf1bc758d877164f6492bd0e4612c461a8d655
Lykoi18 <aiden.ghost666@gmail.com>
2021-01-15 15:10:01 +0300

    Update cloud config

--- examples/with-vanillajs/config.cloud.js
+++ examples/with-vanillajs/config.cloud.js
@@ -7,11 +7,13 @@ export default {
   eventstoreAdapter: {
     module: 'resolve-eventstore-postgresql-serverless',
     options: {
-      awsSecretStoreArn: declareRuntimeEnv('RESOLVE_ES_SECRET_ARN'),
-      dbClusterOrInstanceArn: declareRuntimeEnv('RESOLVE_ES_CLUSTER_ARN'),
-      databaseName: declareRuntimeEnv('RESOLVE_ES_DATABASE'),
-      eventsTableName: declareRuntimeEnv('RESOLVE_ES_EVENTS_TABLE'),
-      secretsTableName: declareRuntimeEnv('RESOLVE_ES_SECRETS_TABLE'),
+      awsSecretStoreArn: declareRuntimeEnv('RESOLVE_USER_SECRET_ARN'),
+      dbClusterOrInstanceArn: declareRuntimeEnv(
+        'RESOLVE_EVENT_STORE_CLUSTER_ARN'
+      ),
+      databaseName: declareRuntimeEnv('RESOLVE_EVENT_STORE_DATABASE_NAME'),
+      eventsTableName: 'events',
+      secretsTableName: 'secrets',
       region: declareRuntimeEnv('AWS_REGION'),
       snapshotBucketSize: 100,
     },
