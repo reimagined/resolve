6926ffae113ad1aabb45909f54f50dc3eac73b71
Max Vasin <maksim.vasin@devexpress.com>
2021-03-02 22:54:01 +0300

    Fix

--- packages/core/scripts/src/generate_custom_mode.js
+++ packages/core/scripts/src/generate_custom_mode.js
@@ -11,7 +11,7 @@ import fsExtra from 'fs-extra'
 import showBuildInfo from './show_build_info'
 import writePackageJsonsForAssemblies from './write_package_jsons_for_assemblies'
 import copyEnvToDist from './copy_env_to_dist'
-import getLog from './getLog'
+import getLog from './get-log'
 
 const log = getLog('custom')
 
