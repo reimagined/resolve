9f6aedff2ea9ff384e5023f85f09853461c63dde
EugeniyBurmistrov <eugeniy.burmistrov@devexpress.com>
2021-03-09 13:01:39 +0300

    Minor fix

--- packages/modules/module-admin/src/commands/event-store/incremental-import.js
+++ packages/modules/module-admin/src/commands/event-store/incremental-import.js
@@ -14,6 +14,6 @@ export const command = 'incremental-import <file-path>'
 export const describe = 'incrementally import an event store to the cloud'
 export const builder = (yargs) =>
   yargs.positional('file-path', {
-    describe: 'path to event store incremental events',
+    describe: 'path to a file containing events for incremental import',
     type: 'string',
   })
