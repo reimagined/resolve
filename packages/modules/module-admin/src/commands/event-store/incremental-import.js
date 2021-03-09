44124c834e41c80bf00fa173b23b8a7e88c3c6d6
Eugeniy Burmistrov <37070809+EugeniyBurmistrov@users.noreply.github.com>
2021-03-09 13:16:57 +0300

    Docs/review module admin strings (#1735)
    * Review strings
    
    * Minor fix

--- packages/modules/module-admin/src/commands/event-store/incremental-import.js
+++ packages/modules/module-admin/src/commands/event-store/incremental-import.js
@@ -11,9 +11,9 @@ export const handler = async ({ url, filePath }) => {
 }
 
 export const command = 'incremental-import <file-path>'
-export const describe = 'incremental import an event-store to the cloud'
+export const describe = 'incrementally import an event store to the cloud'
 export const builder = (yargs) =>
   yargs.positional('file-path', {
-    describe: 'path to event-store incremental events',
+    describe: 'path to a file containing events for incremental import',
     type: 'string',
   })
