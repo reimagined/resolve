36fd87987f403ec58372826a0361fa30515437d5
Roman Chistokhodov <freeslave93@gmail.com>
2021-04-01 21:09:58 +0300

    Generate set and delete secret events for sqlite and postegresql-serverless (#1783)
    Generate set and delete secret events for sqlite and postegresql-serverless

--- packages/runtime/adapters/eventstore-adapters/eventstore-lite/src/delete-secret.ts
+++ packages/runtime/adapters/eventstore-adapters/eventstore-lite/src/delete-secret.ts
@@ -1,20 +1,98 @@
 import getLog from './get-log'
 import { AdapterPool } from './types'
+import {
+  EventstoreFrozenError,
+  InputEvent,
+  makeDeleteSecretEvent,
+} from '@resolve-js/eventstore-base'
 
 const deleteSecret = async (
-  { database, secretsTableName, escapeId }: AdapterPool,
+  pool: AdapterPool,
   selector: string
 ): Promise<void> => {
+  const { database, secretsTableName, escapeId, escape, eventsTableName } = pool
+
   const log = getLog('secretsManager:deleteSecret')
   log.debug(`removing secret from the database`)
 
   log.verbose(`selector: ${selector}`)
   log.verbose(`secretsTableName: ${secretsTableName}`)
 
+  const secretsTableNameAsId = escapeId(secretsTableName)
+  const currentThreadId = Math.floor(Math.random() * 256)
+  const eventsTableNameAsId = escapeId(eventsTableName)
+  const freezeTableNameAsString = escape(`${eventsTableName}-freeze`)
+
+  const deleteSecretEvent: InputEvent = makeDeleteSecretEvent(selector)
+  const serializedPayload = escape(JSON.stringify(deleteSecretEvent.payload))
+
   log.debug(`executing SQL query`)
-  await database.exec(
-    `DELETE FROM ${escapeId(secretsTableName)} WHERE id="${selector}"`
-  )
+
+  try {
+    await database.exec(
+      `BEGIN IMMEDIATE;
+
+    SELECT ABS("CTE"."EventStoreIsFrozen") FROM (
+        SELECT 0 AS "EventStoreIsFrozen"
+      UNION ALL
+        SELECT -9223372036854775808 AS "EventStoreIsFrozen"
+        FROM "sqlite_master"
+        WHERE "type" = 'table' AND 
+        "name" = ${freezeTableNameAsString}
+      ) CTE;
+
+    DELETE FROM ${secretsTableNameAsId} WHERE id=${escape(selector)};
+
+    INSERT INTO ${eventsTableNameAsId}(
+        "threadId",
+        "threadCounter",
+        "timestamp",
+        "aggregateId",
+        "aggregateVersion",
+        "type",
+        "payload"
+      ) VALUES(
+        ${+currentThreadId},
+        COALESCE(
+          (
+            SELECT MAX("threadCounter") FROM ${eventsTableNameAsId}
+            WHERE "threadId" = ${+currentThreadId}
+          ) + 1,
+         0
+        ),
+        MAX(
+          CAST(strftime('%s','now') || substr(strftime('%f','now'),4) AS INTEGER),
+          ${+deleteSecretEvent.timestamp}
+        ),
+        ${escape(deleteSecretEvent.aggregateId)},
+        ${+deleteSecretEvent.aggregateVersion},
+        ${escape(deleteSecretEvent.type)},
+        json(CAST(${serializedPayload} AS BLOB))
+      );
+
+    COMMIT;`
+    )
+  } catch (error) {
+    const errorMessage =
+      error != null && error.message != null ? error.message : ''
+    const errorCode = error != null && error.code != null ? error.code : ''
+
+    try {
+      await database.exec('ROLLBACK;')
+    } catch (e) {}
+
+    if (errorMessage === 'SQLITE_ERROR: integer overflow') {
+      throw new EventstoreFrozenError()
+    } else if (
+      errorCode === 'SQLITE_CONSTRAINT' &&
+      errorMessage.indexOf('PRIMARY') > -1
+    ) {
+      return await deleteSecret(pool, selector)
+    } else {
+      throw error
+    }
+  }
+
   log.debug(`query executed successfully`)
 }
 
--- packages/runtime/adapters/eventstore-adapters/eventstore-postgresql-serverless/src/set-secret.ts
+++ packages/runtime/adapters/eventstore-adapters/eventstore-postgresql-serverless/src/set-secret.ts
@@ -1,6 +1,12 @@
 import getLog from './get-log'
 import { AdapterPool } from './types'
-import { EventstoreFrozenError } from '@resolve-js/eventstore-base'
+import {
+  ConcurrentError,
+  EventstoreFrozenError,
+  InputEvent,
+  makeSetSecretEvent,
+} from '@resolve-js/eventstore-base'
+import { LONG_NUMBER_SQL_TYPE, RESERVED_EVENT_SIZE } from './constants'
 
 const setSecret = async (
   pool: AdapterPool,
@@ -16,6 +22,7 @@ const setSecret = async (
     escape,
     escapeId,
     executeStatement,
+    isTimeoutError,
   } = pool
 
   // TODO: refactor
@@ -40,10 +47,32 @@ const setSecret = async (
   const databaseNameAsString = escape(databaseName)
   const secretsTableNameAsId = escapeId(secretsTableName)
   const freezeTableNameAsString: string = escape(`${eventsTableName}-freeze`)
+  const threadsTableAsId: string = escapeId(`${eventsTableName}-threads`)
+  const eventsTableAsId: string = escapeId(eventsTableName)
 
-  // logging of this sql query can lead to security issues
-  const sql = `WITH "freeze_check" AS (
-              SELECT '' AS "freeze_empty" WHERE (
+  const setSecretEvent: InputEvent = makeSetSecretEvent(selector)
+  const serializedPayload = JSON.stringify(setSecretEvent.payload)
+
+  const serializedEvent: string = [
+    `${escape(setSecretEvent.aggregateId)},`,
+    `${+setSecretEvent.aggregateVersion},`,
+    `${escape(setSecretEvent.type)},`,
+    escape(serializedPayload),
+  ].join('')
+
+  // TODO: Improve calculation byteLength depend on codepage and wide-characters
+  const byteLength: number =
+    Buffer.byteLength(serializedEvent) + RESERVED_EVENT_SIZE
+
+  try {
+    savingSecret: while (true) {
+      try {
+        // logging of this sql query can lead to security issues
+
+        // prettier-ignore
+        await executeStatement(
+          `WITH "freeze_check" AS (
+              SELECT 0 AS "freeze_zero" WHERE (
                 (SELECT 1 AS "EventStoreIsFrozen")
               UNION ALL
                 (SELECT 1 AS "EventStoreIsFrozen"
@@ -51,28 +80,95 @@ const setSecret = async (
                 WHERE "table_schema" = ${databaseNameAsString}
                 AND "table_name" = ${freezeTableNameAsString})
               ) = 1
-            ) INSERT INTO ${databaseNameAsId}.${secretsTableNameAsId}("id", "secret") 
-      VALUES (${escape(selector)}, ${escape(secret)} || (
-        SELECT "freeze_check"."freeze_empty" from "freeze_check" LIMIT 1
-      ))`
-
-  try {
-    log.debug(`executing SQL query`)
-
-    await executeStatement(sql)
-
-    log.debug(`query executed successfully`)
+            ), "vacant_thread_id" AS (
+              SELECT "threadId"
+              FROM ${databaseNameAsId}.${threadsTableAsId}
+              FOR NO KEY UPDATE SKIP LOCKED
+              LIMIT 1
+            ), "random_thread_id" AS (
+              SELECT "threadId"
+              FROM ${databaseNameAsId}.${threadsTableAsId}
+              OFFSET FLOOR(Random() * 256)
+              LIMIT 1
+            ), "vector_id" AS (
+              SELECT "threadId", "threadCounter"
+              FROM ${databaseNameAsId}.${threadsTableAsId}
+              WHERE "threadId" = COALESCE(
+                (SELECT "threadId" FROM "vacant_thread_id"),
+                (SELECT "threadId" FROM "random_thread_id")
+              ) FOR NO KEY UPDATE
+              LIMIT 1
+            ), "update_vector_id" AS (
+              UPDATE ${databaseNameAsId}.${threadsTableAsId}
+              SET "threadCounter" = "threadCounter" + 1
+              WHERE "threadId" = (
+                SELECT "threadId" FROM "vector_id" LIMIT 1
+              )
+              RETURNING *
+            ), "set_secret" AS (
+            INSERT INTO ${databaseNameAsId}.${secretsTableNameAsId}("id", "secret") 
+              VALUES (${escape(selector)}, ${escape(secret)})
+            ) INSERT INTO ${databaseNameAsId}.${eventsTableAsId}(
+              "threadId",
+              "threadCounter",
+              "timestamp",
+              "aggregateId",
+              "aggregateVersion",
+              "type",
+              "payload",
+              "eventSize"
+            ) VALUES (
+              (SELECT "threadId" FROM "vector_id" LIMIT 1) +
+              (SELECT "freeze_zero" FROM "freeze_check" LIMIT 1),
+              (SELECT "threadCounter" FROM "vector_id" LIMIT 1),
+              GREATEST(
+                CAST(extract(epoch from clock_timestamp()) * 1000 AS ${LONG_NUMBER_SQL_TYPE}),
+                ${+setSecretEvent.timestamp}
+              ),
+              ${serializedEvent},
+              ${byteLength}
+            )`
+        )
+        break
+      } catch (error) {
+        if (isTimeoutError(error)) {
+          while (true) {
+            try {
+              const rows = await executeStatement(
+                `SELECT * FROM ${databaseNameAsId}.${eventsTableAsId} 
+                WHERE "aggregateId"=${escape(setSecretEvent.aggregateId)} 
+                AND "aggregateVersion"=${+setSecretEvent.aggregateVersion}
+                LIMIT 1
+                `
+              )
+              if (rows[0] == null) {
+                continue savingSecret
+              }
+              break
+            } catch (selectError) {
+              if (isTimeoutError(selectError)) {
+                continue
+              } else {
+                throw selectError
+              }
+            }
+          }
+        } else {
+          throw error
+        }
+      }
+    }
   } catch (error) {
     const errorMessage =
       error != null && error.message != null ? error.message : ''
 
     if (errorMessage.indexOf('subquery used as an expression') > -1) {
       throw new EventstoreFrozenError()
+    } else if (/aggregateIdAndVersion/i.test(errorMessage)) {
+      throw new ConcurrentError(setSecretEvent.aggregateId)
     } else {
-      log.error(error.message)
-      log.verbose(error.stack)
+      throw error
     }
-    throw error
   }
 }
 
