c7d9ec508f36941773f3c6eca1a31eff41add13d
mrcheater <mister.cheater@list.ru>
2021-04-09 14:55:18 +0300

    Update.$set.tests

--- tests/read-model-store-api/update.$inc.test.js
+++ tests/read-model-store-api/update.$inc.test.js
@@ -23,7 +23,7 @@ describe('Read-model Store API. Update. $inc', () => {
     },
   ]
 
-  test(`store.insert('test', { testId, key: 0 })\n      store.update('test', { testId }, { $inc: { ['key']: 1 } } )`, async () => {
+  test(`Projection\n        store.insert('test', { testId, key: 0 })\n        store.update('test', { testId }, { $inc: { ['key']: 1 } } )\n      Resolver should return [{ testId: 'root', key: 1 }]`, async () => {
     const adapter = createConnector({
       databaseFile: ':memory:',
     })
@@ -58,20 +58,19 @@ describe('Read-model Store API. Update. $inc', () => {
       },
     }
 
-    const result =
-      await givenEvents(events)
-        .readModel({
-          name: 'StoreApi',
-          projection,
-          resolvers,
-        })
-        .withAdapter(adapter)
-        .query('all', {})
+    const result = await givenEvents(events)
+      .readModel({
+        name: 'StoreApi',
+        projection,
+        resolvers,
+      })
+      .withAdapter(adapter)
+      .query('all', {})
 
-    expect(result).toMatchSnapshot()
+    expect(result).toEqual([{ testId: 'root', key: 1 }])
   })
 
-  test(`store.insert('test', { testId, key: undefined })\n      store.update('test', { testId }, { $inc: { ['key']: 1 } } )`, async () => {
+  test(`Projection\n        store.insert('test', { testId, key: undefined })\n        store.update('test', { testId }, { $inc: { ['key']: 1 } } )`, async () => {
     const adapter = createConnector({
       databaseFile: ':memory:',
     })
@@ -106,19 +105,19 @@ describe('Read-model Store API. Update. $inc', () => {
       },
     }
 
-    const result  =await givenEvents(events)
-        .readModel({
-          name: 'StoreApi',
-          projection,
-          resolvers,
-        })
-        .withAdapter(adapter)
-        .query('all', {})
+    const result = await givenEvents(events)
+      .readModel({
+        name: 'StoreApi',
+        projection,
+        resolvers,
+      })
+      .withAdapter(adapter)
+      .query('all', {})
 
     expect(result).toMatchSnapshot()
   })
 
-  test(`store.insert('test', { testId, key: null })\n      store.update('test', { testId }, { $inc: { ['key']: 1 } } )`, async () => {
+  test(`Projection\n        store.insert('test', { testId, key: null })\n        store.update('test', { testId }, { $inc: { ['key']: 1 } } )`, async () => {
     const adapter = createConnector({
       databaseFile: ':memory:',
     })
@@ -154,19 +153,18 @@ describe('Read-model Store API. Update. $inc', () => {
     }
 
     const result = await givenEvents(events)
-        .readModel({
-          name: 'StoreApi',
-          projection,
-          resolvers,
-        })
-        .withAdapter(adapter)
-        .query('all', {})
-
+      .readModel({
+        name: 'StoreApi',
+        projection,
+        resolvers,
+      })
+      .withAdapter(adapter)
+      .query('all', {})
 
     expect(result).toMatchSnapshot()
   })
 
-  test(`store.insert('test', { testId, obj: { key: 0 } })\n      store.update('test', { testId }, { $inc: { ['obj.key']: 1 } } )`, async () => {
+  test(`Projection\n        store.insert('test', { testId, obj: { key: 0 } })\n        store.update('test', { testId }, { $inc: { ['obj.key']: 1 } } )`, async () => {
     const adapter = createConnector({
       databaseFile: ':memory:',
     })
@@ -203,20 +201,19 @@ describe('Read-model Store API. Update. $inc', () => {
       },
     }
 
-    const result =
-      await givenEvents(events)
-        .readModel({
-          name: 'StoreApi',
-          projection,
-          resolvers,
-        })
-        .withAdapter(adapter)
-        .query('all', {})
+    const result = await givenEvents(events)
+      .readModel({
+        name: 'StoreApi',
+        projection,
+        resolvers,
+      })
+      .withAdapter(adapter)
+      .query('all', {})
 
-    expect(result).toMatchSnapshot()
+    expect(result).toEqual([{ testId: 'root', obj: { key: 1 } }])
   })
 
-  test(`store.insert('test', { testId })\n      store.update('test', { testId }, { $inc: { ['key']: 1 } } )`, async () => {
+  test(`Projection\n        store.insert('test', { testId })\n        store.update('test', { testId }, { $inc: { ['key']: 1 } } )`, async () => {
     const adapter = createConnector({
       databaseFile: ':memory:',
     })
@@ -262,7 +259,7 @@ describe('Read-model Store API. Update. $inc', () => {
     expect(result).toMatchSnapshot()
   })
 
-  test(`store.insert('test', { testId })\n      store.update('test', { testId }, { $inc: { ['obj.key']: 1 } } )`, async () => {
+  test(`Projection\n        store.insert('test', { testId })\n        store.update('test', { testId }, { $inc: { ['obj.key']: 1 } } )`, async () => {
     const adapter = createConnector({
       databaseFile: ':memory:',
     })
@@ -308,7 +305,7 @@ describe('Read-model Store API. Update. $inc', () => {
     expect(result).toMatchSnapshot()
   })
 
-  test(`store.insert('test', { testId, obj: {} })\n      store.update('test', { testId }, { $inc: { ['obj.key']: 1 } } )`, async () => {
+  test(`Projection\n        store.insert('test', { testId, obj: {} })\n        store.update('test', { testId }, { $inc: { ['obj.key']: 1 } } )`, async () => {
     const adapter = createConnector({
       databaseFile: ':memory:',
     })
@@ -355,7 +352,7 @@ describe('Read-model Store API. Update. $inc', () => {
     expect(result).toMatchSnapshot()
   })
 
-  test(`store.insert('test', { testId, deepObj: {} })\n      store.update('test', { testId }, { $inc: { ['deepObj.obj.key']: 1 } } )`, async () => {
+  test(`Projection\n        store.insert('test', { testId, deepObj: {} })\n        store.update('test', { testId }, { $inc: { ['deepObj.obj.key']: 1 } } )`, async () => {
     const adapter = createConnector({
       databaseFile: ':memory:',
     })
