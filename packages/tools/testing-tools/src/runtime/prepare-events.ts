3c344558390dc929e541c6f5aab5ca005853ae80
mrcheater <mister.cheater@list.ru>
2021-04-22 15:30:27 +0300

    Fix testing-tools

--- packages/tools/testing-tools/src/runtime/prepare-events.ts
+++ packages/tools/testing-tools/src/runtime/prepare-events.ts
@@ -8,7 +8,6 @@ export const prepareEvents = (
 ): any => {
   let timestamp = 1
   const aggregateVersionsMap = new Map()
-  const threadCountersMap = new Map()
 
   return events.map((testEvent) => {
     const aggregateId =
@@ -19,25 +18,15 @@ export const prepareEvents = (
     const aggregateVersion = aggregateVersionsMap.has(aggregateId)
       ? aggregateVersionsMap.get(aggregateId) + 1
       : 1
-    const threadId = Buffer.from(JSON.stringify(testEvent)).reduce(
-      (acc, val) => (acc + val) % 256,
-      0
-    )
-    const threadCounter = threadCountersMap.has(threadId)
-      ? threadCountersMap.get(threadId) + 1
-      : 1
 
     const event = {
       ...testEvent,
       aggregateId,
       aggregateVersion,
       timestamp: timestamp++,
-      threadId,
-      threadCounter,
     }
 
     aggregateVersionsMap.set(aggregateId, aggregateVersion)
-    threadCountersMap.set(threadId, threadCounter + 1)
 
     return event
   })
