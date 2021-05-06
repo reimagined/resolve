058ffc952ee7e4ae00299a557866cd4bbab69a37
mrcheater <mister.cheater@list.ru>
2021-05-06 18:03:36 +0300

    Add count.test.js (failed)

---
+++ tests/read-model-store-api/count.test.js
@@ -0,0 +1,63 @@
+import givenEvents from '@resolve-js/testing-tools'
+
+import { jestTimeout, adapters, adapterFactory } from '../readmodel-test-utils'
+import resolvers from './resolvers'
+
+jest.setTimeout(jestTimeout())
+
+// eslint-disable-next-line no-console
+console.error = () => {}
+
+describe(`${adapterFactory.name}. Read-model Store API. Count`, () => {
+  beforeEach(adapterFactory.create('count'))
+  afterEach(adapterFactory.destroy('count'))
+
+  const adapter = adapters['count']
+
+  test(
+    [
+      `Projection (Event count = 200)`,
+      `  store.defineTable({ /* ... */ fields: [] })`,
+      `  store.insert(/* ... *//)`,
+      `  store.count should return 200`,
+    ].join('\n'),
+    async () => {
+      const eventCount = 200
+
+      const events = []
+      for (let index = 0; index < eventCount; index++) {
+        events.push({
+          aggregateId: `id${index}`,
+          type: 'TEST',
+          payload: {},
+        })
+      }
+
+      const projection = {
+        Init: async (store) => {
+          await store.defineTable('test', {
+            indexes: { testId: 'string' },
+            fields: [],
+          })
+        },
+
+        TEST: async (store, { aggregateId }) => {
+          await store.insert('test', {
+            testId: aggregateId,
+          })
+        },
+      }
+
+      const result = await givenEvents(events)
+        .readModel({
+          name: 'StoreApi',
+          projection,
+          resolvers,
+        })
+        .withAdapter(adapter)
+        .query('count', {})
+
+      expect(result).toEqual(eventCount)
+    }
+  )
+})
