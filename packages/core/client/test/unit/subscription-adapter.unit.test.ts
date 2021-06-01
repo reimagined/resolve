669f73673b4a2c80f54baf43b5e2b93fa6310381
timbset <timbset@gmail.com>
2021-06-01 16:58:38 +0300

    Add messaging unit tests in subscription adapter

--- packages/core/client/test/unit/subscription-adapter.unit.test.ts
+++ packages/core/client/test/unit/subscription-adapter.unit.test.ts
@@ -1,3 +1,6 @@
+import { v4 as uuidV4 } from 'uuid'
+import { mocked } from 'ts-jest/utils'
+
 import adapterFactory, {
   SubscriptionAdapter,
 } from '../../src/subscribe-adapter'
@@ -8,6 +11,10 @@ import {
 } from '../../src/subscribe-adapter-constants'
 import { SubscriptionAdapterStatus } from '../../src/types'
 
+jest.mock('uuid', () => ({
+  v4: jest.fn(),
+}))
+
 let ws: {
   readyState: number
   onopen: Function
@@ -15,6 +22,9 @@ let ws: {
   send: jest.Mock
   close: jest.Mock
 }
+
+let uuidV4Mock = mocked(uuidV4)
+
 beforeAll(() => {
   ws = {
     readyState: 0,
@@ -36,6 +46,10 @@ beforeEach(() => {
     cursor: 'A',
     onEvent,
   })
+})
+
+afterEach(() => {
+  uuidV4Mock.mockClear()
 })
 
 describe('construction', () => {
@@ -79,6 +93,100 @@ describe('init', () => {
     ws.onopen()
     adapter.close()
     expect(adapter.init).toThrow(subscriptionAdapterClosed)
+  })
+})
+
+describe('messaging', () => {
+  const events = [
+    {
+      threadCounter: 0,
+      threadId: 0,
+      type: 'ItemUpdated',
+      timestamp: 1,
+      aggregateId: 'test-aggregate',
+      aggregateVersion: 0,
+      payload: {
+        isUpdated: true,
+      },
+    },
+    {
+      threadCounter: 1,
+      threadId: 0,
+      type: 'ItemUpdated',
+      timestamp: 2,
+      aggregateId: 'test-aggregate',
+      aggregateVersion: 1,
+      payload: {
+        isUpdated: true,
+      },
+    },
+  ]
+
+  test('sends "pullEvents" message on connection open', () => {
+    uuidV4Mock.mockReturnValueOnce('test-id-1')
+
+    adapter.init()
+    ws.onopen()
+
+    expect(ws.send).toBeCalledWith(
+      JSON.stringify({
+        type: 'pullEvents',
+        cursor: 'A',
+        requestId: 'test-id-1',
+      })
+    )
+  })
+
+  test('calls onEvent with pulled events', () => {
+    uuidV4Mock.mockReturnValueOnce('test-id-1')
+
+    adapter.init()
+    ws.onopen()
+
+    ws.onmessage({
+      data: JSON.stringify({
+        type: 'pullEvents',
+        payload: {
+          cursor: 'B',
+          events,
+        },
+      }),
+    })
+
+    expect(onEvent).toBeCalledWith(events[0])
+    expect(onEvent).toBeCalledWith(events[1])
+  })
+
+  test('pulls event on event message with new cursor', () => {
+    uuidV4Mock.mockReturnValueOnce('test-id-1').mockReturnValueOnce('test-id-2')
+
+    adapter.init()
+    ws.onopen()
+
+    ws.onmessage({
+      data: JSON.stringify({
+        type: 'pullEvents',
+        payload: {
+          cursor: 'B',
+          events: [events[0]],
+        },
+      }),
+    })
+
+    ws.onmessage({
+      data: JSON.stringify({
+        type: 'event',
+        event: [events[1]],
+      }),
+    })
+
+    expect(ws.send).toBeCalledWith(
+      JSON.stringify({
+        type: 'pullEvents',
+        cursor: 'B',
+        requestId: 'test-id-2',
+      })
+    )
   })
 })
 
