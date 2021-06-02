904d88ff84af3d56f7d959cc2bdd4f65ce0b7ab8
Max <mozg.mayhem@gmail.com>
2021-06-02 14:09:14 +0300

    Refactor StaticImage

--- examples/hacker-news/client/containers/StaticImage.js
+++ examples/hacker-news/client/containers/StaticImage.js
@@ -1,5 +1,9 @@
-import { connectStaticBasedUrls } from '@resolve-js/redux'
+import React from 'react'
+import { useStaticResolver } from '@resolve-js/react-hooks'
 
-const StaticImage = connectStaticBasedUrls(['src'])('img')
+const StaticImage = ({ src, ...props }) => {
+  const asset = useStaticResolver()
+  return <img src={asset(src)} {...props} />
+}
 
 export { StaticImage }
