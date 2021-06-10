6f92f2f0f3cf5c7580402bb71683f40ac78a7245
Max <mozg.mayhem@gmail.com>
2021-06-10 20:41:17 +0300

    wip

--- templates/ts/vue/run.ts
+++ templates/ts/vue/run.ts
@@ -75,7 +75,7 @@ void (async () => {
         await runTestcafe({
           resolveConfig,
           adjustWebpackConfigs,
-          functionalTestsDir: 'test/functional',
+          functionalTestsDir: 'test/e2e',
           browser: process.argv[3],
           customArgs: ['--stop-on-first-fail'],
         })
