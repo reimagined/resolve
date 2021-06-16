e559d7f18e45303c996fdccb5624abd18a8180b9
Max <mozg.mayhem@gmail.com>
2021-06-16 18:30:21 +0300

    Unify jest configs

--- examples/ts/hacker-news/jest.config.ts
+++ examples/ts/hacker-news/jest.config.ts
@@ -1,11 +1,17 @@
+import type { Config } from '@jest/types'
+
 process.env.TZ = 'Europe/Moscow'
 
-module.exports = {
+const config: Config.InitialOptions = {
   testEnvironment: 'node',
+  setupFiles: ['<rootDir>/test/unit/jest-setup.ts'],
   coveragePathIgnorePatterns: ['node_modules/', 'dist/', 'coverage/'],
   unmockedModulePathPatterns: ['node_modules/react/', 'node_modules/enzyme/'],
+  testMatch: ['**/test/unit/**/*.test.ts'],
   transform: {
     '^.+\\.js$': 'babel-jest',
     '^.+\\.tsx?$': 'ts-jest',
   },
 }
+
+export default config
--- templates/ts/styled-components/jest.config.js
+++
@@ -1,9 +0,0 @@
-process.env.TZ = 'Europe/Moscow'
-
-module.exports = {
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-}
