fece928b218810f59fd917f0a8f4bd39ecef7abc
Max <mozg.mayhem@gmail.com>
2021-06-17 11:53:24 +0300

    wip

--- examples/ts/hacker-news/jest.config.ts
+++ examples/ts/hacker-news/jest.config.ts
@@ -7,7 +7,7 @@ const config: Config.InitialOptions = {
   setupFiles: ['<rootDir>/test/unit/jest-setup.ts'],
   coveragePathIgnorePatterns: ['node_modules/', 'dist/', 'coverage/'],
   unmockedModulePathPatterns: ['node_modules/react/', 'node_modules/enzyme/'],
-  testMatch: ['**/test/unit/**/*.test.ts'],
+  testMatch: ['**/test/unit/**/*.test.(j|t)s(x)?'],
   transform: {
     '^.+\\.js$': 'babel-jest',
     '^.+\\.tsx?$': 'ts-jest',
--- examples/ts/shopping-list-redux-hoc/jest.config.ts
+++ examples/ts/shopping-list-redux-hoc/jest.config.ts
@@ -6,7 +6,7 @@ const config: Config.InitialOptions = {
   testEnvironment: 'node',
   coveragePathIgnorePatterns: ['node_modules/', 'dist/', 'coverage/'],
   unmockedModulePathPatterns: ['node_modules/react/', 'node_modules/enzyme/'],
-  testMatch: ['**/test/unit/*.test.ts'],
+  testMatch: ['**/test/unit/**/*.test.(j|t)s(x)?'],
   transform: {
     '^.+\\.js$': 'babel-jest',
     '^.+\\.tsx?$': 'ts-jest',
