9f97a8401386415afd4e59b2f66ae1f9793b7e52
Roman Chistokhodov <freeslave93@gmail.com>
2021-06-18 14:53:13 +0300

    Fix load events with timestamp filter, add tests

--- tests/eventstore-test-utils/index.ts
+++ tests/eventstore-test-utils/index.ts
@@ -109,7 +109,7 @@ let adapters: Record<string, Adapter> = {}
 const proxy = new Proxy(
   {},
   {
-    get(_: any, adapterName: string): any {
+    get(_: any, adapterName: string): Adapter {
       return new Proxy(
         {},
         {
@@ -124,13 +124,13 @@ const proxy = new Proxy(
             throw new TypeError()
           },
         }
-      )
+      ) as Adapter
     },
     set() {
       throw new TypeError()
     },
   }
-)
+) as typeof adapters
 
 export { proxy as adapters }
 
