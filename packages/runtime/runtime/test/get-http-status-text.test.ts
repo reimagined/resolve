8f9cdf1fcb09da5b002f595fa043a2f419023f2a
mrcheater <mister.cheater@list.ru>
2021-07-16 16:53:16 +0300

    Fix typo

--- packages/runtime/runtime/test/get-http-status-text.test.ts
+++ packages/runtime/runtime/test/get-http-status-text.test.ts
@@ -1,24 +1,26 @@
+import { getReasonPhrase } from 'http-status-codes'
+
 import getHttpStatusText from '../src/common/utils/get-http-status-text'
 
 describe('method "getHttpStatusText"', () => {
-  test('should return 500 when code = undefined', () => {
-    expect(getHttpStatusText(200)).toEqual('OK')
+  test('should return "OK" when status = 200', () => {
+    expect(getHttpStatusText(200)).toEqual(getReasonPhrase(200))
+  })
+
+  test('should return "Bad Gateway" when status = 400', () => {
+    expect(getHttpStatusText(400)).toEqual(getReasonPhrase(400))
+  })
+
+  test('should return "Not Found" when status = 404', () => {
+    expect(getHttpStatusText(404)).toEqual(getReasonPhrase(404))
+  })
+
+  test('should return "Internal Server Error" when status = 500', () => {
+    expect(getHttpStatusText(500)).toEqual(getReasonPhrase(500))
   })
 
   test('should return "Bad Gateway" when status = 502', () => {
-    expect(getHttpStatusText(400)).toEqual('Bad Request')
-  })
-
-  test('should return "Not Found" when status = 502', () => {
-    expect(getHttpStatusText(404)).toEqual('Not Found')
-  })
-
-  test('should return "Internal Server Error" when status = 502', () => {
-    expect(getHttpStatusText(500)).toEqual('Internal Server Error')
-  })
-
-  test('should return "Bad Gateway" when status = 502', () => {
-    expect(getHttpStatusText(502)).toEqual('Bad Gateway')
+    expect(getHttpStatusText(502)).toEqual(getReasonPhrase(502))
   })
 
   test('should return "" when unknown error', () => {
