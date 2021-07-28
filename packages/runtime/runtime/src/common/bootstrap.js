179f162b7794bf0f143010c5e5ccb98e07c621e2
Vladislav V <ihost@mail.ru>
2021-07-28 17:40:07 +0300

    Soft bootstrap (primitive without mutexes for concurrent bootstrap/shutdown) (#1971)

--- packages/runtime/runtime/src/common/bootstrap.js
+++ packages/runtime/runtime/src/common/bootstrap.js
@@ -1,23 +1,61 @@
 import debugLevels from '@resolve-js/debug-levels'
 
 import bootstrapOne from './bootstrap-one'
+import shutdownOne from './shutdown-one'
 
 const log = debugLevels('resolve:runtime:bootstrap')
 
 const bootstrap = async (resolve) => {
   log.debug('bootstrap started')
   const promises = []
+
+  const existingEventSubscribers = (
+    await resolve.eventstoreAdapter.getEventSubscribers({
+      applicationName: resolve.eventSubscriberScope,
+    })
+  ).map(({ eventSubscriber }) => eventSubscriber)
+
   for (const { name, eventTypes } of resolve.eventListeners.values()) {
     promises.push(
-      bootstrapOne({
+      // eslint-disable-next-line no-loop-func
+      (async () => {
+        let eventSubscriberExists = existingEventSubscribers.indexOf(name) > -1
+        if (eventSubscriberExists) {
+          try {
+            eventSubscriberExists =
+              (await resolve.eventSubscriber.status({
+                eventSubscriber: name,
+              })) != null
+          } catch (err) {}
+        }
+        if (!eventSubscriberExists) {
+          await bootstrapOne({
+            applicationName: resolve.eventSubscriberScope,
+            eventstoreAdapter: resolve.eventstoreAdapter,
+            eventSubscriber: resolve.eventSubscriber,
+            name,
+            eventTypes,
+            destination: resolve.getEventSubscriberDestination(name),
+            upstream: resolve.upstream,
+            ensureQueue: resolve.ensureQueue,
+          })
+        }
+      })()
+    )
+  }
+
+  for (const name of existingEventSubscribers.filter(
+    (name) => !resolve.eventListeners.has(name)
+  )) {
+    promises.push(
+      shutdownOne({
         applicationName: resolve.eventSubscriberScope,
         eventstoreAdapter: resolve.eventstoreAdapter,
         eventSubscriber: resolve.eventSubscriber,
         name,
-        eventTypes,
-        destination: resolve.getEventSubscriberDestination(name),
         upstream: resolve.upstream,
-        ensureQueue: resolve.ensureQueue,
+        deleteQueue: resolve.deleteQueue,
+        soft: true,
       })
     )
   }
