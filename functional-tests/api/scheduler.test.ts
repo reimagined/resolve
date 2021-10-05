0d5ed0afe38fbdef082b141935064c0893f89316
Max <mozg.mayhem@gmail.com>
2021-10-05 19:49:06 +0300

    Remove deprecated

--- functional-tests/api/scheduler.test.ts
+++ functional-tests/api/scheduler.test.ts
@@ -1,5 +1,5 @@
 import { nanoid } from 'nanoid'
-import { Client } from '@resolve-js/client'
+import { Client, createWaitForResponseMiddleware } from '@resolve-js/client'
 
 import { getClient } from '../utils/utils'
 
@@ -43,10 +43,17 @@ test('executes scheduled command correctly', async () => {
         args: { id: testId },
       },
       {
-        waitFor: {
-          validator: (result) => result?.data != null,
-          attempts: 5,
-          period: 3000,
+        middleware: {
+          response: createWaitForResponseMiddleware({
+            validator: async (response, confirm) => {
+              const result = response.json()
+              if (result != null) {
+                confirm(result)
+              }
+            },
+            attempts: 5,
+            period: 3000,
+          }),
         },
       }
     )
