382118ee271ee8df0bc18c8422e66055bdccf05d
Roman Chistokhodov <freeslave93@gmail.com>
2021-10-18 21:24:36 +0300

    Use eventLoader in replicator

--- packages/modules/module-replication/src/api-handlers/replication_state.ts
+++ packages/modules/module-replication/src/api-handlers/replication_state.ts
@@ -2,7 +2,12 @@ import type { ResolveRequest, ResolveResponse } from '@resolve-js/core'
 
 const handler = async (req: ResolveRequest, res: ResolveResponse) => {
   const result = await req.resolve.eventstoreAdapter.getReplicationState()
-  res.json(result)
+  const description = await req.resolve.eventstoreAdapter.describe()
+  res.json({
+    ...result,
+    totalEventCount: description.eventCount,
+    totalSecretCount: description.secretCount,
+  })
 }
 
 export default handler
