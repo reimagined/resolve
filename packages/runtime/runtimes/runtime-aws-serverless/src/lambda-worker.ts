eb7d8479225fe2a1830dccd7c2298dc0dfa3abe0
Roman Chistokhodov <freeslave93@gmail.com>
2021-11-22 17:54:44 +0300

    Better logging. Fix #2141. Fix casting loadEventsByTimestamp result to JSON (#2142)

--- packages/runtime/runtimes/runtime-aws-serverless/src/lambda-worker.ts
+++ packages/runtime/runtimes/runtime-aws-serverless/src/lambda-worker.ts
@@ -341,10 +341,10 @@ export const lambdaWorker = async (
     runtime?.monitoring?.group({ Part: 'Internal' }).error(error)
 
     if (error instanceof Error) {
-      log.error('error', error.message)
-      log.error('error', error.stack)
+      log.error('Lambda top-level error message:', error.message)
+      log.error('Lambda top-level error stack:', error.stack)
     } else {
-      log.error(JSON.stringify(error))
+      log.error('Lambda unknown top-level error:', JSON.stringify(error))
     }
 
     throw error
