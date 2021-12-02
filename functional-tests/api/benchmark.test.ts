562c43f0fef491b5b3d4a00b5f77dfc782ebfa14
Aleksander Safronov <lok_92@mail.ru>
2021-12-02 15:01:21 +0300

    More highload errors

--- functional-tests/api/benchmark.test.ts
+++ functional-tests/api/benchmark.test.ts
@@ -120,7 +120,7 @@ const getReadModelFeedingRateMetric = async (readModelName: string) => {
 
 const isHighloadError = (error: Error) =>
   error != null &&
-  /(?:HTTP ERROR 503)|(?:ETIMEDOUT)|(?:ECONNRESET)|(?:EMFILE)|(?:sorry, too many clients already)|(?:remaining connection slots are reserved for non-replication superuser and rds_superuser connections)/.test(
+  /(?:HTTP ERROR 503)|(?:ETIMEDOUT)|(?:ECONNRESET)|(?:EMFILE)|(?:sorry, too many clients already)|(?:remaining connection slots are reserved for non-replication superuser and rds_superuser connections)|(?:Connection terminated unexpectedly)/.test(
     error.message
   )
 
