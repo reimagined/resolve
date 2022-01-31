9053f574f2e1f4df16d3ff2e01282f77a45f0381
EugeniyBurmistrov <eugeniy.burmistrov@devexpress.com>
2022-01-31 10:21:28 +0300

    Deploy website - based on 7b665eca98fd47d29444d500f55a0ffdc8b8907c

--- assets/js/814f3328.45faa4be.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[2535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome to reSolve","permalink":"/resolve/blog/2018/12/5/welcome"}]}')}}]);
---
+++ assets/js/a80da1cf.9b19d795.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[205],{4863:function(s){s.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"/blog/tags/docusaurus","name":"docusaurus","count":3,"permalink":"/blog/tags/docusaurus"}')}}]);
