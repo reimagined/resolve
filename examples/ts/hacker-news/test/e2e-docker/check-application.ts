02fb6d376b0aa7dd79807a0b1cf26275d8dda6be
Timofey Traynenkov <timbset@gmail.com>
2022-02-14 14:18:59 +0300

    Docker example (#2170)
    * Add example of using of Docker into Hacker-News example. Setup CI checking that image has built correctly
    
    * Fix directories in CI
    
    * Fix CI script
    
    * Try to fix e2e task
    
    * Try to fix e2e task
    
    * Try to fix e2e task
    
    * Try to fix e2e task
    
    * Try to fix e2e task
    
    * Fix docker e2e tests runner
    
    * Uncomment all skipped CI tasks
    
    * Remove redundant condition in CI
    
    * Add running in containers docs
    
    * Structure
    
    * Configure reSolve + postgres
    
    * Minor adjustments
    
    * Draft sections
    
    * Build and run in Docker Compose
    
    * Add intro
    
    * Minor fix
    
    * Factual error
    
    * Note on example credentials
    
    * Apply corrections
    
    * Stylistic changes and fixes
    
    * Apply suggestions from code review
    
    Co-authored-by: Albert Totten <49917542+albertov05@users.noreply.github.com>
    
    * Change CI job run condition
    
    * Exclude e2e docker run into different script
    
    Co-authored-by: EugeniyBurmistrov <eugeniy.burmistrov@devexpress.com>
    Co-authored-by: Psycrab <psycrab88@gmail.com>
    Co-authored-by: Eugeniy Burmistrov <37070809+EugeniyBurmistrov@users.noreply.github.com>
    Co-authored-by: Albert Totten <49917542+albertov05@users.noreply.github.com>

---
+++ examples/ts/hacker-news/test/e2e-docker/check-application.ts
@@ -0,0 +1,10 @@
+import { ROOT_URL, login } from '../e2e/utils'
+
+fixture`application check`.beforeEach(async (t) => {
+  await t.setNativeDialogHandler(() => true)
+  await t.navigateTo(`${ROOT_URL}/login`)
+})
+
+test(`login`, async (t) => {
+  await login(t)
+})
