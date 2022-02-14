f656854515bf9b8df82000f5e55f267f79935e5e
Eugeniy Burmistrov <37070809+EugeniyBurmistrov@users.noreply.github.com>
2022-02-14 12:25:26 +0300

    Fix linter warnings (#2238)

--- tutorial/lesson-5/client/routes.js
+++ tutorial/lesson-5/client/routes.js
@@ -2,7 +2,7 @@ import App from './components/App'
 import MyLists from './components/MyLists'
 import ShoppingList from './components/ShoppingList'
 
-export const routes = [
+const routes = [
   {
     component: App,
     routes: [
@@ -18,3 +18,5 @@ export const routes = [
     ],
   },
 ]
+
+export default routes
