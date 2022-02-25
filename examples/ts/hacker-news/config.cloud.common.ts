36750a6878b9887237359d870e5fd94cd18ea55d
mrcheater <mister.cheater@list.ru>
2022-02-25 16:03:16 +0300

    Revert hacker-news config.cloud.common.ts

--- examples/ts/hacker-news/config.cloud.common.ts
+++ examples/ts/hacker-news/config.cloud.common.ts
@@ -1,10 +1,57 @@
+import { declareRuntimeEnv } from '@resolve-js/scripts'
+
 const cloudCommonConfig = {
   mode: 'production',
   runtime: {
     module: '@resolve-js/runtime-aws-serverless',
     options: { importMode: 'dynamic' },
   },
+  staticPath: declareRuntimeEnv('RESOLVE_CLOUD_STATIC_URL'),
+  eventstoreAdapter: {
+    module: '@resolve-js/eventstore-postgresql',
+    options: {
+      databaseName: declareRuntimeEnv('RESOLVE_EVENT_STORE_DATABASE_NAME'),
+      host: declareRuntimeEnv('RESOLVE_EVENT_STORE_CLUSTER_HOST'),
+      port: declareRuntimeEnv('RESOLVE_EVENT_STORE_CLUSTER_PORT'),
+      user: declareRuntimeEnv('RESOLVE_USER_ID'),
+      password: declareRuntimeEnv('RESOLVE_USER_PASSWORD'),
+      database: 'postgres',
+    },
+  },
   readModelConnectors: {
+    default: {
+      module: '@resolve-js/readmodel-postgresql',
+      options: {
+        databaseName: declareRuntimeEnv('RESOLVE_READMODEL_DATABASE_NAME'),
+        host: declareRuntimeEnv('RESOLVE_READMODEL_CLUSTER_HOST'),
+        port: declareRuntimeEnv('RESOLVE_READMODEL_CLUSTER_PORT'),
+        user: declareRuntimeEnv('RESOLVE_USER_ID'),
+        password: declareRuntimeEnv('RESOLVE_USER_PASSWORD'),
+        database: 'postgres',
+      },
+    },
+    hackerNews: {
+      module: '@resolve-js/readmodel-postgresql',
+      options: {
+        databaseName: declareRuntimeEnv('RESOLVE_READMODEL_DATABASE_NAME'),
+        host: declareRuntimeEnv('RESOLVE_READMODEL_CLUSTER_HOST'),
+        port: declareRuntimeEnv('RESOLVE_READMODEL_CLUSTER_PORT'),
+        user: declareRuntimeEnv('RESOLVE_USER_ID'),
+        password: declareRuntimeEnv('RESOLVE_USER_PASSWORD'),
+        database: 'postgres',
+      },
+    },
+    comments: {
+      module: '@resolve-js/readmodel-postgresql',
+      options: {
+        databaseName: declareRuntimeEnv('RESOLVE_READMODEL_DATABASE_NAME'),
+        host: declareRuntimeEnv('RESOLVE_READMODEL_CLUSTER_HOST'),
+        port: declareRuntimeEnv('RESOLVE_READMODEL_CLUSTER_PORT'),
+        user: declareRuntimeEnv('RESOLVE_USER_ID'),
+        password: declareRuntimeEnv('RESOLVE_USER_PASSWORD'),
+        database: 'postgres',
+      },
+    },
     elasticSearch: {
       module: 'common/read-models/elastic-search-connector.ts',
       options: {
